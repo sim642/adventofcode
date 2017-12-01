4a4313535745f79c5d69165ca6955abf0efa6949
Simmo Saan <simmo.saan@gmail.com>
2017-12-01 13:29:53 +0200

    Extract input into resources in 2017 day 1

--- src/test/scala/eu/sim642/adventofcode2017/Day1Test.scala
+++ src/test/scala/eu/sim642/adventofcode2017/Day1Test.scala
@@ -1,26 +1,31 @@
 package eu.sim642.adventofcode2017
 
+import eu.sim642.adventofcode2017.Day1._
 import org.scalatest.FunSuite
 
 class Day1Test extends FunSuite {
 
-  test("Part 1") {
-    import eu.sim642.adventofcode2017.Day1.Part1.captcha
-
-    assert(captcha("1122") == 3)
-    assert(captcha("1111") == 4)
-    assert(captcha("1234") == 0)
-    assert(captcha("91212129") == 9)
+  test("Part 1 examples") {
+    assert(Part1.captcha("1122") == 3)
+    assert(Part1.captcha("1111") == 4)
+    assert(Part1.captcha("1234") == 0)
+    assert(Part1.captcha("91212129") == 9)
   }
 
-  test("Part 2") {
-    import eu.sim642.adventofcode2017.Day1.Part2.captcha
+  test("Part 1 input answer") {
+    assert(Part1.captcha(input) == 1069)
+  }
 
-    assert(captcha("1212") == 6)
-    assert(captcha("1221") == 0)
-    assert(captcha("123425") == 4)
-    assert(captcha("123123") == 12)
-    assert(captcha("12131415") == 4)
+  test("Part 2 examples") {
+    assert(Part2.captcha("1212") == 6)
+    assert(Part2.captcha("1221") == 0)
+    assert(Part2.captcha("123425") == 4)
+    assert(Part2.captcha("123123") == 12)
+    assert(Part2.captcha("12131415") == 4)
+  }
+
+  test("Part 2 input answer") {
+    assert(Part2.captcha(input) == 1268)
   }
 
 }
