adda35858f2837340a6362f544b00c48a358a7d3
Simmo Saan <simmo.saan@gmail.com>
2017-12-01 11:55:16 +0200

    Solve 2017 day 1 part 2

--- src/test/scala/eu/sim642/adventofcode2017/Day1Test.scala
+++ src/test/scala/eu/sim642/adventofcode2017/Day1Test.scala
@@ -1,15 +1,26 @@
 package eu.sim642.adventofcode2017
 
-import eu.sim642.adventofcode2017.Day1.captcha
 import org.scalatest.FunSuite
 
 class Day1Test extends FunSuite {
 
-  test("captcha") {
+  test("Part 1") {
+    import eu.sim642.adventofcode2017.Day1.Part1.captcha
+
     assert(captcha("1122") == 3)
     assert(captcha("1111") == 4)
     assert(captcha("1234") == 0)
     assert(captcha("91212129") == 9)
+  }
+
+  test("Part 2") {
+    import eu.sim642.adventofcode2017.Day1.Part2.captcha
+
+    assert(captcha("1212") == 6)
+    assert(captcha("1221") == 0)
+    assert(captcha("123425") == 4)
+    assert(captcha("123123") == 12)
+    assert(captcha("12131415") == 4)
   }
 
 }
