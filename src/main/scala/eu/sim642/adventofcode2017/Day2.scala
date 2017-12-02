4e271d811f20e0f60328876fd38dd10f731cc0ab
Simmo Saan <simmo.saan@gmail.com>
2017-12-02 10:14:05 +0200

    Solve 2017 day 2 part 2

--- src/main/scala/eu/sim642/adventofcode2017/Day2.scala
+++ src/main/scala/eu/sim642/adventofcode2017/Day2.scala
@@ -2,18 +2,42 @@ package eu.sim642.adventofcode2017
 
 object Day2 {
 
-  def rowChecksum(row: Seq[Int]): Int = row.max - row.min
+  trait Part {
+    def rowChecksum(row: Seq[Int]): Int
 
-  def checksum(spreadsheet: Seq[Seq[Int]]): Int = spreadsheet.map(rowChecksum).sum
+    def checksum(spreadsheet: Seq[Seq[Int]]): Int = spreadsheet.map(rowChecksum).sum
 
-  def checksum(s: String): Int = {
-    val spreadsheet = s.trim.split('\n').toSeq.map(_.trim.split("\\s+").toSeq.map(_.toInt))
-    checksum(spreadsheet)
+    def checksum(s: String): Int = {
+      val spreadsheet = s.trim.split('\n').toSeq.map(_.trim.split("\\s+").toSeq.map(_.toInt))
+      checksum(spreadsheet)
+    }
+  }
+
+  object Part1 extends Part {
+    override def rowChecksum(row: Seq[Int]): Int = row.max - row.min
+  }
+
+  object Part2 extends Part {
+    override def rowChecksum(row: Seq[Int]): Int = {
+      val sortedRow = row.sorted.reverse
+      val dividePairs = for {
+        i <- 0 until sortedRow.size
+        j <- (i + 1) until sortedRow.size
+        p = sortedRow(i)
+        q = sortedRow(j)
+        if p % q == 0
+      } yield (p, q)
+
+      require(dividePairs.size == 1)
+      val (p, q) = dividePairs.head
+      p / q
+    }
   }
 
   val input: String = io.Source.fromInputStream(getClass.getResourceAsStream("day2.txt")).mkString.trim
 
   def main(args: Array[String]): Unit = {
-    println(checksum(input))
+    println(Part1.checksum(input))
+    println(Part2.checksum(input))
   }
 }
--- src/test/scala/eu/sim642/adventofcode2017/Day2Test.scala
+++ src/test/scala/eu/sim642/adventofcode2017/Day2Test.scala
@@ -1,9 +1,24 @@
 package eu.sim642.adventofcode2017
 
+import eu.sim642.adventofcode2017.Day2._
 import org.scalatest.FunSuite
 
 class Day2Test extends FunSuite {
+
   test("Part 1 example") {
-    assert(Day2.checksum("5 1 9 5\n7 5 3\n2 4 6 8") == 18)
+    assert(Part1.checksum("5 1 9 5\n7 5 3\n2 4 6 8") == 18)
   }
+
+  test("Part 1 input answer") {
+    assert(Part1.checksum(input) == 51833)
+  }
+
+  test("Part 2 example") {
+    assert(Part2.checksum("5 9 2 8\n9 4 7 3\n3 8 6 5") == 9)
+  }
+
+  test("Part 2 input answer") {
+    assert(Part2.checksum(input) == 288)
+  }
+
 }
