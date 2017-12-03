4486bffe28f860e405d37f9bdd9538cb7b456fad
Simmo Saan <simmo.saan@gmail.com>
2017-12-03 11:28:30 +0200

    Refactor 2017 day 3 part 1

--- src/main/scala/eu/sim642/adventofcode2017/Day3.scala
+++ src/main/scala/eu/sim642/adventofcode2017/Day3.scala
@@ -17,29 +17,32 @@ object Day3 {
   }
 
   def squarePos(square: Int): Pos = {
+    def nextOddSqrt(n: Int): Int = {
+      /*val odds = Stream.from(1, 2)
+      odds.find(x => x * x >= n).get*/
+      val nextSqrt = Math.sqrt(n).ceil.toInt
+      nextSqrt + (if (nextSqrt % 2 == 0) 1 else 0)
+    }
+
     if (square == 1)
       Pos(0, 0)
     else {
-      val odds = Stream.from(1, 2)
-      val boundSize = odds.find(x => x * x >= square).get
-      val boundOffset = boundSize / 2
+      val boundSize = nextOddSqrt(square)
+
+      val innerSize = boundSize - 2
+      val innerOffset = innerSize / 2
+      val innerArea = innerSize * innerSize
+
       val edgeSize = boundSize - 1
-      val boundArea = boundSize * boundSize
-      val innerArea = boundArea - 4 * edgeSize
-      val innerOffset = boundOffset - 1
-
       val edgeSquare = square - innerArea - 1
+      val edgeOffset = edgeSquare % edgeSize
 
-      if ((0 until edgeSize).contains(edgeSquare)) // right
-        Pos(innerOffset + 1, -innerOffset + edgeSquare)
-      else if ((edgeSize until 2 * edgeSize).contains(edgeSquare)) // top
-        Pos(innerOffset - (edgeSquare - edgeSize), innerOffset + 1)
-      else if ((2 * edgeSize until 3 * edgeSize).contains(edgeSquare)) // left
-        Pos(-innerOffset - 1, innerOffset - (edgeSquare - 2 * edgeSize))
-      else if ((3 * edgeSize until 4 * edgeSize).contains(edgeSquare)) // bottom
-        Pos(-innerOffset + (edgeSquare - 3 * edgeSize), -innerOffset - 1)
-      else
-        throw new RuntimeException("impossible")
+      edgeSquare / edgeSize match {
+        case 0 => Pos(innerOffset + 1, -innerOffset + edgeOffset) // right
+        case 1 => Pos(innerOffset - edgeOffset, innerOffset + 1) // top
+        case 2 => Pos(-innerOffset - 1, innerOffset - edgeOffset) // left
+        case 3 => Pos(-innerOffset + edgeOffset, -innerOffset - 1) // bottom
+      }
     }
   }
 
