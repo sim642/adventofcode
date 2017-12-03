31b42051ff3f281975191968442aeb81eafea381
Simmo Saan <simmo.saan@gmail.com>
2017-12-03 11:10:14 +0200

    Refactor 2017 day 3

--- src/main/scala/eu/sim642/adventofcode2017/Day3.scala
+++ src/main/scala/eu/sim642/adventofcode2017/Day3.scala
@@ -4,82 +4,83 @@ import scala.collection.{AbstractIterator, mutable}
 
 object Day3 {
 
-  def coord(square: Int): (Int, Int) = {
-    val odds = Stream.from(1, 2)
-    val boundSize = odds.find(x => x * x >= square).get
-    val boundOffset = boundSize / 2
-    val edgeSize = boundSize - 1
-    val boundArea = boundSize * boundSize
-    val innerArea = boundArea - 4 * edgeSize
-    val innerOffset = boundOffset - 1
+  case class Pos(x: Int, y: Int) {
+    def manhattanDistance(other: Pos): Int = (x - other.x).abs + (y - other.y).abs
 
-    val edgeSquare = square - innerArea - 1
-
-    if (edgeSquare < edgeSize) // right
-      (innerOffset + 1, -innerOffset + edgeSquare)
-    else if (edgeSize <= edgeSquare && edgeSquare < 2 * edgeSize) // top
-      (innerOffset - (edgeSquare - edgeSize), innerOffset + 1)
-    else if (2 * edgeSize <= edgeSquare && edgeSquare < 3 * edgeSize) // left
-      (-innerOffset - 1, innerOffset - (edgeSquare - 2 * edgeSize))
-    else if (3 * edgeSize <= edgeSquare && edgeSquare < 4 * edgeSize) // bottom
-      (-innerOffset + (edgeSquare - 3 * edgeSize), -innerOffset - 1)
-    else
-      throw new RuntimeException("impossible")
+    def +(other: Pos): Pos = Pos(x + other.x, y + other.y)
   }
 
-  def distance(square: Int): Int = {
-    val (x, y) = coord(square)
-    x.abs + y.abs
+  object Pos {
+    val axisOffsets: Seq[Pos] = Seq(Pos(0, 1), Pos(-1, 0), Pos(1, 0), Pos(0, -1))
+    val diagonalOffsets: Seq[Pos] = Seq(Pos(-1, 1), Pos(1, 1), Pos(-1, -1), Pos(1, -1))
+    val allOffsets: Seq[Pos] = axisOffsets ++ diagonalOffsets
   }
 
-  class SumIterator extends AbstractIterator[Int] {
+  def squarePos(square: Int): Pos = {
+    if (square == 1)
+      Pos(0, 0)
+    else {
+      val odds = Stream.from(1, 2)
+      val boundSize = odds.find(x => x * x >= square).get
+      val boundOffset = boundSize / 2
+      val edgeSize = boundSize - 1
+      val boundArea = boundSize * boundSize
+      val innerArea = boundArea - 4 * edgeSize
+      val innerOffset = boundOffset - 1
 
-    implicit class CoordPair(p: (Int, Int)) {
-      def +(q: (Int, Int)): (Int, Int) = (p._1 + q._1, p._2 + q._2)
+      val edgeSquare = square - innerArea - 1
+
+      if ((0 until edgeSize).contains(edgeSquare)) // right
+        Pos(innerOffset + 1, -innerOffset + edgeSquare)
+      else if ((edgeSize until 2 * edgeSize).contains(edgeSquare)) // top
+        Pos(innerOffset - (edgeSquare - edgeSize), innerOffset + 1)
+      else if ((2 * edgeSize until 3 * edgeSize).contains(edgeSquare)) // left
+        Pos(-innerOffset - 1, innerOffset - (edgeSquare - 2 * edgeSize))
+      else if ((3 * edgeSize until 4 * edgeSize).contains(edgeSquare)) // bottom
+        Pos(-innerOffset + (edgeSquare - 3 * edgeSize), -innerOffset - 1)
+      else
+        throw new RuntimeException("impossible")
     }
+  }
 
-    private val sums = mutable.Map[(Int, Int), Int]()
+  def steps(square: Int): Int = {
+    squarePos(square) manhattanDistance Pos(0, 0)
+  }
+
+  class SpiralSumIterator extends AbstractIterator[Int] {
+
+    private val sums = mutable.Map[Pos, Int]()
     private var square = 1
-
-    private val offsetCoords = Seq(
-      (-1, 1), (0, 1), (1, 1),
-      (-1, 0), (1, 0),
-      (-1, -1), (0, -1), (1, -1)
-    )
 
     override def hasNext: Boolean = true
 
     override def next(): Int = {
-      val squareCoord = coord(square)
-      if (square == 1) {
-        sums += squareCoord -> 1
-        square += 1
+      val pos = squarePos(square)
+      val sum = if (square == 1)
         1
-      }
       else {
-        val neighSums = for {
-          offsetCoord <- offsetCoords
-          coord = squareCoord + offsetCoord
-          if sums.contains(coord)
-        } yield sums(coord)
-
-        val sum = neighSums.sum
-        sums += squareCoord -> sum
-        square += 1
-        sum
+        val adjacentSums = for {
+          offset <- Pos.allOffsets
+          offsetPos = pos + offset
+        } yield sums.getOrElse(offsetPos, 0)
+        adjacentSums.sum
       }
+
+      sums += pos -> sum
+      square += 1
+      sum
     }
   }
 
-  def sumLarger(input: Int): Int = {
-    val it = new SumIterator
+  def spiralSumLarger(input: Int): Int = {
+    val it = new SpiralSumIterator
     it.find(x => x > input).get
   }
 
   val input: Int = 289326
 
   def main(args: Array[String]): Unit = {
-    println(distance(input))
-    println(sumLarger(input))
+    println(steps(input))
+    println(spiralSumLarger(input))
   }
 }
--- src/test/scala/eu/sim642/adventofcode2017/Day3Test.scala
+++ src/test/scala/eu/sim642/adventofcode2017/Day3Test.scala
@@ -7,75 +7,74 @@ import org.scalatest.prop.PropertyChecks
 
 class Day3Test extends FunSuite with PropertyChecks {
 
-  test("coord") {
+  test("squarePos") {
     val coords = Table(
-      ("square", "expectedCoord"),
-      (1,       (0, 0)),
+      ("square", "pos"),
+      (1,       Pos(0, 0)),
 
-      (2,       (1, 0)),
-      (3,       (1, 1)),
-      (4,       (0, 1)),
-      (5,       (-1, 1)),
-      (6,       (-1, 0)),
-      (7,       (-1, -1)),
-      (8,       (0, -1)),
-      (9,       (1, -1)),
+      (2,       Pos(1, 0)),
+      (3,       Pos(1, 1)),
+      (4,       Pos(0, 1)),
+      (5,       Pos(-1, 1)),
+      (6,       Pos(-1, 0)),
+      (7,       Pos(-1, -1)),
+      (8,       Pos(0, -1)),
+      (9,       Pos(1, -1)),
 
-      (10,      (2, -1)),
-      (11,      (2, 0)),
-      (12,      (2, 1)),
-      (13,      (2, 2)),
-      (14,      (1, 2)),
-      (15,      (0, 2)),
-      (16,      (-1, 2)),
-      (17,      (-2, 2)),
-      (18,      (-2, 1)),
-      (19,      (-2, 0)),
-      (20,      (-2, -1)),
-      (21,      (-2, -2)),
-      (22,      (-1, -2)),
-      (23,      (0, -2)),
-      (24,      (1, -2)),
-      (25,      (2, -2))
+      (10,      Pos(2, -1)),
+      (11,      Pos(2, 0)),
+      (12,      Pos(2, 1)),
+      (13,      Pos(2, 2)),
+      (14,      Pos(1, 2)),
+      (15,      Pos(0, 2)),
+      (16,      Pos(-1, 2)),
+      (17,      Pos(-2, 2)),
+      (18,      Pos(-2, 1)),
+      (19,      Pos(-2, 0)),
+      (20,      Pos(-2, -1)),
+      (21,      Pos(-2, -2)),
+      (22,      Pos(-1, -2)),
+      (23,      Pos(0, -2)),
+      (24,      Pos(1, -2)),
+      (25,      Pos(2, -2))
     )
 
-    forAll (coords) { (square, expectedCoord) =>
-      assert(coord(square) == expectedCoord)
+    forAll (coords) { (square, pos) =>
+      assert(squarePos(square) == pos)
     }
   }
 
   test("Part 1 example") {
-    assert(distance(1) == 0)
-    assert(distance(12) == 3)
-    assert(distance(23) == 2)
-    assert(distance(1024) == 31)
+    assert(steps(1) == 0)
+    assert(steps(12) == 3)
+    assert(steps(23) == 2)
+    assert(steps(1024) == 31)
   }
 
   test("Part 1 input answer") {
-    assert(distance(input) == 419)
+    assert(steps(input) == 419)
   }
 
-  test("SumIterator") {
+  test("SpiralSumIterator") {
     val sums = Table("sum", 1, 1, 2, 4, 5, 10, 11, 23, 25, 26, 54, 57, 59, 122, 133, 142, 147, 304, 330, 351, 362, 747, 806)
-    val it = new SumIterator
+    val it = new SpiralSumIterator
     forAll (sums) { sum =>
       assert(it.next() == sum)
     }
   }
 
-  test("sumLarger") {
+  test("spiralSumLarger") {
     forAll (Gen.choose(0, 1000000)) { (input: Int) =>
-      println(input)
-      val sum = sumLarger(input)
+      val sum = spiralSumLarger(input)
 
       assert(sum > input)
 
-      val it = new SumIterator
+      val it = new SpiralSumIterator
       assert(it.contains(sum))
     }
   }
 
   test("Part 2 input answer") {
-    assert(sumLarger(input) == 295229)
+    assert(spiralSumLarger(input) == 295229)
   }
 }
