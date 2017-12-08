a14730559355fd6f2b9cb9c5f7086d81e1f4f790
Simmo Saan <simmo.saan@gmail.com>
2017-12-08 08:12:14 +0200

    Solve 2017 day 8 part 1

---
+++ src/main/scala/eu/sim642/adventofcode2017/Day8.scala
@@ -0,0 +1,88 @@
+package eu.sim642.adventofcode2017
+
+object Day8 {
+
+  type Register = String
+
+  sealed trait Operation
+  case class Inc(amount: Int) extends Operation
+  case class Dec(amount: Int) extends Operation
+
+  sealed trait Comparison
+  case object Eq extends Comparison
+  case object NotEq extends Comparison
+  case object Less extends Comparison
+  case object LessEq extends Comparison
+  case object Greater extends Comparison
+  case object GreaterEq extends Comparison
+
+  case class Condition(register: Register, comparison: Comparison, amount: Int)
+
+  case class Instruction(register: Register, operation: Operation, condition: Condition)
+
+  private val instructionRegex = """(\w+) (inc|dec) (-?\d+) if (\w+) (==|!=|<|<=|>|>=) (-?\d+)""".r
+
+  def parseInstruction(instructionStr: String): Instruction = instructionStr match {
+    case instructionRegex(register, op, amount, conditionRegister, comp, conditionAmount) =>
+      val operation = op match {
+        case "inc" => Inc(amount.toInt)
+        case "dec" => Dec(amount.toInt)
+      }
+
+      val comparison = comp match {
+        case "==" => Eq
+        case "!=" => NotEq
+        case "<" => Less
+        case "<=" => LessEq
+        case ">" => Greater
+        case ">=" => GreaterEq
+      }
+
+      Instruction(register, operation, Condition(conditionRegister, comparison, conditionAmount.toInt))
+  }
+
+  type Instructions = List[Instruction]
+
+  def parseInstructions(instructionsStr: String): Instructions = instructionsStr.lines.map(parseInstruction).toList
+
+  type Registers = Map[Register, Int]
+
+  def evalCondition(condition: Condition, registers: Registers): Boolean = {
+    val registerAmount = registers(condition.register)
+    condition.comparison match {
+      case Eq => registerAmount == condition.amount
+      case NotEq => registerAmount != condition.amount
+      case Less => registerAmount < condition.amount
+      case LessEq => registerAmount <= condition.amount
+      case Greater => registerAmount > condition.amount
+      case GreaterEq => registerAmount >= condition.amount
+    }
+  }
+
+  def execOperation(operation: Operation, registerAmount: Int): Int = operation match {
+    case Inc(amount) => registerAmount + amount
+    case Dec(amount) => registerAmount - amount
+  }
+
+  def run(instructions: Instructions): Registers = {
+    instructions.foldLeft(Map.empty.withDefaultValue(0): Registers) { (registers, instruction) =>
+      if (evalCondition(instruction.condition, registers))
+        registers.updated(instruction.register, execOperation(instruction.operation, registers(instruction.register)))
+      else
+        registers
+    }
+  }
+
+  def largestValue(instructions: Instructions): Int = {
+    val registers = run(instructions)
+    registers.values.max
+  }
+
+  def largestValue(instructionsStr: String): Int = largestValue(parseInstructions(instructionsStr))
+
+  lazy val input: String = io.Source.fromInputStream(getClass.getResourceAsStream("day8.txt")).mkString.trim
+
+  def main(args: Array[String]): Unit = {
+    println(largestValue(input))
+  }
+}
---
+++ src/test/scala/eu/sim642/adventofcode2017/Day8Test.scala
@@ -0,0 +1,27 @@
+package eu.sim642.adventofcode2017
+
+import Day8._
+import org.scalatest.FunSuite
+
+class Day8Test extends FunSuite {
+
+  test("parseInstruction") {
+    assert(parseInstruction("b inc 5 if a > 1") == Instruction("b", Inc(5), Condition("a", Greater, 1)))
+    assert(parseInstruction("a inc 1 if b < 5") == Instruction("a", Inc(1), Condition("b", Less, 5)))
+    assert(parseInstruction("c dec -10 if a >= 1") == Instruction("c", Dec(-10), Condition("a", GreaterEq, 1)))
+    assert(parseInstruction("c inc -20 if c == 10") == Instruction("c", Inc(-20), Condition("c", Eq, 10)))
+  }
+
+  test("Part 1 example") {
+    val instructionsStr = """b inc 5 if a > 1
+                            |a inc 1 if b < 5
+                            |c dec -10 if a >= 1
+                            |c inc -20 if c == 10""".stripMargin
+    assert(largestValue(instructionsStr) == 1)
+  }
+
+  test("Part 1 input answer") {
+    assert(largestValue(input) == 5752)
+  }
+
+}
