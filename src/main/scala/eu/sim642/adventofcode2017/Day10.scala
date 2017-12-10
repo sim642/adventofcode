2fd05a4e8dc53505bde1a83dffb6a15be42c0880
Simmo Saan <simmo.saan@gmail.com>
2017-12-10 13:01:35 +0200

    Solve 2017 day 10 part 2

--- src/main/scala/eu/sim642/adventofcode2017/Day10.scala
+++ src/main/scala/eu/sim642/adventofcode2017/Day10.scala
@@ -20,10 +20,25 @@ object Day10 {
 
   def simulate(knotState: KnotState, lengths: Seq[Int]): KnotState = lengths.foldLeft(knotState)(_.reverse(_))
 
+  def asciiLengths(input: String): Seq[Int] = input.map(_.toInt) ++ Seq(17, 31, 73, 47, 23)
+
+  def simulate64(initialKnotState: KnotState, lengths: Seq[Int]): KnotState = {
+    (0 until 64).foldLeft(initialKnotState) { (knotState, round) =>
+      simulate(knotState, lengths)
+    }
+  }
+
+  def sparse2dense(sparse: Seq[Int]): Seq[Int] = sparse.grouped(16).map(_.reduce(_ ^ _)).toSeq
+
+  def mkHexString(dense: Seq[Int]): String = dense.map(_.formatted("%02x")).mkString
+
+  def hash2(input: String): String = mkHexString(sparse2dense(simulate64(KnotState(), asciiLengths(input)).elems))
+
   lazy val input: String = io.Source.fromInputStream(getClass.getResourceAsStream("day10.txt")).mkString.trim
   lazy val inputLengths: Seq[Int] = input.split(",").toSeq.map(_.toInt)
 
   def main(args: Array[String]): Unit = {
     println(simulate(KnotState(), inputLengths).hash)
+    println(hash2(input))
   }
 }
--- src/test/scala/eu/sim642/adventofcode2017/Day10Test.scala
+++ src/test/scala/eu/sim642/adventofcode2017/Day10Test.scala
@@ -31,4 +31,30 @@ class Day10Test extends FunSuite with PropertyChecks {
   test("Part 1 input answer") {
     assert(simulate(KnotState(), inputLengths).hash == 46600)
   }
+
+  test("Part 2 example lengths") {
+    assert(asciiLengths("1,2,3") == Seq(49,44,50,44,51,17,31,73,47,23))
+  }
+
+  test("Part 2 example dense hash") {
+    assert(sparse2dense(Seq(65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22)) == Seq(64))
+  }
+
+  test("Part 2 example hex string") {
+    assert(mkHexString(Seq(64, 7, 255)) == "4007ff")
+  }
+
+  test("Part 2 examples") {
+    val hashes = Table(
+      ("input", "hash"),
+      ("", "a2582a3a0e66e6e86e3812dcb672a272"),
+      ("AoC 2017", "33efeb34ea91902bb2f59c9920caa6cd"),
+      ("1,2,3", "3efbe78a8d82f29979031a4aa0b16a9d"),
+      ("1,2,4", "63960835bcdc130f0b66d7ff4f6a5a8e")
+    )
+
+    forAll (hashes) { (input, hash) =>
+      assert(hash2(input) == hash)
+    }
+  }
 }
