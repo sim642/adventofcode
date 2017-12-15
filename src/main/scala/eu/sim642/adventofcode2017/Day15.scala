d50dc67f577cbd16f261c1f4234e21cbfe35b9fc
Simmo Saan <simmo.saan@gmail.com>
2017-12-15 16:00:31 +0200

    Solve 2017 day 15 part 2

--- src/main/scala/eu/sim642/adventofcode2017/Day15.scala
+++ src/main/scala/eu/sim642/adventofcode2017/Day15.scala
@@ -22,6 +22,20 @@ object Day15 {
     new GeneratorA(startA).zip(new GeneratorB(startB)).take(pairs).count({ case (a, b) => (a & 0xFFFF) == (b & 0xFFFF) })
   }
 
+  class DelegateIterator[A](it: Iterator[A]) extends AbstractIterator[A] {
+    override def hasNext: Boolean = it.hasNext
+    override def next(): A = it.next()
+  }
+
+  class MultipleIterator(it: Iterator[Int], multiple: Int) extends DelegateIterator(it.filter(_ % multiple == 0))
+
+  class MultipleGeneratorA(start: Int) extends MultipleIterator(new GeneratorA(start), 4)
+  class MultipleGeneratorB(start: Int) extends MultipleIterator(new GeneratorB(start), 8)
+
+  def multipleMatchesCount(startA: Int, startB: Int, pairs: Int = 5000000): Int = {
+    new MultipleGeneratorA(startA).zip(new MultipleGeneratorB(startB)).take(pairs).count({ case (a, b) => (a & 0xFFFF) == (b & 0xFFFF) })
+  }
+
   /*private val inputRegex =
     """Generator A starts with (\d+)\n
       |Generator B starts with (\d+)""".r
@@ -34,5 +48,6 @@ object Day15 {
 
   def main(args: Array[String]): Unit = {
     println(matchesCount(inputStartA, inputStartB))
+    println(multipleMatchesCount(inputStartA, inputStartB))
   }
 }
--- src/test/scala/eu/sim642/adventofcode2017/Day15Test.scala
+++ src/test/scala/eu/sim642/adventofcode2017/Day15Test.scala
@@ -27,5 +27,39 @@ class Day15Test extends FunSuite with PropertyChecks {
 
   test("Part 1 example matches count") {
     assert(matchesCount(65, 8921, 5) == 1)
+    assert(matchesCount(65, 8921) == 588)
+  }
+
+  test("Part 1 input answer") {
+    assert(matchesCount(inputStartA, inputStartB) == 569)
+  }
+
+  test("Part 2 example generators") {
+    val genA = new MultipleGeneratorA(65)
+    val genB = new MultipleGeneratorB(8921)
+
+    val values = Table(
+      ("a", "b"),
+      (1352636452, 1233683848),
+      (1992081072, 862516352),
+      (530830436, 1159784568),
+      (1980017072, 1616057672),
+      (740335192, 412269392)
+    )
+
+    forAll (values) { (a, b) =>
+      assert(genA.next() == a)
+      assert(genB.next() == b)
+    }
+  }
+
+  test("Part 2 example matches count") {
+    assert(multipleMatchesCount(65, 8921, 1055) == 0)
+    assert(multipleMatchesCount(65, 8921, 1056) == 1)
+    assert(multipleMatchesCount(65, 8921) == 309)
+  }
+
+  test("Part 2 input answer") {
+    assert(multipleMatchesCount(inputStartA, inputStartB) == 298)
   }
 }
