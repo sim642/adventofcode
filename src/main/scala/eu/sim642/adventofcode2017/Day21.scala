af152221640459d0842e3a8e8a4d0a0c6900a79c
Simmo Saan <simmo.saan@gmail.com>
2017-12-21 22:50:23 +0200

    Solve 2017 day 21 part 1

---
+++ src/main/scala/eu/sim642/adventofcode2017/Day21.scala
@@ -0,0 +1,79 @@
+package eu.sim642.adventofcode2017
+
+object Day21 {
+
+  type Grid[A] = Vector[Vector[A]]
+
+  implicit class GridOps[A](grid: Grid[A]) {
+    def rotateCW: Grid[A] = {
+      val h = grid.size
+      val w = grid(0).size
+
+      /*
+      1 2 3
+      4 5 6
+
+      4 1
+      5 2
+      6 3
+       */
+
+      Vector.tabulate(w, h)((y, x) => grid(h - 1 - x)(y))
+    }
+
+    def flipV: Grid[A] = grid.reverse
+
+    def rotations: Set[Grid[A]] = (1 to 3).scanLeft(grid)({ case (acc, _) => acc.rotateCW }).toSet
+
+    def symmetries: Set[Grid[A]] = grid.rotations ++ grid.flipV.rotations
+
+    def groupedGrid(groupSize: Int): Grid[Grid[A]] =
+      grid.grouped(groupSize).map(_.map(_.grouped(groupSize).toVector).transpose).toVector
+
+    def mapGrid[B](f: A => B): Grid[B] = grid.map(_.map(f))
+
+    def flattenGrid[B](implicit asGrid: A => Grid[B]): Grid[B] =
+      grid.mapGrid(asGrid).map(_.transpose.map(_.flatten)).flatten
+
+    def countGrid(p: A => Boolean): Int = grid.map(_.count(p)).sum
+  }
+
+
+  type Pattern = Grid[Boolean]
+
+  def parsePattern(str: String, delimiter: Char = '/'): Pattern = str.split(delimiter).map(_.map(_ == '#').toVector).toVector
+
+  private val ruleRegex = """([.#/]*) => ([.#/]*)""".r
+
+  def parseRule(str: String): (Pattern, Pattern) = str match {
+    case ruleRegex(from, to) => parsePattern(from) -> parsePattern(to)
+  }
+
+  def parseRules(input: String): Map[Pattern, Pattern] = input.lines.map(parseRule).toMap
+
+
+  val initialPattern = parsePattern(
+    """.#.
+      |..#
+      |###""".stripMargin, '\n')
+
+  def stepPattern(rules: Map[Pattern, Pattern])(pattern: Pattern): Pattern = {
+    val groupSize = (2 to 3).find(pattern.size % _ == 0).get
+    pattern.groupedGrid(groupSize).mapGrid(rules).flattenGrid
+  }
+
+  def iteratePattern(rules: Map[Pattern, Pattern]): Iterator[Pattern] = {
+    val completeRules = rules.flatMap({ case (from, to) => from.symmetries.map(_ -> to)})
+    Iterator.iterate(initialPattern)(stepPattern(completeRules))
+  }
+
+  def countOn(rules: Map[Pattern, Pattern], iteration: Int): Int = iteratePattern(rules).drop(iteration).next().countGrid(b => b)
+
+  def countOn(input: String, iteration: Int = 5): Int = countOn(parseRules(input), iteration)
+
+  lazy val input: String = io.Source.fromInputStream(getClass.getResourceAsStream("day21.txt")).mkString.trim
+
+  def main(args: Array[String]): Unit = {
+    println(countOn(input))
+  }
+}
---
+++ src/test/scala/eu/sim642/adventofcode2017/Day21Test.scala
@@ -0,0 +1,121 @@
+package eu.sim642.adventofcode2017
+
+import Day21._
+import org.scalatest.FunSuite
+import org.scalatest.prop.PropertyChecks
+
+class Day21Test extends FunSuite with PropertyChecks {
+
+  val exampleRules = """../.# => ##./#../...
+                       |.#./..#/### => #..#/..../..../#..#""".stripMargin
+
+  test("parsePattern") {
+    assert(parsePattern("../.#") == Vector(
+      Vector(false, false),
+      Vector(false, true)
+    ))
+
+    assert(parsePattern(".#./..#/###") == Vector(
+      Vector(false, true, false),
+      Vector(false, false, true),
+      Vector(true, true, true)
+    ))
+
+    assert(parsePattern("#..#/..../#..#/.##.") == Vector(
+      Vector(true, false, false, true),
+      Vector(false, false, false, false),
+      Vector(true, false, false, true),
+      Vector(false, true, true, false)
+    ))
+  }
+
+  test("Part 1 example symmetries") {
+    val grid = parsePattern(
+      """.#.
+        |..#
+        |###""".stripMargin, '\n')
+    val symmetries = grid.symmetries
+
+    assert(symmetries.contains(grid))
+    assert(symmetries.contains(parsePattern(
+      """.#.
+        |#..
+        |###""".stripMargin, '\n')))
+    assert(symmetries.contains(parsePattern(
+      """#..
+        |#.#
+        |##.""".stripMargin, '\n')))
+    assert(symmetries.contains(parsePattern(
+      """###
+        |..#
+        |.#.""".stripMargin, '\n')))
+  }
+
+  test("parseRule") {
+    assert(parseRule("../.# => ##./#../...") == (parsePattern("../.#") -> parsePattern("##./#../...")))
+    assert(parseRule(".#./..#/### => #..#/..../..../#..#") == (parsePattern(".#./..#/###") -> parsePattern("#..#/..../..../#..#")))
+  }
+
+  test("groupedGrid") {
+    val pattern = parsePattern(
+      """#..#
+        |....
+        |....
+        |#..#""".stripMargin, '\n')
+
+    assert(pattern.groupedGrid(2) == Vector(
+      Vector(parsePattern("#./.."), parsePattern(".#/..")),
+      Vector(parsePattern("../#."), parsePattern("../.#"))
+    ))
+  }
+
+  test("flattenGrid") {
+    val pattern = parsePattern("##./#../...")
+    val patternGrid = Vector(
+      Vector(pattern, pattern),
+      Vector(pattern, pattern)
+    )
+
+    assert(patternGrid.flattenGrid == parsePattern("""##.##.
+                                                     |#..#..
+                                                     |......
+                                                     |##.##.
+                                                     |#..#..
+                                                     |......""".stripMargin, '\n'))
+  }
+
+  test("Part 1 example patterns") {
+    val patterns = Table(
+      "pattern",
+
+      """.#.
+        |..#
+        |###""".stripMargin,
+
+      """#..#
+        |....
+        |....
+        |#..#""".stripMargin,
+
+      """##.##.
+        |#..#..
+        |......
+        |##.##.
+        |#..#..
+        |......""".stripMargin
+    )
+
+    val it = iteratePattern(parseRules(exampleRules))
+    forAll (patterns) { pattern =>
+      assert(it.next() == parsePattern(pattern, '\n'))
+    }
+  }
+
+  test("Part 1 example") {
+    assert(countOn(exampleRules, 2) == 12)
+  }
+
+  test("Part 1 input answer") {
+    assert(countOn(input) == 142)
+  }
+}
