57b965a9b0001062c23327cb68e78e4ef2519279
Simmo Saan <simmo.saan@gmail.com>
2017-12-22 10:53:09 +0200

    Solve 2017 day 22 part 2

--- src/main/scala/eu/sim642/adventofcode2017/Day22.scala
+++ src/main/scala/eu/sim642/adventofcode2017/Day22.scala
@@ -7,39 +7,84 @@ object Day22 {
   implicit class OffsetPos(offset: Pos) {
     def right: Pos = Pos(offset.y, -offset.x)
     def left: Pos = Pos(-offset.y, offset.x)
+    def reverse: Pos = Pos(-offset.x, -offset.y)
   }
 
-  case class InfectionState(infected: Set[Pos], pos: Pos = Pos(0, 0), prevOffset: Pos = Pos(0, 1)) {
-    def next: InfectionState = {
-      if (infected.contains(pos)) {
-        val offset = prevOffset.right
-        InfectionState(infected - pos, pos + offset, offset)
-      }
-      else {
-        val offset = prevOffset.left
-        InfectionState(infected + pos, pos + offset, offset)
+  trait Part {
+    sealed trait Status
+    case object Clean extends Status
+    case object Infected extends Status
+
+    case class InfectionState(statuses: Map[Pos, Status], pos: Pos = Pos(0, 0), prevOffset: Pos = Pos(0, 1)) {
+      def posStatus: Status = statuses(pos)
+
+      def next(statuses: Map[Pos, Status], offset: Pos) = InfectionState(statuses, pos + offset, offset)
+    }
+
+    protected def nextState(state: InfectionState): InfectionState
+
+    def parseMap(input: String): Map[Pos, Status] = {
+      val grid = input.lines.map(_.map(_ == '#').toVector).toVector
+      val (h, w) = (grid.size, grid(0).size)
+
+      (for {
+        y <- grid.indices
+        x <- grid(y).indices
+        if grid(y)(x)
+      } yield Pos(x - w / 2, -(y - h / 2)) -> Infected).toMap.withDefaultValue(Clean)
+    }
+
+    def iterateBursts(input: String): Iterator[InfectionState] = Iterator.iterate(InfectionState(parseMap(input)))(nextState)
+
+    protected val defaultBursts: Int
+    protected val toBeInfectedStatus: Status
+
+    def infectionBursts(input: String, bursts: Int = defaultBursts): Int = iterateBursts(input).take(bursts).count(_.posStatus == toBeInfectedStatus)
+  }
+
+  object Part1 extends Part {
+    override protected def nextState(state: InfectionState): InfectionState = {
+      val InfectionState(statuses, pos, prevOffset) = state
+
+      state.posStatus match {
+        case Clean =>
+          state.next(statuses + (pos -> Infected), prevOffset.left)
+        case Infected =>
+          state.next(statuses - pos, prevOffset.right)
       }
     }
+
+    override protected val defaultBursts: Int = 10000
+    override protected val toBeInfectedStatus: Status = Clean
   }
 
-  def parseMap(input: String): Set[Pos] = {
-    val grid = input.lines.map(_.map(_ == '#').toVector).toVector
-    val (h, w) = (grid.size, grid(0).size)
+  object Part2 extends Part {
+    case object Weakened extends Status
+    case object Flagged extends Status
 
-    (for {
-      y <- grid.indices
-      x <- grid(y).indices
-      if grid(y)(x)
-    } yield Pos(x - w / 2, -(y - h / 2))).toSet
+    override protected def nextState(state: InfectionState): InfectionState = {
+      val InfectionState(statuses, pos, prevOffset) = state
+
+      state.posStatus match {
+        case Clean =>
+          state.next(statuses + (pos -> Weakened), prevOffset.left)
+        case Weakened =>
+          state.next(statuses + (pos -> Infected), prevOffset)
+        case Infected =>
+          state.next(statuses + (pos -> Flagged), prevOffset.right)
+        case Flagged =>
+          state.next(statuses - pos, prevOffset.reverse)
+      }
+    }
+
+    override protected val defaultBursts: Int = 10000000
+    override protected val toBeInfectedStatus: Status = Weakened
   }
-
-  def iterateBursts(input: String): Iterator[InfectionState] = Iterator.iterate(InfectionState(parseMap(input)))(_.next)
-
-  def infectionBursts(input: String, bursts: Int = 10000): Int = iterateBursts(input).take(bursts).count(state => !state.infected.contains(state.pos))
 
   lazy val input: String = io.Source.fromInputStream(getClass.getResourceAsStream("day22.txt")).mkString.trim
 
   def main(args: Array[String]): Unit = {
-    println(infectionBursts(input))
+    println(Part1.infectionBursts(input))
+    println(Part2.infectionBursts(input))
   }
 }
--- src/test/scala/eu/sim642/adventofcode2017/Day22Test.scala
+++ src/test/scala/eu/sim642/adventofcode2017/Day22Test.scala
@@ -12,16 +12,18 @@ class Day22Test extends FunSuite with PropertyChecks {
                        |...""".stripMargin
 
   test("Part 1 example states") {
+    import Part1._
+
     val states = Table(
       "state",
-      InfectionState(Set(Pos(-1, 0), Pos(1, 1)), Pos(0, 0), Pos(0, 1)),
-      InfectionState(Set(Pos(-1, 0), Pos(1, 1), Pos(0, 0)), Pos(-1, 0), Pos(-1, 0)),
-      InfectionState(Set(Pos(1, 1), Pos(0, 0)), Pos(-1, 1), Pos(0, 1)),
-      InfectionState(Set(Pos(1, 1), Pos(0, 0), Pos(-1, 1)), Pos(-2, 1), Pos(-1, 0)),
-      InfectionState(Set(Pos(1, 1), Pos(0, 0), Pos(-1, 1), Pos(-2, 1)), Pos(-2, 0), Pos(0, -1)),
-      InfectionState(Set(Pos(1, 1), Pos(0, 0), Pos(-1, 1), Pos(-2, 1), Pos(-2, 0)), Pos(-1, 0), Pos(1, 0)),
-      InfectionState(Set(Pos(1, 1), Pos(0, 0), Pos(-1, 1), Pos(-2, 1), Pos(-2, 0), Pos(-1, 0)), Pos(-1, 1), Pos(0, 1)),
-      InfectionState(Set(Pos(1, 1), Pos(0, 0), Pos(-2, 1), Pos(-2, 0), Pos(-1, 0)), Pos(0, 1), Pos(1, 0))
+      InfectionState(Map(Pos(-1, 0) -> Infected, Pos(1, 1) -> Infected), Pos(0, 0), Pos(0, 1)),
+      InfectionState(Map(Pos(-1, 0) -> Infected, Pos(1, 1) -> Infected, Pos(0, 0) -> Infected), Pos(-1, 0), Pos(-1, 0)),
+      InfectionState(Map(Pos(1, 1) -> Infected, Pos(0, 0) -> Infected), Pos(-1, 1), Pos(0, 1)),
+      InfectionState(Map(Pos(1, 1) -> Infected, Pos(0, 0) -> Infected, Pos(-1, 1) -> Infected), Pos(-2, 1), Pos(-1, 0)),
+      InfectionState(Map(Pos(1, 1) -> Infected, Pos(0, 0) -> Infected, Pos(-1, 1) -> Infected, Pos(-2, 1) -> Infected), Pos(-2, 0), Pos(0, -1)),
+      InfectionState(Map(Pos(1, 1) -> Infected, Pos(0, 0) -> Infected, Pos(-1, 1) -> Infected, Pos(-2, 1) -> Infected, Pos(-2, 0) -> Infected), Pos(-1, 0), Pos(1, 0)),
+      InfectionState(Map(Pos(1, 1) -> Infected, Pos(0, 0) -> Infected, Pos(-1, 1) -> Infected, Pos(-2, 1) -> Infected, Pos(-2, 0) -> Infected, Pos(-1, 0) -> Infected), Pos(-1, 1), Pos(0, 1)),
+      InfectionState(Map(Pos(1, 1) -> Infected, Pos(0, 0) -> Infected, Pos(-2, 1) -> Infected, Pos(-2, 0) -> Infected, Pos(-1, 0) -> Infected), Pos(0, 1), Pos(1, 0))
     )
 
     val it = iterateBursts(exampleInput)
@@ -39,11 +41,50 @@ class Day22Test extends FunSuite with PropertyChecks {
     )
 
     forAll (burstInfections) { (bursts, infections) =>
-      assert(infectionBursts(exampleInput, bursts) == infections)
+      assert(Part1.infectionBursts(exampleInput, bursts) == infections)
     }
   }
 
   test("Part 1 input answer") {
-    assert(infectionBursts(input) == 5460)
+    assert(Part1.infectionBursts(input) == 5460)
+  }
+
+  test("Part 2 example states") {
+    import Part2._
+
+    val states = Table(
+      "state",
+      InfectionState(Map(Pos(-1, 0) -> Infected, Pos(1, 1) -> Infected), Pos(0, 0), Pos(0, 1)),
+      InfectionState(Map(Pos(-1, 0) -> Infected, Pos(1, 1) -> Infected, Pos(0, 0) -> Weakened), Pos(-1, 0), Pos(-1, 0)),
+      InfectionState(Map(Pos(-1, 0) -> Flagged, Pos(1, 1) -> Infected, Pos(0, 0) -> Weakened), Pos(-1, 1), Pos(0, 1)),
+
+      InfectionState(Map(Pos(-1, 0) -> Flagged, Pos(1, 1) -> Infected, Pos(0, 0) -> Weakened, Pos(-1, 1) -> Weakened), Pos(-2, 1), Pos(-1, 0)),
+      InfectionState(Map(Pos(-1, 0) -> Flagged, Pos(1, 1) -> Infected, Pos(0, 0) -> Weakened, Pos(-1, 1) -> Weakened, Pos(-2, 1) -> Weakened), Pos(-2, 0), Pos(0, -1)),
+      InfectionState(Map(Pos(-1, 0) -> Flagged, Pos(1, 1) -> Infected, Pos(0, 0) -> Weakened, Pos(-1, 1) -> Weakened, Pos(-2, 1) -> Weakened, Pos(-2, 0) -> Weakened), Pos(-1, 0), Pos(1, 0)),
+
+      InfectionState(Map(Pos(1, 1) -> Infected, Pos(0, 0) -> Weakened, Pos(-1, 1) -> Weakened, Pos(-2, 1) -> Weakened, Pos(-2, 0) -> Weakened), Pos(-2, 0), Pos(-1, 0)),
+      InfectionState(Map(Pos(1, 1) -> Infected, Pos(0, 0) -> Weakened, Pos(-1, 1) -> Weakened, Pos(-2, 1) -> Weakened, Pos(-2, 0) -> Infected), Pos(-3, 0), Pos(-1, 0)),
+    )
+
+    val it = iterateBursts(exampleInput)
+    forAll (states) { state =>
+      assert(it.next() == state)
+    }
+  }
+
+  test("Part 2 infection bursts") {
+    val burstInfections = Table(
+      ("bursts", "infections"),
+      (100, 26),
+      (10000000, 2511944)
+    )
+
+    forAll (burstInfections) { (bursts, infections) =>
+      assert(Part2.infectionBursts(exampleInput, bursts) == infections)
+    }
+  }
+
+  test("Part 2 input answer") {
+    assert(Part2.infectionBursts(input) == 2511702)
   }
 }
