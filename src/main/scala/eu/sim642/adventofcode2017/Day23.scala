9eb4a3530a5da82dcb36ab76b9553c1807e9a39b
Simmo Saan <simmo.saan@gmail.com>
2017-12-23 18:46:42 +0200

    Extract simulated and reverse engineered solutions of 2017 day 23

--- src/main/scala/eu/sim642/adventofcode2017/Day23.scala
+++ src/main/scala/eu/sim642/adventofcode2017/Day23.scala
@@ -1,114 +1,138 @@
 package eu.sim642.adventofcode2017
 
+import scala.util.matching.Regex
+
 object Day23 {
 
   type Register = Char
   type Integer = Long
 
-  sealed trait Value
-  case class RegisterValue(register: Register) extends Value
-  case class ConstValue(const: Integer) extends Value
+  def isPrime(n: Int): Boolean = n >= 2 && (2 to Math.sqrt(n).toInt).forall(n % _ != 0)
 
-  sealed trait Instruction
-  case class Set(x: Register, y: Value) extends Instruction
-  case class Sub(x: Register, y: Value) extends Instruction
-  case class Mul(x: Register, y: Value) extends Instruction
-  case class Jnz(x: Value, y: Value) extends Instruction
+  trait Solution {
+    def countMul(input: String): Int
+    def registerH(input: String): Integer
+  }
 
-  type Instructions = Vector[Instruction]
-  type Registers = Map[Register, Integer]
+  object SimulateSolution extends Solution {
 
-  case class AsmState(instructions: Instructions,
-                      pc: Int = 0,
-                      registers: Registers = Map.empty.withDefaultValue(0)) {
-    def get(value: Value): Integer = value match {
-      case RegisterValue(register) => registers(register)
-      case ConstValue(const) => const
+    sealed trait Value
+    case class RegisterValue(register: Register) extends Value
+    case class ConstValue(const: Integer) extends Value
+
+    sealed trait Instruction
+    case class Set(x: Register, y: Value) extends Instruction
+    case class Sub(x: Register, y: Value) extends Instruction
+    case class Mul(x: Register, y: Value) extends Instruction
+    case class Jnz(x: Value, y: Value) extends Instruction
+
+    type Instructions = Vector[Instruction]
+    type Registers = Map[Register, Integer]
+
+    case class AsmState(instructions: Instructions,
+                        pc: Int = 0,
+                        registers: Registers = Map.empty.withDefaultValue(0)) {
+      def get(value: Value): Integer = value match {
+        case RegisterValue(register) => registers(register)
+        case ConstValue(const) => const
+      }
+
+      def instruction: Instruction = instructions(pc)
+
+      def terminated: Boolean = !instructions.indices.contains(pc)
     }
 
-    def instruction: Instruction = instructions(pc)
+    private val instructionRegex = """([a-z]+) ([a-z]|-?\d+)(?: ([a-z]|-?\d+))?""".r
 
-    def terminated: Boolean = !instructions.indices.contains(pc)
-  }
-
-  private val instructionRegex = """([a-z]+) ([a-z]|-?\d+)(?: ([a-z]|-?\d+))?""".r
-
-  def parseValue(str: String): Value = {
-    val c = str.head
-    if (c.isLetter)
-      RegisterValue(c)
-    else
-      ConstValue(str.toLong)
-  }
-
-  def parseInstruction(str: String): Instruction = str match {
-    case instructionRegex(instruction, x, y) => instruction match {
-      case "set" => Set(x.head, parseValue(y))
-      case "sub" => Sub(x.head, parseValue(y))
-      case "mul" => Mul(x.head, parseValue(y))
-      case "jnz" => Jnz(parseValue(x), parseValue(y))
+    def parseValue(str: String): Value = {
+      val c = str.head
+      if (c.isLetter)
+        RegisterValue(c)
+      else
+        ConstValue(str.toLong)
     }
-  }
 
-  def parseInstructions(str: String): Instructions = str.lines.map(parseInstruction).toVector
-
-  def execSmallStep(state: AsmState): AsmState = {
-    val AsmState(instructions, pc, registers) = state
-
-    if (state.terminated)
-      state
-
-    state.instruction match {
-      case Set(x, y) => state.copy(registers = registers + (x -> state.get(y)), pc = pc + 1)
-      case Sub(x, y) => state.copy(registers = registers + (x -> (registers(x) - state.get(y))), pc = pc + 1)
-      case Mul(x, y) => state.copy(registers = registers + (x -> (registers(x) * state.get(y))), pc = pc + 1)
-      case Jnz(x, y) =>
-        if (state.get(x) != 0)
-          state.copy(pc = pc + state.get(y).toInt)
-        else
-          state.copy(pc = pc + 1)
+    def parseInstruction(str: String): Instruction = str match {
+      case instructionRegex(instruction, x, y) => instruction match {
+        case "set" => Set(x.head, parseValue(y))
+        case "sub" => Sub(x.head, parseValue(y))
+        case "mul" => Mul(x.head, parseValue(y))
+        case "jnz" => Jnz(parseValue(x), parseValue(y))
+      }
     }
-  }
 
-  trait Part {
-    def iterateSmallStep(instructions: Instructions): Iterator[AsmState]
-  }
+    def parseInstructions(str: String): Instructions = str.lines.map(parseInstruction).toVector
 
-  object Part1 extends Part {
-    override def iterateSmallStep(instructions: Instructions): Iterator[AsmState] = Iterator.iterate(AsmState(instructions))(execSmallStep)
+    def execSmallStep(state: AsmState): AsmState = {
+      val AsmState(instructions, pc, registers) = state
+
+      if (state.terminated)
+        state
+
+      state.instruction match {
+        case Set(x, y) => state.copy(registers = registers + (x -> state.get(y)), pc = pc + 1)
+        case Sub(x, y) => state.copy(registers = registers + (x -> (registers(x) - state.get(y))), pc = pc + 1)
+        case Mul(x, y) => state.copy(registers = registers + (x -> (registers(x) * state.get(y))), pc = pc + 1)
+        case Jnz(x, y) =>
+          if (state.get(x) != 0)
+            state.copy(pc = pc + state.get(y).toInt)
+          else
+            state.copy(pc = pc + 1)
+      }
+    }
+
+    def iterateSmallStep(instructions: Instructions): Iterator[AsmState] = Iterator.iterate(AsmState(instructions))(execSmallStep)
 
     def countMul(instructions: Instructions): Int = iterateSmallStep(instructions).takeWhile(!_.terminated).count(state => state.instruction match {
       case _: Mul => true
       case _ => false
     })
 
-    def countMul(input: String): Int = countMul(parseInstructions(input))
+    override def countMul(input: String): Int = countMul(parseInstructions(input))
+
+    override def registerH(input: String): Integer = ???
   }
 
-  object Part2 {
+  object ReverseEngineeredSolution extends Solution {
 
-    def sieve: Stream[Int] => Stream[Int] = {
-      case p #:: xs => xs.filterNot(_ % p == 0)
+    lazy val inputRegex: Regex = io.Source.fromInputStream(getClass.getResourceAsStream("day23/reverse_regex.txt")).mkString.trim.r
+
+    case class RangeParams(b: Int, bMul: Int, bAdd: Int, cAdd: Int, bInc: Int) {
+      def bRange: Range = {
+        val b2 = b * bMul + bAdd
+        val c2 = b2 + cAdd
+        b2 to c2 by bInc
+      }
     }
 
-    val primes: Stream[Int] = Stream.iterate(Stream.from(2))(sieve).map(_.head)
+    def parseParams(input: String): RangeParams = {
+      val m = inputRegex.findFirstMatchIn(input).get
+      RangeParams(
+        b = m.group("b").toInt,
+        bMul = m.group("bMul").toInt,
+        bAdd = m.group("bAdd").toInt,
+        cAdd = m.group("cAdd").toInt,
+        bInc = m.group("bInc").toInt
+      )
+    }
 
-    def countPrimes(b: Int, c: Int, bMul: Int, bAdd: Int, cAdd: Int, bInc: Int): Int = {
-      val b2 = b * bMul + bAdd
-      val c2 = b2 + cAdd
+    override def countMul(input: String): Int = {
+      val params: RangeParams = parseParams(input)
+      (params.b - 2) * (params.b - 2)
+    }
 
-      val ps = primes.dropWhile(_ < b2).takeWhile(_ <= c2).toSet
-      println(ps)
-      //cAdd / bInc - ps.count(_ % bInc == b2 % bInc)
-      ((b2 to c2 by bInc).toSet -- ps).size
+    override def registerH(input: String): Integer = {
+      val params: RangeParams = parseParams(input)
+      params.bRange.count(!isPrime(_))
     }
   }
-
 
   lazy val input: String = io.Source.fromInputStream(getClass.getResourceAsStream("day23.txt")).mkString.trim
 
   def main(args: Array[String]): Unit = {
-    println(Part1.countMul(input))
-    println(Part2.countPrimes(93, 93, 100, 100000, 17000, 17)) // 911
+    import ReverseEngineeredSolution._
+
+    println(countMul(input))
+    println(registerH(input))
   }
 }
--- src/test/scala/eu/sim642/adventofcode2017/Day23Test.scala
+++ src/test/scala/eu/sim642/adventofcode2017/Day23Test.scala
@@ -1,15 +1,34 @@
 package eu.sim642.adventofcode2017
 
 import Day23._
-import org.scalatest.FunSuite
+import eu.sim642.adventofcode2017.Day23Test._
+import org.scalatest.{FunSuite, Suites}
 
-class Day23Test extends FunSuite {
+class Day23Test extends Suites(
+  new BaseTest,
+  new SimulateSolutionTest,
+  new ReverseEngineeredSolutionTest
+)
 
-  test("Part 1 input answer") {
-    assert(Part1.countMul(input) == 8281)
+object Day23Test {
+
+  class BaseTest extends FunSuite {
+    test("isPrime") {
+      assert((0 to 30).filter(isPrime) == Seq(2, 3, 5, 7, 11, 13, 17, 19, 23, 29))
+    }
   }
 
-  test("Primes") {
-    assert(Part2.primes.take(10) == Seq(2, 3, 5, 7, 11, 13, 17, 19, 23, 29))
+  sealed abstract class SolutionTest(solution: Solution) extends FunSuite {
+    test("Part 1 input answer") {
+      assert(solution.countMul(input) == 8281)
+    }
+
+    test("Part 2 input answer") {
+      assert(solution.registerH(input) == 911)
+    }
   }
+
+  class SimulateSolutionTest extends SolutionTest(SimulateSolution)
+
+  class ReverseEngineeredSolutionTest extends SolutionTest(ReverseEngineeredSolution)
 }
