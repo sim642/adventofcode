62f7c0deb7659ae5501bb01ad02f0f76d06c3a8b
Simmo Saan <simmo.saan@gmail.com>
2017-12-24 11:36:14 +0200

    Solve 2017 day 24 part 1 & part 2

---
+++ src/main/scala/eu/sim642/adventofcode2017/Day24.scala
@@ -0,0 +1,58 @@
+package eu.sim642.adventofcode2017
+
+object Day24 {
+
+  case class Component(a: Int, b: Int) {
+
+    def contains(i: Int): Boolean = a == i || b == i
+
+    def other(i: Int): Int = if (a == i) b else a
+  }
+
+  private val componentRegex = """(\d+)/(\d+)""".r
+
+  def parseComponent(str: String): Component = str match {
+    case componentRegex(a, b) => Component(a.toInt, b.toInt)
+  }
+
+  def parseComponents(input: String): Seq[Component] = input.lines.map(parseComponent).toSeq
+
+  type Bridge = Seq[Component]
+
+  def validBridges(components: Seq[Component]): Set[Bridge] = {
+
+    def helper(components: Seq[Component], port: Int): Set[Bridge] = {
+      val portComponents = components.filter(_.contains(port))
+      if (portComponents.isEmpty)
+        Set(Seq())
+      else
+        (for {
+          component <- portComponents
+          newPort = component.other(port)
+          bridge <- Set(Seq()) ++ helper(components.diff(Seq(component)), newPort)
+        } yield component +: bridge).toSet
+    }
+
+    helper(components, 0)
+  }
+
+  def strongestBridgeStrength(components: Seq[Component]): Int = validBridges(components).map(_.map(c => c.a + c.b).sum).max
+
+  def strongestBridgeStrength(input: String): Int = strongestBridgeStrength(parseComponents(input))
+
+
+  def longestBridgeStrength(components: Seq[Component]): Int = {
+    val bridges = validBridges(components)
+    val maxLength = bridges.map(_.length).max
+    bridges.filter(_.length == maxLength).map(_.map(c => c.a + c.b).sum).max
+  }
+
+  def longestBridgeStrength(input: String): Int = longestBridgeStrength(parseComponents(input))
+
+  lazy val input: String = io.Source.fromInputStream(getClass.getResourceAsStream("day24.txt")).mkString.trim
+
+  def main(args: Array[String]): Unit = {
+    println(strongestBridgeStrength(input))
+    println(longestBridgeStrength(input))
+  }
+}
---
+++ src/test/scala/eu/sim642/adventofcode2017/Day24Test.scala
@@ -0,0 +1,48 @@
+package eu.sim642.adventofcode2017
+
+import Day24._
+import org.scalatest.FunSuite
+
+class Day24Test extends FunSuite {
+
+  val exampleInput = """0/2
+                       |2/2
+                       |2/3
+                       |3/4
+                       |3/5
+                       |0/1
+                       |10/1
+                       |9/10""".stripMargin
+
+  test("Part 1 example bridges") {
+    assert(validBridges(parseComponents(exampleInput)) == Set(
+      Seq(Component(0, 1)),
+      Seq(Component(0, 1), Component(10, 1)),
+      Seq(Component(0, 1), Component(10, 1), Component(9, 10)),
+      Seq(Component(0, 2)),
+      Seq(Component(0, 2), Component(2, 3)),
+      Seq(Component(0, 2), Component(2, 3), Component(3, 4)),
+      Seq(Component(0, 2), Component(2, 3), Component(3, 5)),
+      Seq(Component(0, 2), Component(2, 2)),
+      Seq(Component(0, 2), Component(2, 2), Component(2, 3)),
+      Seq(Component(0, 2), Component(2, 2), Component(2, 3), Component(3, 4)),
+      Seq(Component(0, 2), Component(2, 2), Component(2, 3), Component(3, 5))
+    ))
+  }
+
+  test("Part 1 example") {
+    assert(strongestBridgeStrength(exampleInput) == 31)
+  }
+
+  test("Part 1 input answer") {
+    assert(strongestBridgeStrength(input) == 2006)
+  }
+
+  test("Part 2 example") {
+    assert(longestBridgeStrength(exampleInput) == 19)
+  }
+
+  test("Part 2 input answer") {
+    assert(longestBridgeStrength(input) == 1994)
+  }
+}
