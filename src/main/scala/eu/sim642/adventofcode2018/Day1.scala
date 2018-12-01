9531911f6e8c0ea8c52ead8dc05b930b29242f62
Simmo Saan <simmo.saan@gmail.com>
2018-12-01 10:38:42 +0200

    Extract Seq cycling to be reusable

--- src/main/scala/eu/sim642/adventofcode2018/Day1.scala
+++ src/main/scala/eu/sim642/adventofcode2018/Day1.scala
@@ -5,8 +5,15 @@ import scala.collection.mutable
 object Day1 {
   def resultingFreq(freqChanges: Seq[Int]): Int = freqChanges.sum
 
+  implicit class CycleSeq[A](s: Seq[A]) {
+    def cycle: Iterator[A] = {
+      // https://stackoverflow.com/a/2099896
+      Iterator.continually(s).flatten
+    }
+  }
+
   def firstTwiceFreq(freqChanges: Seq[Int]): Int = {
-    val it = Iterator.continually(freqChanges).flatten.scanLeft(0)(_ + _)
+    val it = freqChanges.cycle.scanLeft(0)(_ + _)
     val prevFreqs = mutable.Set[Int]()
 
     var firstTwice: Option[Int] = None
@@ -18,7 +25,6 @@ object Day1 {
 
     firstTwice.get
   }
-
 
   lazy val input: String = io.Source.fromInputStream(getClass.getResourceAsStream("day1.txt")).mkString.trim
   lazy val inputFreqChanges: Seq[Int] = input.lines.map(_.toInt).toSeq
