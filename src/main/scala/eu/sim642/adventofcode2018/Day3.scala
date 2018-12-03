cea8bf0be6ad232271f93bea2f13f0bb0cff70bf
Simmo Saan <simmo.saan@gmail.com>
2018-12-03 14:22:18 +0200

    Solve 2018 day 3 part 2

--- src/main/scala/eu/sim642/adventofcode2018/Day3.scala
+++ src/main/scala/eu/sim642/adventofcode2018/Day3.scala
@@ -1,7 +1,6 @@
 package eu.sim642.adventofcode2018
 
 import scala.collection.mutable
-import scala.language.implicitConversions
 
 object Day3 {
 
@@ -16,6 +15,7 @@ object Day3 {
   def parseRectangles(input: String): Seq[Rectangle] = input.lines.map(parseRectangle).toSeq
 
   def overlappingFabric(rects: Seq[Rectangle]): Int = {
+    // TODO: nasty mutable
     val overlaps: mutable.Map[(Int, Int), Int] = mutable.Map.empty.withDefaultValue(0)
 
     for {
@@ -30,10 +30,33 @@ object Day3 {
     overlaps.values.count(_ >= 2)
   }
 
+  def nonOverlappingRectangle(rects: Seq[Rectangle]): Int = {
+    // TODO: duplication
+    val overlaps: mutable.Map[(Int, Int), Rectangle] = mutable.Map.empty
+    val nonOverlaps: mutable.Set[Rectangle] = rects.to[mutable.Set]
+
+    for {
+      rect@Rectangle(_, left, top, width, height) <- rects.toSet
+      x <- left until (left + width)
+      y <- top until (top + height)
+    } {
+      val pos = (x, y)
+      if (overlaps.contains(pos)) {
+        nonOverlaps -= rect
+        nonOverlaps -= overlaps(pos)
+      }
+      else
+        overlaps(pos) = rect
+    }
+
+    nonOverlaps.head.id
+  }
+
 
   lazy val input: String = io.Source.fromInputStream(getClass.getResourceAsStream("day3.txt")).mkString.trim
 
   def main(args: Array[String]): Unit = {
     println(overlappingFabric(parseRectangles(input)))
+    println(nonOverlappingRectangle(parseRectangles(input)))
   }
 }
--- src/test/scala/eu/sim642/adventofcode2018/Day3Test.scala
+++ src/test/scala/eu/sim642/adventofcode2018/Day3Test.scala
@@ -5,19 +5,27 @@ import org.scalatest.FunSuite
 
 class Day3Test extends FunSuite {
 
+  val exampleInput = """#1 @ 1,3: 4x4
+                       |#2 @ 3,1: 4x4
+                       |#3 @ 5,5: 2x2""".stripMargin
+
   test("parseRectangle") {
     assert(parseRectangle("#123 @ 3,2: 5x4") == Rectangle(123, 3, 2, 5, 4))
   }
 
   test("Part 1 examples") {
-    assert(overlappingFabric(parseRectangles(
-      """#1 @ 1,3: 4x4
-        |#2 @ 3,1: 4x4
-        |#3 @ 5,5: 2x2""".stripMargin
-    )) == 4)
+    assert(overlappingFabric(parseRectangles(exampleInput)) == 4)
   }
 
   test("Part 1 input answer") {
     assert(overlappingFabric(parseRectangles(input)) == 115242)
+  }
+
+  test("Part 2 examples") {
+    assert(nonOverlappingRectangle(parseRectangles(exampleInput)) == 3)
+  }
+
+  test("Part 2 input answer") {
+    assert(nonOverlappingRectangle(parseRectangles(input)) == 1046)
   }
 }
