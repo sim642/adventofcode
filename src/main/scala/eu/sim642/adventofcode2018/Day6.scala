12df5444e11c99dc74786f8346880e0737ee98cf
Simmo Saan <simmo.saan@gmail.com>
2018-12-06 10:21:01 +0200

    Solve 2018 day 6 part 2

--- src/main/scala/eu/sim642/adventofcode2018/Day6.scala
+++ src/main/scala/eu/sim642/adventofcode2018/Day6.scala
@@ -4,7 +4,7 @@ import eu.sim642.adventofcode2017.Day3.Pos
 
 object Day6 {
 
-  def largestFiniteArea(coords: Set[Pos]): Int = {
+  def largestFiniteArea(coords: Seq[Pos]): Int = {
     def closestCoord(pos: Pos): Option[Pos] = {
       val Seq((coord1, d1), (coord2, d2)) = coords.toSeq.map(coord => coord -> (coord manhattanDistance pos)).sortBy(_._2).take(2) // TODO: optimize taking only smallest 2
       if (d1 < d2)
@@ -31,18 +31,38 @@ object Day6 {
     finiteSizes.values.max
   }
 
+  def safeArea(coords: Seq[Pos], safeDistance: Int = 10000): Int = {
+    def totalDistance(pos: Pos): Int = {
+      coords.map(coord => coord manhattanDistance pos).sum
+    }
+
+    val minX = coords.minBy(_.x).x
+    val minY = coords.minBy(_.y).y
+    val maxX = coords.maxBy(_.x).x
+    val maxY = coords.maxBy(_.y).y
+
+    (for {
+      x <- minX to maxX
+      y <- minY to maxY
+      pos = Pos(x, y)
+      total = totalDistance(pos)
+      if total < safeDistance
+    } yield pos).size
+  }
+
   private val coordRegex = """(\d+), (\d+)""".r
 
   def parseCoord(s: String): Pos = s match {
     case coordRegex(x, y) => Pos(x.toInt, y.toInt)
   }
 
-  def parseCoords(input: String): Set[Pos] = input.lines.map(parseCoord).toSet
+  def parseCoords(input: String): Seq[Pos] = input.lines.map(parseCoord).toSeq
 
 
   lazy val input: String = io.Source.fromInputStream(getClass.getResourceAsStream("day6.txt")).mkString.trim
 
   def main(args: Array[String]): Unit = {
     println(largestFiniteArea(parseCoords(input)))
+    println(safeArea(parseCoords(input)))
   }
 }
--- src/test/scala/eu/sim642/adventofcode2018/Day6Test.scala
+++ src/test/scala/eu/sim642/adventofcode2018/Day6Test.scala
@@ -20,4 +20,12 @@ class Day6Test extends FunSuite {
   test("Part 1 input answer") {
     assert(largestFiniteArea(parseCoords(input)) == 4771)
   }
+
+  test("Part 2 examples") {
+    assert(safeArea(parseCoords(exampleInput), 32) == 16)
+  }
+
+  test("Part 2 input answer") {
+    assert(safeArea(parseCoords(input)) == 39149)
+  }
 }
