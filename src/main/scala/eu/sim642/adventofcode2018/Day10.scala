7f61ce2445815018b30312bba51d828d9257d0af
Simmo Saan <simmo.saan@gmail.com>
2018-12-10 20:50:41 +0200

    Add 2018 day 10 solution using binary search

--- src/main/scala/eu/sim642/adventofcode2018/Day10.scala
+++ src/main/scala/eu/sim642/adventofcode2018/Day10.scala
@@ -5,8 +5,13 @@ import eu.sim642.adventofcode2017.Day19.Pos2
 
 object Day10 {
 
+  implicit class PosScalar(pos: Pos) {
+    def *:(k: Int): Pos = Pos(pos.x * k, pos.y * k)
+  }
+
   case class Point(position: Pos, velocity: Pos) {
     def step: Point = Point(position + velocity, velocity)
+    def step(t: Int): Point = Point(position + t *: velocity, velocity)
   }
 
   def boundingArea(positions: Seq[Pos]): Long = {
@@ -18,31 +23,59 @@ object Day10 {
 
   def boundingAreaPoints(points: Seq[Point]): Long = boundingArea(points.map(_.position))
 
-  implicit class StreamUnfoldOps(stream: Stream.type) {
-    // https://github.com/tpolecat/examples/blob/ab444af9101b9049d6bd7ebf13ae583bc77ac60a/src/main/scala/eg/Unfold.scala
-    def unfold[A, B](a: A)(f: A => Option[(A, B)]): Stream[B] =
-      f(a).map{ case (a, b) => b #:: unfold(a)(f)}.getOrElse(Stream.empty)
-
-    def unfold0[A](a: A)(f: A => Option[A]): Stream[A] =
-      unfold(a)(a => f(a).map(a => (a, a)))
+  trait Solution {
+    def minimizePointsArea(points: Seq[Point]): (Seq[Point], Int)
   }
 
-  def minimizePointsArea(points: Seq[Point]): (Seq[Point], Int) = {
-    val steps = Stream.unfold0((points, boundingAreaPoints(points)))({ case (points, area) =>
-      val newPoints = points.map(_.step)
-      val newArea = boundingAreaPoints(newPoints)
+  object NaiveSolution extends Solution {
 
-      if (newArea < area)
-        Some((newPoints, newArea))
-      else
-        None
-    }).map(_._1).toIterator // iterator is much faster because it doesn't keep old states in memory
+    implicit class StreamUnfoldOps(stream: Stream.type) {
+      // https://github.com/tpolecat/examples/blob/ab444af9101b9049d6bd7ebf13ae583bc77ac60a/src/main/scala/eg/Unfold.scala
+      def unfold[A, B](a: A)(f: A => Option[(A, B)]): Stream[B] =
+        f(a).map { case (a, b) => b #:: unfold(a)(f) }.getOrElse(Stream.empty)
 
-    val (minPoints, minSecond) = steps.zipWithIndex.minBy({ case (points, second) =>
-      boundingAreaPoints(points)
-    })
+      def unfold0[A](a: A)(f: A => Option[A]): Stream[A] =
+        unfold(a)(a => f(a).map(a => (a, a)))
+    }
 
-    (minPoints, 1 + minSecond) // unfold doesn't add initial, account for it here
+    override def minimizePointsArea(points: Seq[Point]): (Seq[Point], Int) = {
+      val steps = Stream.unfold0((points, boundingAreaPoints(points)))({ case (points, area) =>
+        val newPoints = points.map(_.step)
+        val newArea = boundingAreaPoints(newPoints)
+
+        if (newArea < area)
+          Some((newPoints, newArea))
+        else
+          None
+      }).map(_._1).toIterator // iterator is much faster because it doesn't keep old states in memory
+
+      val (minPoints, minSecond) = steps.zipWithIndex.minBy({ case (points, second) =>
+        boundingAreaPoints(points)
+      })
+
+      (minPoints, 1 + minSecond) // unfold doesn't add initial, account for it here
+    }
+  }
+
+  object BinarySearchSolution extends Solution {
+
+    def stepBoundingArea(points: Seq[Point], t: Int): Long = boundingAreaPoints(points.map(_.step(t)))
+
+    override def minimizePointsArea(points: Seq[Point]): (Seq[Point], Int) = {
+      def search(min: Int, max: Int): Int = {
+        if (min == max)
+          return min
+
+        val mid = (min + max) / 2
+        (stepBoundingArea(points, mid), stepBoundingArea(points, mid + 1)) match {
+          case (m, m1) if m < m1 => search(min, mid)
+          case (m, m1) if m > m1 => search(mid + 1, max)
+        }
+      }
+
+      val minSecond = search(0, 15000)
+      (points.map(_.step(minSecond)), minSecond)
+    }
   }
 
   def printPoints(points: Seq[Point]): Unit = {
@@ -70,6 +103,8 @@ object Day10 {
   lazy val input: String = io.Source.fromInputStream(getClass.getResourceAsStream("day10.txt")).mkString.trim
 
   def main(args: Array[String]): Unit = {
+    import BinarySearchSolution._
+
     val (minPoints, minSecond) = minimizePointsArea(parsePoints(input))
     printPoints(minPoints)
     println(minSecond)
