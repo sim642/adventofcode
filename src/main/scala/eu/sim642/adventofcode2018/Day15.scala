8bdb6498ded622733ab1de0c6609d7ee692be7c7
Simmo Saan <simmo.saan@gmail.com>
2018-12-15 12:27:09 +0200

    Solve 2018 day 15 part 1

---
+++ src/main/scala/eu/sim642/adventofcode2018/Day15.scala
@@ -0,0 +1,182 @@
+package eu.sim642.adventofcode2018
+
+import eu.sim642.adventofcode2017.Day14.PosGrid
+import eu.sim642.adventofcode2017.Day19.Grid
+import eu.sim642.adventofcode2017.Day3.Pos
+import Day14.ZipTailIterator
+
+object Day15 {
+
+  sealed trait UnitType {
+    def target: UnitType
+  }
+
+  case object Elf extends UnitType {
+    override def target: UnitType = Goblin
+  }
+
+  case object Goblin extends UnitType {
+    override def target: UnitType = Elf
+  }
+
+  case class CombatUnit(unitType: UnitType, pos: Pos, hp: Int = 200)
+
+  def readingOrder(pos1: Pos, pos2: Pos): Boolean = pos1.y < pos2.y || (pos1.y == pos2.y && pos1.x < pos2.x)
+  def readingOrder(unit1: CombatUnit, unit2: CombatUnit): Boolean = readingOrder(unit1.pos, unit2.pos)
+
+  val posReadingOrdering: Ordering[Pos] = Ordering.fromLessThan(readingOrder)
+  val unitReadingOrdering: Ordering[CombatUnit] = posReadingOrdering.on(_.pos)
+
+  def getTargets(unit: CombatUnit)(implicit units: List[CombatUnit]): Set[CombatUnit] = units.filter(_.unitType == unit.unitType.target).toSet
+
+  def isFree(pos: Pos)(implicit grid: Grid[Char], units: List[CombatUnit]): Boolean = {
+    grid(pos) == '.' && !units.exists(_.pos == pos)
+  }
+
+  def getInRange(targets: Set[CombatUnit])(implicit grid: Grid[Char], units: List[CombatUnit]): Set[Pos] = {
+    for {
+      target <- targets
+      offset <- Pos.axisOffsets
+      pos = target.pos + offset
+      if isFree(pos)
+    } yield pos
+  }
+
+  def bfs(startPos: Pos)(implicit grid: Grid[Char], units: List[CombatUnit]): Map[Pos, Int] = {
+
+    def helper(visited: Map[Pos, Int], toVisit: Map[Pos, Int]): Map[Pos, Int] = {
+      val neighbors = for {
+        (pos, dist) <- toVisit
+        offset <- Pos.axisOffsets
+        newPos = pos + offset
+        if isFree(newPos)
+      } yield newPos -> (dist + 1)
+      val newVisited = visited ++ toVisit
+      val newToVisit = neighbors -- visited.keys
+      if (newToVisit.isEmpty)
+        newVisited
+      else
+        helper(newVisited, newToVisit)
+    }
+
+    helper(Map.empty, Map(startPos -> 0))
+  }
+
+  def getReachable(unit: CombatUnit, inRange: Set[Pos])(implicit grid: Grid[Char], units: List[CombatUnit]): Map[Pos, Int] = {
+    bfs(unit.pos).filterKeys(inRange)
+  }
+
+  def getNearest(reachable: Map[Pos, Int]): Set[Pos] = {
+    val minDist = reachable.values.min
+    reachable.filter(_._2 == minDist).keySet
+  }
+
+  def getChosen(nearest: Set[Pos]): Pos = nearest.min(posReadingOrdering)
+
+  def getStep(chosen: Pos, unit: CombatUnit)(implicit grid: Grid[Char], units: List[CombatUnit]): Pos = {
+    val bfsMap = bfs(chosen)
+    val unitNeighbors = Pos.axisOffsets.map(unit.pos + _).toSet
+    val neighborDists = bfsMap.filterKeys(unitNeighbors)
+    val minDist = neighborDists.values.min
+    neighborDists.filter(_._2 == minDist).keys.min(posReadingOrdering)
+  }
+
+  def combatOutcome(grid: Grid[Char], units: List[CombatUnit]): Int = {
+
+    var done: Boolean = false
+
+    def round(units: List[CombatUnit]): List[CombatUnit] = {
+
+      def turn(init: List[CombatUnit], tail: List[CombatUnit]): List[CombatUnit] = tail match {
+        case Nil => init
+        case unit :: tl =>
+          implicit val otherUnits: List[CombatUnit] = init ++ tl
+          implicit val implicitGrid: Grid[Char] = grid
+          val targets = getTargets(unit)
+          if (targets.isEmpty)
+            done = true
+          val inRange = getInRange(targets)
+
+          var unit2 = unit
+          if (!inRange.contains(unit.pos)) {
+            val reachable = getReachable(unit, inRange)
+            if (reachable.nonEmpty) {
+              val nearest = getNearest(reachable)
+              val chosen = getChosen(nearest)
+              val step = getStep(chosen, unit)
+              unit2 = unit.copy(pos = step)
+            }
+          }
+
+          val unitNeighbors = Pos.axisOffsets.map(unit2.pos + _).toSet
+          val attackUnits = getTargets(unit2).filter(u => unitNeighbors.contains(u.pos))
+
+          if (attackUnits.nonEmpty) {
+            val attackUnit = attackUnits.minBy(u => (u.hp, u.pos))(Ordering.Tuple2(Ordering.Int, posReadingOrdering))
+            val attackUnit2 = attackUnit.copy(hp = attackUnit.hp - 3)
+            val attackUnit3 = if (attackUnit2.hp > 0) Some(attackUnit2) else None
+
+            val init2 = init.flatMap(u => if (u == attackUnit) attackUnit3 else Some(u))
+            val tl2 = tl.flatMap(u => if (u == attackUnit) attackUnit3 else Some(u))
+            turn(unit2 :: init2, tl2)
+          }
+          else {
+            turn(unit2 :: init, tl)
+          }
+      }
+
+      turn(Nil, units.sorted(unitReadingOrdering))
+    }
+
+    val roundIt = Iterator.iterate(units)(round)
+    val ((_, finalUnits), finalI) = roundIt.zipTail.zipWithIndex.find({ case ((prevUnits, units), i) =>
+      //units.count(_.unitType == Goblin) == 0 || units.count(_.unitType == Elf) == 0
+      //units.isEmpty
+      //prevUnits == units
+      done
+    }).get
+    val hpSum = finalUnits.map(_.hp).sum
+    println(finalUnits)
+    println(hpSum)
+    println(finalI)
+    finalI * hpSum
+  }
+
+  def combatOutcome(input: String): Int = {
+    val (grid, units) = parseInput(input)
+    combatOutcome(grid, units)
+  }
+
+
+  def parseGrid(input: String): Grid[Char] = input.lines.map(_.toVector).toVector
+
+  def parseUnit(cell: Char, pos: Pos): Option[CombatUnit] = {
+    val unitType: Option[UnitType] = cell match {
+      case 'E' => Some(Elf)
+      case 'G' => Some(Goblin)
+      case _ => None
+    }
+
+    unitType.map(CombatUnit(_, pos))
+  }
+
+  def parseInput(input: String): (Grid[Char], List[CombatUnit]) = {
+    val units: List[CombatUnit] = (for {
+      (row, y) <- parseGrid(input).zipWithIndex
+      (cell, x) <- row.zipWithIndex
+      pos = Pos(x, y)
+      unit <- parseUnit(cell, pos)
+    } yield unit).toList
+
+    val grid = parseGrid(input.replace('E', '.').replace('G', '.'))
+
+    (grid, units)
+  }
+
+
+  lazy val input: String = io.Source.fromInputStream(getClass.getResourceAsStream("day15.txt")).mkString.stripLineEnd
+
+  def main(args: Array[String]): Unit = {
+    println(combatOutcome(input))
+  }
+}
---
+++ src/test/scala/eu/sim642/adventofcode2018/Day15Test.scala
@@ -0,0 +1,157 @@
+package eu.sim642.adventofcode2018
+
+import org.scalatest.FunSuite
+import Day15._
+import eu.sim642.adventofcode2017.Day3.Pos
+
+class Day15Test extends FunSuite {
+
+  test("Move example 1") {
+    val moveInput =
+      """#######
+        |#E..G.#
+        |#...#.#
+        |#.G.#G#
+        |#######""".stripMargin
+
+    implicit val (grid, units) = parseInput(moveInput)
+
+    val unit = CombatUnit(Elf, Pos(1, 1))
+    val targets = getTargets(unit)
+    assert(targets == Set(
+      CombatUnit(Goblin, Pos(4, 1)),
+      CombatUnit(Goblin, Pos(2, 3)),
+      CombatUnit(Goblin, Pos(5, 3)),
+    ))
+
+    val inRange = getInRange(targets)
+    assert(inRange == Set(
+      Pos(3, 1), Pos(5, 1),
+      Pos(1, 3), Pos(2, 2), Pos(3, 3),
+      Pos(5, 2)
+    ))
+
+    val reachable = getReachable(unit, inRange)
+    assert(reachable == Map(
+      Pos(3, 1) -> 2,
+      Pos(2, 2) -> 2,
+      Pos(1, 3) -> 2,
+      Pos(3, 3) -> 4
+    ))
+
+    val nearest = getNearest(reachable)
+    assert(nearest == Set(
+      Pos(3, 1),
+      Pos(2, 2),
+      Pos(1, 3),
+    ))
+
+    val chosen = getChosen(nearest)
+    assert(chosen == Pos(3, 1))
+  }
+
+  test("Move example 2") {
+    val moveInput =
+      """#######
+        |#.E...#
+        |#.....#
+        |#...G.#
+        |#######""".stripMargin
+
+    implicit val (grid, units) = parseInput(moveInput)
+
+    val unit = CombatUnit(Elf, Pos(2, 1))
+    val targets = getTargets(unit)
+    assert(targets == Set(
+      CombatUnit(Goblin, Pos(4, 3)),
+    ))
+
+    val inRange = getInRange(targets)
+    assert(inRange == Set(
+      Pos(4, 2), Pos(3, 3), Pos(5, 3)
+    ))
+
+    val reachable = getReachable(unit, inRange)
+
+    val nearest = getNearest(reachable)
+    assert(nearest == Set(
+      Pos(4, 2),
+      Pos(3, 3),
+    ))
+
+    val chosen = getChosen(nearest)
+    assert(chosen == Pos(4, 2))
+
+    val step = getStep(chosen, unit)
+    assert(step == Pos(3, 1))
+  }
+
+  val exampleInput1 =
+    """#######
+      |#.G...#
+      |#...EG#
+      |#.#.#G#
+      |#..G#E#
+      |#.....#
+      |#######""".stripMargin
+
+  val exampleInput2 =
+    """#######
+      |#G..#E#
+      |#E#E.E#
+      |#G.##.#
+      |#...#E#
+      |#...E.#
+      |#######""".stripMargin
+
+  val exampleInput3 =
+    """#######
+      |#E..EG#
+      |#.#G.E#
+      |#E.##E#
+      |#G..#.#
+      |#..E#.#
+      |#######""".stripMargin
+
+  val exampleInput4 =
+    """#######
+      |#E.G#.#
+      |#.#G..#
+      |#G.#.G#
+      |#G..#.#
+      |#...E.#
+      |#######""".stripMargin
+
+  val exampleInput5 =
+    """#######
+      |#.E...#
+      |#.#..G#
+      |#.###.#
+      |#E#G#G#
+      |#...#G#
+      |#######""".stripMargin
+
+  val exampleInput6 =
+    """#########
+      |#G......#
+      |#.E.#...#
+      |#..##..G#
+      |#...##..#
+      |#...#...#
+      |#.G...G.#
+      |#.....G.#
+      |#########""".stripMargin
+
+  test("Part 1 examples") {
+    assert(combatOutcome(exampleInput1) == 27730)
+    assert(combatOutcome(exampleInput2) == 36334)
+    assert(combatOutcome(exampleInput3) == 39514)
+    assert(combatOutcome(exampleInput4) == 27755)
+    assert(combatOutcome(exampleInput5) == 28944)
+    assert(combatOutcome(exampleInput6) == 18740)
+  }
+
+  test("Part 1 input answer") {
+    assert(combatOutcome(input) == 197025)
+  }
+}
