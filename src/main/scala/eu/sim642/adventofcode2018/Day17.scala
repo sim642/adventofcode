13e77cef1715d11edd788ea0b3aa7ecd9eec0316
Simmo Saan <simmo.saan@gmail.com>
2018-12-17 11:16:35 +0200

    Solve 2018 day 17 part 1

---
+++ src/main/scala/eu/sim642/adventofcode2018/Day17.scala
@@ -0,0 +1,100 @@
+package eu.sim642.adventofcode2018
+
+import eu.sim642.adventofcode2017.Day3.Pos
+
+object Day17 {
+
+  sealed trait Tile
+  case object Sand extends Tile
+  case object Clay extends Tile
+  case object Flowing extends Tile
+  case object Stabilizing extends Tile
+  case object Stable extends Tile
+
+  type Tiles = Map[Pos, Tile]
+
+
+  def stabilize(tiles: Tiles, pos: Pos): Tiles = {
+    tiles(pos) match {
+      case Stabilizing =>
+        stabilize(stabilize(tiles + (pos -> Stable), pos + Pos(-1, 0)), pos + Pos(1, 0))
+      case _ => tiles
+    }
+  }
+
+
+  def flood(tiles: Tiles, maxY: Int, pos: Pos, prevPos: Pos): Tiles = {
+    //printTiles(tiles)
+    //println()
+    if (pos.y > maxY)
+      tiles
+    else {
+      tiles(pos) match {
+        case Clay | Flowing | Stable | Stabilizing => tiles
+        case Sand =>
+          val downPos = pos + Pos(0, 1)
+          val downTiles = flood(tiles + (pos -> Flowing), maxY, downPos, pos)
+          downTiles(downPos) match {
+            case Flowing | Stabilizing => downTiles
+            case Clay | Stable =>
+              val leftPos = pos + Pos(-1, 0)
+              val leftTiles = flood(downTiles, maxY, leftPos, pos)
+              val rightPos = pos + Pos(1, 0)
+              val rightTiles = flood(leftTiles, maxY, rightPos, pos)
+              (rightTiles(leftPos), rightTiles(rightPos)) match {
+                case (Clay | Stable | Stabilizing, _) | (_, Clay | Stable | Stabilizing) if prevPos == leftPos || prevPos == rightPos =>
+                  rightTiles + (pos -> Stabilizing)
+                case (Clay | Stable | Stabilizing, Clay | Stable | Stabilizing) =>
+                  stabilize(stabilize(rightTiles, leftPos), rightPos) + (pos -> Stable)
+                case _ => rightTiles
+              }
+            case Sand => downTiles
+          }
+      }
+    }
+  }
+
+  def floodedTiles(tiles: Tiles): Int = {
+    val (min, max) = Day6.boundingRect(tiles.keys.toSeq)
+    val flooded = flood(tiles, max.y, Pos(500, 0), Pos(500, -1))
+    printTiles(flooded)
+    flooded.count({ case (pos, tile) => pos.y >= min.y && (tile == Flowing || tile == Stabilizing || tile == Stable)})
+  }
+
+
+  private val xyRegex = """x=(\d+), y=(\d+)..(\d+)""".r
+  private val yxRegex = """y=(\d+), x=(\d+)..(\d+)""".r
+
+  def parseLine(line: String): Tiles = line match {
+    case xyRegex(x, y1, y2) => (y1.toInt to y2.toInt).map(Pos(x.toInt, _) -> Clay).toMap
+    case yxRegex(y, x1, x2) => (x1.toInt to x2.toInt).map(Pos(_, y.toInt) -> Clay).toMap
+  }
+
+  def parseInput(input: String): Tiles = {
+    input.lines.map(parseLine).reduce(_ ++ _).withDefaultValue(Sand)
+  }
+
+  def printTiles(tiles: Tiles): Unit = {
+    val (min, max) = Day6.boundingRect(tiles.keys.toSeq)
+    for (y <- min.y to max.y) {
+      for (x <- min.x to max.x) {
+        print(tiles(Pos(x, y)) match {
+          case Sand => '.'
+          case Clay => '#'
+          case Flowing => '|'
+          case Stabilizing => '/'
+          case Stable => '~'
+        })
+      }
+      println()
+    }
+  }
+
+  lazy val input: String = io.Source.fromInputStream(getClass.getResourceAsStream("day17.txt")).mkString.trim
+
+  def main(args: Array[String]): Unit = {
+    println(floodedTiles(parseInput(input)))
+
+    // 40454 - too high
+  }
+}
---
+++ src/test/scala/eu/sim642/adventofcode2018/Day17Test.scala
@@ -0,0 +1,42 @@
+package eu.sim642.adventofcode2018
+
+import org.scalatest.FunSuite
+import Day17._
+
+class Day17Test extends FunSuite {
+
+  val exampleInput =
+    """x=495, y=2..7
+      |y=7, x=495..501
+      |x=501, y=3..7
+      |x=498, y=2..4
+      |x=506, y=1..2
+      |x=498, y=10..13
+      |x=504, y=10..13
+      |y=13, x=498..504""".stripMargin
+
+  test("Part 1 examples") {
+    assert(floodedTiles(parseInput(exampleInput)) == 57)
+  }
+
+  test("Split incoming off-by-one") {
+    /*
+    ...||+||..
+    ...|###|..
+    ...|...|..
+    |||||||||#
+    |#~~~~~~~#
+    |#########
+    */
+    val input =
+      """y=1, x=499..501
+        |y=5, x=496..504
+        |x=496, y=4..5
+        |x=504, y=3..5""".stripMargin
+    assert(floodedTiles(parseInput(input)) == 22)
+  }
+
+  test("Part 1 input answer") {
+    assert(floodedTiles(parseInput(input)) == 29063)
+  }
+}
