70fd00f5c3923152feb006e35cb7560b3e23303a
Simmo Saan <simmo.saan@gmail.com>
2018-12-21 20:33:45 +0200

    Refactor 2018 day 21 solutions to provide iterators

--- src/main/scala/eu/sim642/adventofcode2018/Day21.scala
+++ src/main/scala/eu/sim642/adventofcode2018/Day21.scala
@@ -1,82 +1,57 @@
 package eu.sim642.adventofcode2018
 
+import eu.sim642.adventofcode2018.Day16.{Instruction, Registers}
+import eu.sim642.adventofcode2018.Day2.HeadIterator
+import eu.sim642.adventofcode2017.Day8.LastIterator
+
 import scala.collection.mutable
 import scala.util.control.Breaks._
 
 object Day21 {
 
   trait Solution {
-    def r2Seq(input: String): Seq[Int]
+    def iterater2(input: String): Iterator[Int]
 
-    def firstHaltr0(input: String): Int = r2Seq(input).head
+    def r2Seq(input: String): Seq[Int] = {
+      val (ipRegister, program) = Day19.parseInput(input)
+
+      var r2s: mutable.LinkedHashSet[Int] = mutable.LinkedHashSet.empty // keeps order but allows fast contains
+
+      for (r2 <- iterater2(input)) {
+        if (!r2s.add(r2))
+          return r2s.toSeq
+      }
+      ???
+    }
+
+    def firstHaltr0(input: String): Int = iterater2(input).head
     def lastHaltr0(input: String): Int = r2Seq(input).last
   }
 
   object SimulateSolution extends Solution {
-    override def r2Seq(input: String): Seq[Int] = {
-      val (ipRegister, program) = Day19.parseInput(input)
-
-      var r2s: mutable.LinkedHashSet[Int] = mutable.LinkedHashSet.empty // keeps order but allows fast contains
-
-      var registers = Seq.fill(6)(0)
-      var ip = 0
-      while (program.indices.contains(ip)) {
-        val beforeRegisters = registers.updated(ipRegister, ip)
-        if (ip == 28) {
-          val r2 = beforeRegisters(2)
-          if (!r2s.add(r2))
-            return r2s.toSeq
-        }
-
+    def iterateSimulation(program: Vector[Instruction], ipRegister: Int): Iterator[Registers] = {
+      Iterator.iterate(Seq.fill(6)(0))({ beforeRegisters =>
+        val ip = beforeRegisters(ipRegister)
+        assert(program.indices.contains(ip), "program does not halt")
         val instruction = program(ip)
         val afterRegisters = instruction(beforeRegisters)
-        //println(s"$beforeRegisters $instruction $afterRegisters")
-        ip = afterRegisters(ipRegister)
-        registers = afterRegisters
-        ip += 1
-      }
-      ???
+        afterRegisters.updated(ipRegister, afterRegisters(ipRegister) + 1)
+      })
     }
 
-    override def firstHaltr0(input: String): Int = {
-      // TODO: avoid duplication
+    override def iterater2(input: String): Iterator[Int] = {
       val (ipRegister, program) = Day19.parseInput(input)
-
-      var registers = Seq.fill(6)(0)
-      var ip = 0
-      while (program.indices.contains(ip)) {
-        val beforeRegisters = registers.updated(ipRegister, ip)
-        if (ip == 28) {
-          val r2 = beforeRegisters(2)
-          return r2 // return immediately on first r2
-        }
-
-        val instruction = program(ip)
-        val afterRegisters = instruction(beforeRegisters)
-        //println(s"$beforeRegisters $instruction $afterRegisters")
-        ip = afterRegisters(ipRegister)
-        registers = afterRegisters
-        ip += 1
-      }
-      ???
+      iterateSimulation(program, ipRegister).filter(_(ipRegister) == 28).map(_(2))
     }
   }
 
   object ReverseEngineeredSolution extends Solution {
-    override def r2Seq(input: String): Seq[Int] = {
+    override def iterater2(input: String): Iterator[Int] = {
       // ignores input...
 
-      var r2s: mutable.LinkedHashSet[Int] = mutable.LinkedHashSet.empty // keeps order but allows fast contains
+      Iterator.iterate(Seq.fill(6)(0))({ beforeRegisters =>
+        var Seq(r0, r1, r2, r3, r4, r5) = beforeRegisters
 
-      var r0 = 0
-      var r1 = 0
-      var r2 = 0
-      var r3 = 0
-      var r4 = 0
-      var r5 = 0
-
-      r2 = 0
-      do {
         r5 = r2 | 65536
         r2 = 2238642
         breakable {
@@ -88,16 +63,16 @@ object Day21 {
             r2 &= 16777215
 
             /*if (256 <= r5) {
-            // divide r5 by 256, i.e. right shift by 8
-            r3 = 0
-            do {
-              r1 = r3 + 1
-              r1 *= 256
-              if (r1 <= r5)
-                r3 += 1
-            } while (r1 <= r5)
-            r5 = r3
-          }*/
+              // divide r5 by 256, i.e. right shift by 8
+              r3 = 0
+              do {
+                r1 = r3 + 1
+                r1 *= 256
+                if (r1 <= r5)
+                  r3 += 1
+              } while (r1 <= r5)
+              r5 = r3
+            }*/
             if (256 <= r5) {
               r5 /= 256
             }
@@ -106,11 +81,8 @@ object Day21 {
           } while (true)
         }
 
-        //println(r2)
-        if (!r2s.add(r2))
-          return r2s.toSeq
-      } while (r2 != r0)
-      ???
+        Seq(r0, r1, r2, r3, r4, r5)
+      }).drop(1).map(_(2))
     }
   }
 
