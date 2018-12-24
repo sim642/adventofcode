8d030d4d5755a2887343720d602f3aec413b5ed7
Simmo Saan <simmo.saan@gmail.com>
2018-12-24 10:43:42 +0200

    Solve 2018 day 24 part 1

---
+++ src/main/scala/eu/sim642/adventofcode2018/Day24.scala
@@ -0,0 +1,143 @@
+package eu.sim642.adventofcode2018
+
+object Day24 {
+
+  sealed trait GroupType {
+    def targetType: GroupType
+  }
+  case object ImmuneSystem extends GroupType {
+    override def targetType: GroupType = Infection
+  }
+  case object Infection extends GroupType {
+    override def targetType: GroupType = ImmuneSystem
+  }
+
+  case class Group(i: Int, groupType: GroupType, units: Int, unitHp: Int, attackDamage: Int, attackType: String, initiative: Int, weaknesses: Set[String], immunities: Set[String]) {
+
+    def effectivePower: Int = units * attackDamage
+
+    def damageTo(defendingGroup: Group): Int = {
+      if (defendingGroup.immunities.contains(attackType))
+        0
+      else if (defendingGroup.weaknesses.contains(attackType))
+        2 * effectivePower
+      else
+        effectivePower
+    }
+
+    override def toString: String = s"$groupType $i"
+  }
+
+  def targetSelectionPhase(groups: Seq[Group]): Map[Group, Group] = {
+
+    def helper(choosingGroups: List[Group], attackableGroups: Set[Group], targets: Map[Group, Group] = Map.empty): Map[Group, Group] = choosingGroups match {
+      case Nil => targets
+      case attackingGroup :: tl =>
+        val enemyArmy = attackableGroups.filter(_.groupType == attackingGroup.groupType.targetType)
+        if (enemyArmy.nonEmpty) {
+          val target = enemyArmy.maxBy(defendingGroup => (attackingGroup.damageTo(defendingGroup), defendingGroup.effectivePower, defendingGroup.initiative))
+          if (attackingGroup.damageTo(target) > 0)
+            helper(tl, attackableGroups - target, targets + (attackingGroup -> target))
+          else
+            helper(tl, attackableGroups, targets)
+        }
+        else
+          helper(tl, attackableGroups, targets)
+    }
+
+    val choosingGroups = groups.sortBy(group => (-group.effectivePower, -group.initiative))
+    helper(choosingGroups.toList, groups.toSet)
+  }
+
+  def attackingPhase(groups: Seq[Group], targets: Map[Group, Group]): Seq[Group] = {
+
+    def helper(attackingGroups: List[Group], targets: Map[Group, Group], groups: Set[Group]): Seq[Group] = attackingGroups match {
+      case Nil => groups.toSeq
+      case attackingGroup :: tl =>
+        val defendingGroup = targets(attackingGroup)
+        print(s"$attackingGroup attacks $defendingGroup")
+        if (groups.contains(defendingGroup)) { // is needed?
+          val damage = attackingGroup.damageTo(defendingGroup)
+          val killedUnits = damage / defendingGroup.unitHp
+          println(s" killing $killedUnits")
+          val newDefendingGroup = defendingGroup.copy(units = defendingGroup.units - killedUnits)
+
+          if (newDefendingGroup.units > 0) {
+            val newGroups = groups - defendingGroup + newDefendingGroup
+            val newTargets = if (targets.contains(defendingGroup)) targets - defendingGroup + (newDefendingGroup -> targets(defendingGroup)) else targets
+            val newTl = tl.map({ tlGroup =>
+              if (tlGroup == defendingGroup)
+                newDefendingGroup
+              else
+                tlGroup
+            })
+            helper(newTl, newTargets, newGroups)
+          }
+          else {
+            val newGroups = groups - defendingGroup
+            val newTargets = targets - defendingGroup
+            val newTl = tl.diff(List(defendingGroup))
+            helper(newTl, newTargets, newGroups)
+          }
+        }
+        else {
+          println()
+          helper(tl, targets, groups)
+        }
+    }
+
+    val attackingGroups = targets.keys.toSeq.sortBy(-_.initiative)
+    helper(attackingGroups.toList, targets, groups.toSet)
+  }
+
+  def combat(initialGroups: Seq[Group]): Int = {
+    var groups = initialGroups
+    while (groups.exists(_.groupType == ImmuneSystem) && groups.exists(_.groupType == Infection)) {
+      for (group <- groups)
+        println(s"$group has ${group.units} units")
+
+      val targets = targetSelectionPhase(groups)
+      //for ((a, d) <- targets)
+      //  println(s"$a targets $d")
+
+      groups = attackingPhase(groups, targets)
+      println()
+    }
+    groups.map(_.units).sum
+  }
+
+  // TODO: combinator parsing...
+  private val groupWeakImmuneRegex = """(\d+) units each with (\d+) hit points \(weak to ([\w, ]+); immune to ([\w, ]+)\) with an attack that does (\d+) (\w+) damage at initiative (\d+)""".r
+  private val groupImmuneWeakRegex = """(\d+) units each with (\d+) hit points \(immune to ([\w, ]+); weak to ([\w, ]+)\) with an attack that does (\d+) (\w+) damage at initiative (\d+)""".r
+  private val groupWeakRegex = """(\d+) units each with (\d+) hit points \(weak to ([\w, ]+)\) with an attack that does (\d+) (\w+) damage at initiative (\d+)""".r
+  private val groupImmuneRegex = """(\d+) units each with (\d+) hit points \(immune to ([\w, ]+)\) with an attack that does (\d+) (\w+) damage at initiative (\d+)""".r
+  private val groupRegex = """(\d+) units each with (\d+) hit points with an attack that does (\d+) (\w+) damage at initiative (\d+)""".r
+
+  def parseGroup(s: String, i: Int, groupType: GroupType): Group = s match {
+    case groupWeakImmuneRegex(units, unitHp, weaknesses, immunities, attackDamage, attackType, initiative) =>
+      Group(i, groupType, units.toInt, unitHp.toInt, attackDamage.toInt, attackType, initiative.toInt, weaknesses.split(", ").toSet, immunities.split(", ").toSet)
+    case groupImmuneWeakRegex(units, unitHp, immunities, weaknesses, attackDamage, attackType, initiative) =>
+      Group(i, groupType, units.toInt, unitHp.toInt, attackDamage.toInt, attackType, initiative.toInt, weaknesses.split(", ").toSet, immunities.split(", ").toSet)
+    case groupWeakRegex(units, unitHp, weaknesses, attackDamage, attackType, initiative) =>
+      Group(i, groupType, units.toInt, unitHp.toInt, attackDamage.toInt, attackType, initiative.toInt, weaknesses.split(", ").toSet, Set.empty)
+    case groupImmuneRegex(units, unitHp, immunities, attackDamage, attackType, initiative) =>
+      Group(i, groupType, units.toInt, unitHp.toInt, attackDamage.toInt, attackType, initiative.toInt, Set.empty, immunities.split(", ").toSet)
+    case groupRegex(units, unitHp, attackDamage, attackType, initiative) =>
+      Group(i, groupType, units.toInt, unitHp.toInt, attackDamage.toInt, attackType, initiative.toInt, Set.empty, Set.empty)
+  }
+
+  def parseInput(input: String): Seq[Group] = {
+    val Seq("Immune System:" +: immuneLines, "Infection:" +: infectionLines) = input.split("""\n\n""").map(_.lines.toList).toList
+
+    immuneLines.zipWithIndex.map(p => parseGroup(p._1, p._2 + 1, ImmuneSystem)) ++ infectionLines.zipWithIndex.map(p => parseGroup(p._1, p._2 + 1, Infection))
+  }
+
+
+  lazy val input: String = io.Source.fromInputStream(getClass.getResourceAsStream("day24.txt")).mkString.trim
+
+  def main(args: Array[String]): Unit = {
+    println(combat(parseInput(input)))
+
+    // 19293 - too low
+  }
+}
---
+++ src/test/scala/eu/sim642/adventofcode2018/Day24Test.scala
@@ -0,0 +1,34 @@
+package eu.sim642.adventofcode2018
+
+import org.scalatest.FunSuite
+import Day24._
+
+class Day24Test extends FunSuite {
+
+  val exampleInput =
+    """Immune System:
+      |17 units each with 5390 hit points (weak to radiation, bludgeoning) with an attack that does 4507 fire damage at initiative 2
+      |989 units each with 1274 hit points (immune to fire; weak to bludgeoning, slashing) with an attack that does 25 slashing damage at initiative 3
+      |
+      |Infection:
+      |801 units each with 4706 hit points (weak to radiation) with an attack that does 116 bludgeoning damage at initiative 1
+      |4485 units each with 2961 hit points (immune to radiation; weak to fire, cold) with an attack that does 12 slashing damage at initiative 4""".stripMargin
+
+  test("parseGroup") {
+    val s = "18 units each with 729 hit points (weak to fire; immune to cold, slashing) with an attack that does 8 radiation damage at initiative 10"
+    assert(parseGroup(s, 1, ImmuneSystem) == Group(1, ImmuneSystem, 18, 729, 8, "radiation", 10, Set("fire"), Set("cold", "slashing")))
+  }
+
+  test("parseInput") {
+    println(parseInput(exampleInput))
+    println(parseInput(input))
+  }
+
+  test("Part 1 examples") {
+    assert(combat(parseInput(exampleInput)) == 5216)
+  }
+
+  test("Part 1 input answer") {
+    assert(combat(parseInput(input)) == 19295)
+  }
+}
