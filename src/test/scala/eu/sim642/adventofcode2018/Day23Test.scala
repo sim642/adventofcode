416ef974cad833911f88dd08a37b79a759fd5303
Simmo Saan <simmo.saan@gmail.com>
2018-12-24 15:03:05 +0200

    Add 2018 day 23 part 2 test from VikeStep

--- src/test/scala/eu/sim642/adventofcode2018/Day23Test.scala
+++ src/test/scala/eu/sim642/adventofcode2018/Day23Test.scala
@@ -47,6 +47,7 @@ object Day23Test {
     lazy val dataforceInput: String = io.Source.fromInputStream(getClass.getResourceAsStream("day23dataforce.txt")).mkString.trim
     lazy val dataforceFriendInput: String = io.Source.fromInputStream(getClass.getResourceAsStream("day23dataforcefriend.txt")).mkString.trim
     lazy val lamperiInput: String = io.Source.fromInputStream(getClass.getResourceAsStream("day23lamperi.txt")).mkString.trim
+    lazy val vikestepInput: String = io.Source.fromInputStream(getClass.getResourceAsStream("day23vikestep.txt")).mkString.trim
 
     test("Part 2 examples") {
       assert(part2Solution.closestMostNanobots(parseInput(exampleInput2)) == 36)
@@ -66,6 +67,10 @@ object Day23Test {
 
     test("Part 2 Lamperi") {
       assert(part2Solution.closestMostNanobots(parseInput(lamperiInput)) == 107272899) // unconfirmed answer
+    }
+
+    test("Part 2 VikeStep") {
+      assert(part2Solution.closestMostNanobots(parseInput(vikestepInput)) == 93750870)
     }
   }
 
