3dd51a678ca6cf1ab3e6b73c8dec99ea940994d5
Simmo Saan <simmo.saan@gmail.com>
2018-12-24 15:46:52 +0200

    Add 2018 day 23 part 2 tests for closest to origin

--- src/test/scala/eu/sim642/adventofcode2018/Day23Test.scala
+++ src/test/scala/eu/sim642/adventofcode2018/Day23Test.scala
@@ -2,7 +2,9 @@ package eu.sim642.adventofcode2018
 
 import org.scalatest.{FunSuite, Suites}
 import Day23._
+import eu.sim642.adventofcode2017.Day20.Pos3
 import eu.sim642.adventofcode2018.Day23Test.{CliquePart2SolutionTest, Part1Test, SplittingPart2SolutionTest}
+import org.scalatest.prop.PropertyChecks
 
 class Day23Test extends Suites(
   new Part1Test,
@@ -34,7 +36,7 @@ object Day23Test {
     }
   }
 
-  sealed abstract class Part2SolutionTest(part2Solution: Part2Solution) extends FunSuite {
+  sealed abstract class Part2SolutionTest(part2Solution: Part2Solution) extends FunSuite with PropertyChecks {
 
     val exampleInput2 =
       """pos=<10,12,12>, r=2
@@ -51,6 +53,76 @@ object Day23Test {
 
     test("Part 2 examples") {
       assert(part2Solution.closestMostNanobots(parseInput(exampleInput2)) == 36)
+    }
+
+    test("Part 2 closest to origin (symmetric)") {
+      val positions = Table(
+        ("pos1", "pos2"),
+        (Pos3(1, 3, 0), Pos3(-1, 3, 0)),
+        (Pos3(1, -3, 0), Pos3(-1, -3, 0)),
+        (Pos3(3, 1, 0), Pos3(3, -1, 0)),
+        (Pos3(-3, 1, 0), Pos3(-3, -1, 0)),
+
+        (Pos3(0, 3, 1), Pos3(0, 3, -1)),
+        (Pos3(0, -3, 1), Pos3(0, -3, -1)),
+        (Pos3(3, 0, 1), Pos3(3, 0, -1)),
+        (Pos3(-3, 0, 1), Pos3(-3, 0, -1)),
+
+        (Pos3(1, 0, 3), Pos3(-1, 0, 3)),
+        (Pos3(1, 0, -3), Pos3(-1, 0, -3)),
+        (Pos3(0, 1, 3), Pos3(0, -1, 3)),
+        (Pos3(0, 1, -3), Pos3(0, -1, -3)),
+      )
+
+      forAll (positions) { (pos1, pos2) =>
+        val nanobots = Seq(
+          Nanobot(pos1, 2),
+          Nanobot(pos2, 2)
+        )
+
+        assert(part2Solution.closestMostNanobots(nanobots) == 2)
+      }
+    }
+
+    test("Part 2 closest to origin (asymmetric)") {
+      val positions = Table(
+        ("pos1", "pos2"),
+        (Pos3(1, 4, 0), Pos3(-1, 3, 0)),
+        (Pos3(1, 3, 0), Pos3(-1, 4, 0)),
+        (Pos3(1, -4, 0), Pos3(-1, -3, 0)),
+        (Pos3(1, -3, 0), Pos3(-1, -4, 0)),
+        (Pos3(4, 1, 0), Pos3(3, -1, 0)),
+        (Pos3(3, 1, 0), Pos3(4, -1, 0)),
+        (Pos3(-4, 1, 0), Pos3(-3, -1, 0)),
+        (Pos3(-3, 1, 0), Pos3(-4, -1, 0)),
+
+        (Pos3(0, 4, 1), Pos3(0, 3, -1)),
+        (Pos3(0, 3, 1), Pos3(0, 4, -1)),
+        (Pos3(0, -4, 1), Pos3(0, -3, -1)),
+        (Pos3(0, -3, 1), Pos3(0, -4, -1)),
+        (Pos3(4, 0, 1), Pos3(3, 0, -1)),
+        (Pos3(3, 0, 1), Pos3(4, 0, -1)),
+        (Pos3(-4, 0, 1), Pos3(-3, 0, -1)),
+        (Pos3(-3, 0, 1), Pos3(-4, 0, -1)),
+
+        (Pos3(1, 0, 4), Pos3(-1, 0, 3)),
+        (Pos3(1, 0, 3), Pos3(-1, 0, 4)),
+        (Pos3(1, 0, -4), Pos3(-1, 0, -3)),
+        (Pos3(1, 0, -3), Pos3(-1, 0, -4)),
+        (Pos3(0, 1, 4), Pos3(0, -1, 3)),
+        (Pos3(0, 1, 3), Pos3(0, -1, 4)),
+        (Pos3(0, 1, -4), Pos3(0, -1, -3)),
+        (Pos3(0, 1, -3), Pos3(0, -1, -4)),
+      )
+
+      forAll (positions) { (pos1, pos2) =>
+        val nanobots = Seq(
+          Nanobot(pos1, 2),
+          Nanobot(pos2, 2)
+        )
+
+        assert(part2Solution.closestMostNanobots(nanobots) == 3)
+      }
     }
 
     test("Part 2 input answer") {
