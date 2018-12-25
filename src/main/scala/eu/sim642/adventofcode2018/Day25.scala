a6eef6096039ea010c0920fa1d83272b1f3c3b1b
Simmo Saan <simmo.saan@gmail.com>
2018-12-25 10:16:41 +0200

    Solve 2018 day 25 part 1

---
+++ src/main/scala/eu/sim642/adventofcode2018/Day25.scala
@@ -0,0 +1,51 @@
+package eu.sim642.adventofcode2018
+
+object Day25 {
+
+  case class Pos4(x: Int, y: Int, z: Int, w: Int) {
+    def manhattanDistance(that: Pos4): Int =
+      (x - that.x).abs + (y - that.y).abs + (z - that.z).abs + (w - that.w).abs
+  }
+
+  // copied from 2018 Day 14
+  def bfs(poss: Set[Pos4], startPos: Pos4): Set[Pos4] = {
+
+    def helper(visited: Set[Pos4], toVisit: Set[Pos4]): Set[Pos4] = {
+      val neighbors = toVisit.flatMap(pos => poss.filter(_.manhattanDistance(pos) <= 3))
+      val newVisited = visited ++ toVisit
+      val newToVisit = neighbors -- visited
+      if (newToVisit.isEmpty)
+        newVisited
+      else
+        helper(newVisited, newToVisit)
+    }
+
+    helper(Set.empty, Set(startPos))
+  }
+
+  def bfsGroups(poss: Set[Pos4]): Set[Set[Pos4]] = {
+    if (poss.isEmpty)
+      Set.empty
+    else {
+      val startPos = poss.head // take any node
+      val group = bfs(poss, startPos) // find its component
+      val restPoss = poss -- group
+      bfsGroups(restPoss) + group
+    }
+  }
+
+  def countConstellations(points: Seq[Pos4]): Int = bfsGroups(points.toSet).size
+
+  def parsePoint(s: String): Pos4 = {
+    val Seq(x, y, z, w) = s.split(",").toSeq.map(_.toInt)
+    Pos4(x, y, z, w)
+  }
+
+  def parseInput(input: String): Seq[Pos4] = input.lines.map(parsePoint).toSeq
+
+  lazy val input: String = io.Source.fromInputStream(getClass.getResourceAsStream("day25.txt")).mkString.trim
+
+  def main(args: Array[String]): Unit = {
+    println(countConstellations(parseInput(input)))
+  }
+}
---
+++ src/test/scala/eu/sim642/adventofcode2018/Day25Test.scala
@@ -0,0 +1,64 @@
+package eu.sim642.adventofcode2018
+
+import org.scalatest.FunSuite
+import Day25._
+
+class Day25Test extends FunSuite {
+
+  val exampleInput1 =
+    """0,0,0,0
+      |3,0,0,0
+      |0,3,0,0
+      |0,0,3,0
+      |0,0,0,3
+      |0,0,0,6
+      |9,0,0,0
+      |12,0,0,0""".stripMargin
+
+  val exampleInput2 =
+    """-1,2,2,0
+      |0,0,2,-2
+      |0,0,0,-2
+      |-1,2,0,0
+      |-2,-2,-2,2
+      |3,0,2,-1
+      |-1,3,2,2
+      |-1,0,-1,0
+      |0,2,1,-2
+      |3,0,0,0""".stripMargin
+
+  val exampleInput3 =
+    """1,-1,0,1
+      |2,0,-1,0
+      |3,2,-1,0
+      |0,0,3,1
+      |0,0,-1,-1
+      |2,3,-2,0
+      |-2,2,0,0
+      |2,-2,0,-1
+      |1,-1,0,-1
+      |3,2,0,2""".stripMargin
+
+  val exampleInput4 =
+    """1,-1,-1,-2
+      |-2,-2,0,1
+      |0,2,1,3
+      |-2,3,-2,1
+      |0,2,3,-2
+      |-1,-1,1,-2
+      |0,-2,-1,0
+      |-2,2,3,-1
+      |1,2,2,0
+      |-1,-2,0,-2""".stripMargin
+
+  test("Part 1 examples") {
+    assert(countConstellations(parseInput(exampleInput1)) == 2)
+    assert(countConstellations(parseInput(exampleInput2)) == 4)
+    assert(countConstellations(parseInput(exampleInput3)) == 3)
+    assert(countConstellations(parseInput(exampleInput4)) == 8)
+  }
+
+  test("Part 1 input answer") {
+    assert(countConstellations(parseInput(input)) == 327)
+  }
+}
