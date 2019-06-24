af613b2c2dd9849f91593c3da88f2d91dc9bf13d
Simmo Saan <simmo.saan@gmail.com>
2019-06-24 12:34:16 +0300

    Solve 2016 day 9 part 2

--- src/main/scala/eu/sim642/adventofcode2016/Day9.scala
+++ src/main/scala/eu/sim642/adventofcode2016/Day9.scala
@@ -4,20 +4,42 @@ object Day9 {
 
   private val markerRegex = """\((\d+)x(\d+)\)""".r
 
-  def decompressedLength(str: String): Int = {
-    markerRegex.findFirstMatchIn(str) match {
-      case Some(m) =>
-        val markerLength = m.group(1).toInt
-        val markerTimes = m.group(2).toInt
-        m.start + markerTimes * markerLength + decompressedLength(m.after.toString.substring(markerLength))
-      case None =>
-        str.length
+  trait Part {
+    def decompressedLength(str: String): Long
+  }
+
+  object Part1 extends Part {
+    override def decompressedLength(str: String): Long = {
+      markerRegex.findFirstMatchIn(str) match {
+        case Some(m) =>
+          val markerLength = m.group(1).toInt
+          val markerTimes = m.group(2).toInt
+          m.start + markerTimes * markerLength + decompressedLength(m.after.toString.substring(markerLength))
+        case None =>
+          str.length
+      }
+    }
+  }
+
+  object Part2 extends Part {
+    override def decompressedLength(str: String): Long = {
+      markerRegex.findFirstMatchIn(str) match {
+        case Some(m) =>
+          val markerLength = m.group(1).toInt
+          val markerTimes = m.group(2).toInt
+          val afterStr = m.after.toString
+          val markerStr = afterStr.substring(0, markerLength)
+          m.start + markerTimes * decompressedLength(markerStr) + decompressedLength(afterStr.substring(markerLength))
+        case None =>
+          str.length
+      }
     }
   }
 
   lazy val input: String = io.Source.fromInputStream(getClass.getResourceAsStream("day9.txt")).mkString.trim
 
   def main(args: Array[String]): Unit = {
-    println(decompressedLength(input))
+    println(Part1.decompressedLength(input))
+    println(Part2.decompressedLength(input))
   }
 }
--- src/test/scala/eu/sim642/adventofcode2016/Day9Test.scala
+++ src/test/scala/eu/sim642/adventofcode2016/Day9Test.scala
@@ -6,15 +6,26 @@ import org.scalatest.FunSuite
 class Day9Test extends FunSuite {
 
   test("Part 1 examples") {
-    assert(decompressedLength("ADVENT") == 6)
-    assert(decompressedLength("A(1x5)BC") == 7)
-    assert(decompressedLength("(3x3)XYZ") == 9)
-    assert(decompressedLength("A(2x2)BCD(2x2)EFG") == 11)
-    assert(decompressedLength("(6x1)(1x3)A") == 6)
-    assert(decompressedLength("X(8x2)(3x3)ABCY") == 18)
+    assert(Part1.decompressedLength("ADVENT") == 6)
+    assert(Part1.decompressedLength("A(1x5)BC") == 7)
+    assert(Part1.decompressedLength("(3x3)XYZ") == 9)
+    assert(Part1.decompressedLength("A(2x2)BCD(2x2)EFG") == 11)
+    assert(Part1.decompressedLength("(6x1)(1x3)A") == 6)
+    assert(Part1.decompressedLength("X(8x2)(3x3)ABCY") == 18)
   }
 
   test("Part 1 input answer") {
-    assert(decompressedLength(input) == 97714)
+    assert(Part1.decompressedLength(input) == 97714)
+  }
+
+  test("Part 2 examples") {
+    assert(Part2.decompressedLength("(3x3)XYZ") == 9)
+    assert(Part2.decompressedLength("X(8x2)(3x3)ABCY") == "XABCABCABCABCABCABCY".length)
+    assert(Part2.decompressedLength("(27x12)(20x12)(13x14)(7x10)(1x12)A") == 241920)
+    assert(Part2.decompressedLength("(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN") == 445)
+  }
+
+  test("Part 2 input answer") {
+    assert(Part2.decompressedLength(input) == 10762972461L)
   }
 }
