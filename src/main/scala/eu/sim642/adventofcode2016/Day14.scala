ee2d3576788454bdea27fac26cdb485eaf400192
Simmo Saan <simmo.saan@gmail.com>
2019-08-05 18:40:06 +0300

    Solve 2016 day 14 part 2

--- src/main/scala/eu/sim642/adventofcode2016/Day14.scala
+++ src/main/scala/eu/sim642/adventofcode2016/Day14.scala
@@ -1,34 +1,69 @@
 package eu.sim642.adventofcode2016
 
+import java.security.MessageDigest
+
 import eu.sim642.adventofcode2018.Day2.HeadIterator
 
 object Day14 {
 
-  def keys(salt: String): Iterator[(Int, String)] = {
-    Iterator.from(0)
-      .map(i => (i, Day5.md5(salt + i)))
-      .sliding(1 + 1000)
-      .filter({ case (i, hash) +: next1000 =>
-        hash.sliding(3).find(s3 => s3(0) == s3(1) && s3(1) == s3(2)) match {
-          case None =>
-            false
-          case Some(s3) =>
-            val c = s3.head
-            val s5 = s"$c$c$c$c$c"
-            next1000.exists(_._2.contains(s5))
-        }
-      })
-      .map(_.head)
+  // https://old.reddit.com/r/adventofcode/comments/5i8pzz/2016_day_14_solutions/dbd7lvz/
+  // https://stackoverflow.com/a/9855338/854540
+  private val HEX_ARRAY = "0123456789abcdef".toCharArray
+  def bytesToHex(bytes: Array[Byte]): String = {
+    val hexChars = new Array[Char](bytes.length * 2)
+    for (j <- bytes.indices) {
+      val v = bytes(j) & 0xFF
+      hexChars(j * 2) = HEX_ARRAY(v >>> 4)
+      hexChars(j * 2 + 1) = HEX_ARRAY(v & 0x0F)
+    }
+    new String(hexChars)
   }
 
-  def key64Index(salt: String): Int = {
-    keys(salt).drop(64 - 1).head._1
+  // https://stackoverflow.com/a/5992852
+  def md5(s: String): String = {
+    bytesToHex(MessageDigest.getInstance("MD5").digest(s.getBytes))
+  }
+
+  trait Part {
+    def hash(s: String): String
+
+    def keys(salt: String): Iterator[(Int, String)] = {
+      Iterator.from(0)
+        .map(i => (i, hash(salt + i)))
+        .sliding(1 + 1000)
+        .filter({ case (i, hash) +: next1000 =>
+          hash.sliding(3).find(s3 => s3(0) == s3(1) && s3(1) == s3(2)) match {
+            case None =>
+              false
+            case Some(s3) =>
+              val c = s3.head
+              val s5 = s"$c$c$c$c$c"
+              next1000.exists(_._2.contains(s5))
+          }
+        })
+        .map(_.head)
+    }
+
+    def key64Index(salt: String): Int = {
+      keys(salt).drop(64 - 1).head._1
+    }
+  }
+
+  object Part1 extends Part {
+    override def hash(s: String): String = md5(s)
+  }
+
+  object Part2 extends Part {
+    override def hash(s: String): String = {
+      Iterator.iterate(s)(md5).drop(2017).head
+    }
   }
 
   //lazy val input: String = io.Source.fromInputStream(getClass.getResourceAsStream("day14.txt")).mkString.trim
   val input = "qzyelonm"
 
   def main(args: Array[String]): Unit = {
-    println(key64Index(input))
+    println(Part1.key64Index(input))
+    println(Part2.key64Index(input))
   }
 }
--- src/test/scala/eu/sim642/adventofcode2016/Day14Test.scala
+++ src/test/scala/eu/sim642/adventofcode2016/Day14Test.scala
@@ -8,20 +8,47 @@ class Day14Test extends FunSuite with PropertyChecks {
 
   val exampleInput = "abc"
 
+  test("md5") {
+    assert(md5("The quick brown fox jumps over the lazy dog") == "9e107d9d372bb6826bd81d3542a419d6")
+  }
+
   test("Part 1 keys") {
     val keyIndices = Table("i", 39, 92)
 
-    val it = keys(exampleInput)
+    val it = Part1.keys(exampleInput)
     forAll (keyIndices) { i =>
       assert(it.next()._1 == i)
     }
   }
 
   test("Part 1 examples") {
-    assert(key64Index(exampleInput) == 22728)
+    assert(Part1.key64Index(exampleInput) == 22728)
   }
 
   test("Part 1 input answer") {
-    assert(key64Index(input) == 15168)
+    assert(Part1.key64Index(input) == 15168)
+  }
+
+  test("Part 2 hash") {
+    assert(Part2.hash("abc0") == "a107ff634856bb300138cac6568c0f24")
+  }
+
+  test("Part 2 keys") {
+    val keyIndices = Table("i", 10)
+
+    val it = Part2.keys(exampleInput)
+    forAll (keyIndices) { i =>
+      assert(it.next()._1 == i)
+    }
+  }
+
+  // ignored because very slow (20s each)?
+
+  ignore("Part 2 examples") {
+    assert(Part2.key64Index(exampleInput) == 22551)
+  }
+
+  ignore("Part 2 input answer") {
+    assert(Part2.key64Index(input) == 20864)
   }
 }
