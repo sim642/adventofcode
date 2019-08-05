c9c432b4fa1a4a33e1c172057b2af7a8c672bf8a
Simmo Saan <simmo.saan@gmail.com>
2019-08-05 21:43:56 +0300

    Optimize 2016 day 16 part 2 by avoiding intermediate Strings

--- src/main/scala/eu/sim642/adventofcode2016/Day16.scala
+++ src/main/scala/eu/sim642/adventofcode2016/Day16.scala
@@ -3,20 +3,32 @@ package eu.sim642.adventofcode2016
 object Day16 {
 
   def generateData(initial: String, length: Int): String = {
-    val it = Iterator.iterate(initial)(s => s + "0" + s.reverseMap({ case '0' => '1' case '1' => '0' }))
-    it.find(_.length >= length).get.take(length)
+    val sb = new StringBuilder(length, initial)
+    while (sb.length < length) {
+      //val s = sb.mkString
+      sb.append('0')
+      //sb.append(s.reverseMap({ case '0' => '1' case '1' => '0' }))
+      //s.reverseIterator.take(length - sb.length).map({ case '0' => '1' case '1' => '0' }).foreach(sb.append)
+      sb.view(0, sb.length - 1).reverseIterator.take(length - sb.length).map({ case '0' => '1' case '1' => '0' }).foreach(sb.append)
+    }
+    //sb.substring(0, length)
+    sb.mkString
   }
 
   def checksum(data: String): String = {
-    val newData = data.grouped(2).map({
-      case "00" | "11" => '1'
-      case "01" | "10" => '0'
-    }).mkString
 
-    if (newData.length % 2 == 0)
-      checksum(newData)
-    else
-      newData
+    def reduce(it: Iterator[Char]): Iterator[Char] = {
+      it.grouped(2).map(s => if (s(0) == s(1)) '1' else '0')
+    }
+
+    def helper(it: Iterator[Char], length: Int): Iterator[Char] = {
+      if (length % 2 == 0)
+        helper(reduce(it), length / 2)
+      else
+        it
+    }
+
+    helper(data.iterator, data.length).mkString
   }
 
   def fillChecksum(initial: String, length: Int): String = checksum(generateData(initial, length))
@@ -31,4 +43,10 @@ object Day16 {
     println(fillChecksum(input, part1Length))
     println(fillChecksum(input, part2Length)) // TODO: optimize without generating entire string
   }
+
+  /*
+  10000 0 11110 0 10000 1 11|110
+   0 1  1  1 1  1  0 1  0  1| 1
+     0     1    1    0     0
+   */
 }
