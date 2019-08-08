5a1112dffef795755429656015cc7bc2fff1b41a
Simmo Saan <simmo.saan@gmail.com>
2019-08-08 11:29:45 +0300

    Solve 2016 day 24 part 1

---
+++ src/main/scala/eu/sim642/adventofcode2016/Day24.scala
@@ -0,0 +1,64 @@
+package eu.sim642.adventofcode2016
+
+import eu.sim642.adventofcode2017.Day19.Grid
+import eu.sim642.adventofcode2017.Day3.Pos
+import eu.sim642.adventofcodelib.graph.{BFS, GraphTraversal, UnitNeighbors}
+import eu.sim642.adventofcode2017.Day14.PosGrid
+
+object Day24 {
+
+  def findPois(grid: Grid[Char]): Map[Int, Pos] = {
+    (for {
+      (row, y) <- grid.zipWithIndex.iterator
+      (cell, x) <- row.zipWithIndex.iterator
+      if cell.isDigit
+    } yield cell.asDigit -> Pos(x, y)).toMap
+  }
+
+  def getPoiDistMatrix(grid: Grid[Char], pois: Map[Int, Pos]): Map[Int, Map[Int, Int]] = {
+    pois.mapValues({ fromPos =>
+
+      val graphTraversal = new GraphTraversal[Pos] with UnitNeighbors[Pos] {
+        override val startNode: Pos = fromPos
+
+        override def unitNeighbors(pos: Pos): TraversableOnce[Pos] = {
+          for {
+            offset <- Pos.axisOffsets
+            newPos = pos + offset
+            if grid(newPos) != '#'
+          } yield newPos
+        }
+      }
+
+      val distances = BFS.traverse(graphTraversal).distances
+      pois.mapValues(distances)
+    })
+  }
+
+  def tsp[A](distMatrix: Map[A, Map[A, Int]], start: A): Int = {
+    (distMatrix.keySet - start).toList
+      .permutations
+      .map(start :: _)
+      .map({ path =>
+        path.zip(path.tail)
+          .map({ case (from, to) => distMatrix(from)(to) })
+          .sum
+      }).min
+  }
+
+  def shortestRoute(grid: Grid[Char]): Int = {
+    val pois = findPois(grid)
+    val distMatrix = getPoiDistMatrix(grid, pois)
+    tsp(distMatrix, 0)
+  }
+
+  def parseGrid(input: String): Grid[Char] = input.lines.map(_.toVector).toVector
+
+  def shortestRoute(input: String): Int = shortestRoute(parseGrid(input))
+
+  lazy val input: String = io.Source.fromInputStream(getClass.getResourceAsStream("day24.txt")).mkString.trim
+
+  def main(args: Array[String]): Unit = {
+    println(shortestRoute(input))
+  }
+}
---
+++ src/test/scala/eu/sim642/adventofcode2016/Day24Test.scala
@@ -0,0 +1,33 @@
+package eu.sim642.adventofcode2016
+
+import org.scalatest.FunSuite
+import Day24._
+import eu.sim642.adventofcode2017.Day3.Pos
+
+class Day24Test extends FunSuite {
+
+  val exampleInput =
+    """###########
+      |#0.1.....2#
+      |#.#######.#
+      |#4.......3#
+      |###########""".stripMargin
+
+  test("findPois") {
+    assert(findPois(parseGrid(exampleInput)) == Map(
+      0 -> Pos(1, 1),
+      1 -> Pos(3, 1),
+      2 -> Pos(9, 1),
+      3 -> Pos(9, 3),
+      4 -> Pos(1, 3),
+    ))
+  }
+
+  test("Part 1 examples") {
+    assert(shortestRoute(exampleInput) == 14)
+  }
+
+  test("Part 1 input answer") {
+    assert(shortestRoute(input) == 470)
+  }
+}
