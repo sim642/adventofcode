63c9a701a57eb8c562af0cb389b5053e00f8b2c4
Simmo Saan <simmo.saan@gmail.com>
2019-08-21 17:03:36 +0300

    Optimize 2018 day 6 part 1 using boolean flag in minStrict

--- src/main/scala/eu/sim642/adventofcodelib/TraversableOnceImplicits.scala
+++ src/main/scala/eu/sim642/adventofcodelib/TraversableOnceImplicits.scala
@@ -1,7 +1,5 @@
 package eu.sim642.adventofcodelib
 
-import scala.math.Ordering.Implicits._
-
 // TODO: in Scala 2.13 convert to IterableOnce
 object TraversableOnceImplicits {
 
@@ -10,14 +8,21 @@ object TraversableOnceImplicits {
     def maxOption(implicit ordering: Ordering[A]): Option[A] = coll.reduceOption(ordering.max)
 
     def minStrict(implicit ordering: Ordering[A]): Option[A] = {
-      val minOptions = coll.foldLeft((Option.empty[A], Option.empty[A]))({
-        case (minOptions@(_, Some(min2)), x) if min2 <= x => minOptions
-        case ((minOption@Some(min), _), x) if min <= x => (minOption, Some(x))
-        case ((_, min2Option), x) => (Some(x), min2Option)
+      // optional min value, min value duplication flag
+      val acc = coll.foldLeft((Option.empty[A], false))({
+        case ((None, _), x) => (Some(x), false)
+        case (acc@(minOption@Some(min), _), x) =>
+          val c = ordering.compare(x, min)
+          if (c < 0)
+            (Some(x), false)
+          else if (c == 0)
+            (minOption, true)
+          else
+            acc
       })
 
-      minOptions match {
-        case (Some(min), Some(min2)) if min equiv min2 => None // equiv uses ordering, == doesn't!
+      acc match {
+        case (Some(_), true) => None
         case (minOption, _) => minOption
       }
     }
