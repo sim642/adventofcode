a88a31cb5eeca1e1f463df5bff41456f61c381a2
Simmo Saan <simmo.saan@gmail.com>
2019-08-27 20:50:24 +0300

    Optimize 2015 day 6 using mutable

--- src/main/scala/eu/sim642/adventofcode2015/Day6.scala
+++ src/main/scala/eu/sim642/adventofcode2015/Day6.scala
@@ -4,6 +4,8 @@ import eu.sim642.adventofcodelib.box.Box
 import eu.sim642.adventofcodelib.pos.Pos
 import eu.sim642.adventofcodelib.GridImplicits._
 
+import scala.collection.mutable
+
 object Day6 {
 
   sealed trait Operation
@@ -14,35 +16,35 @@ object Day6 {
   case class Instruction(operation: Operation, box: Box)
 
   def countLit(instructions: Seq[Instruction]): Int = {
-    // TODO: optimize, possibly using mutable
-    val initialGrid = Vector.fill(1000, 1000)(false)
-    val finalGrid = instructions.foldLeft(initialGrid)({ case (grid, instruction) =>
-      instruction.box.iterator.foldLeft(grid)({ case (grid, pos) =>
-        grid.updatedGrid(pos, instruction.operation match {
+    // TODO: abstract GridImplicits to work on mutable
+    val grid = mutable.ArrayBuffer.fill(1000, 1000)(false)
+    for (instruction <- instructions) {
+      for (pos <- instruction.box.iterator) {
+        grid(pos.y)(pos.x) = instruction.operation match {
           case TurnOn => true
           case TurnOff => false
-          case Toggle => !grid(pos)
-        })
-      })
-    })
-    finalGrid.countGrid(identity)
+          case Toggle => !grid(pos.y)(pos.x)
+        }
+      }
+    }
+    grid.iterator.map(_.count(identity)).sum
   }
 
   def countLit(input: String): Int = countLit(parseInstructions(input))
 
   def totalBrightness(instructions: Seq[Instruction]): Int = {
-    // TODO: optimize, possibly using mutable
-    val initialGrid = Vector.fill(1000, 1000)(0)
-    val finalGrid = instructions.foldLeft(initialGrid)({ case (grid, instruction) =>
-      instruction.box.iterator.foldLeft(grid)({ case (grid, pos) =>
-        grid.updatedGrid(pos, instruction.operation match {
-          case TurnOn => grid(pos) + 1
-          case TurnOff => (grid(pos) - 1) max 0
-          case Toggle => grid(pos) + 2
-        })
-      })
-    })
-    finalGrid.sumGrid
+    // TODO: abstract GridImplicits to work on mutable
+    val grid = mutable.ArrayBuffer.fill(1000, 1000)(0)
+    for (instruction <- instructions) {
+      for (pos <- instruction.box.iterator) {
+        grid(pos.y)(pos.x) = instruction.operation match {
+          case TurnOn => grid(pos.y)(pos.x) + 1
+          case TurnOff => (grid(pos.y)(pos.x) - 1) max 0
+          case Toggle => grid(pos.y)(pos.x) + 2
+        }
+      }
+    }
+    grid.iterator.map(_.sum).sum
   }
 
   def totalBrightness(input: String): Int = totalBrightness(parseInstructions(input))
