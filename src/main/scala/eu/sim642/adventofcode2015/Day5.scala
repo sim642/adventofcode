0acc15f5038d1f98f43a429b7f3bab689c4364d2
Simmo Saan <simmo.saan@gmail.com>
2019-08-27 18:23:40 +0300

    Solve 2015 day 5 part 2

--- src/main/scala/eu/sim642/adventofcode2015/Day5.scala
+++ src/main/scala/eu/sim642/adventofcode2015/Day5.scala
@@ -2,22 +2,38 @@ package eu.sim642.adventofcode2015
 
 object Day5 {
 
-  private val naughtyStrings = Set("ab", "cd", "pq", "xy")
+  trait Part {
+    def isNice(s: String): Boolean
 
-  def isNice(s: String): Boolean = {
-    val cond1 = s.count("aeiou".contains(_)) >= 3
-    val cond2 = (s lazyZip s.tail).exists({ case (c1, c2) => c1 == c2 })
-    val cond3 = naughtyStrings.forall(!s.contains(_))
-    cond1 && cond2 && cond3
+    def countNice(ss: Seq[String]): Int = ss.count(isNice)
+    def countNice(input: String): Int = countNice(input.linesIterator.toSeq)
   }
 
-  def countNice(ss: Seq[String]): Int = ss.count(isNice)
+  object Part1 extends Part {
+    private val naughtyStrings = Set("ab", "cd", "pq", "xy")
 
-  def countNice(input: String): Int = countNice(input.linesIterator.toSeq)
+    override def isNice(s: String): Boolean = {
+      // TODO: regex would be faster here?
+      val cond1 = s.count("aeiou".contains(_)) >= 3
+      val cond2 = (s lazyZip s.tail).exists({ case (c1, c2) => c1 == c2 })
+      val cond3 = naughtyStrings.forall(!s.contains(_))
+      cond1 && cond2 && cond3
+    }
+  }
+
+  object Part2 extends Part {
+    private val xyxyRegex = """(..).*\1""".r
+    private val xyxRegex = """(.).\1""".r
+
+    override def isNice(s: String): Boolean = {
+      xyxyRegex.findFirstIn(s).isDefined && xyxRegex.findFirstIn(s).isDefined
+    }
+  }
 
   lazy val input: String = io.Source.fromInputStream(getClass.getResourceAsStream("day5.txt")).mkString.trim
 
   def main(args: Array[String]): Unit = {
-    println(countNice(input))
+    println(Part1.countNice(input))
+    println(Part2.countNice(input))
   }
 }
--- src/test/scala/eu/sim642/adventofcode2015/Day5Test.scala
+++ src/test/scala/eu/sim642/adventofcode2015/Day5Test.scala
@@ -6,14 +6,25 @@ import Day5._
 class Day5Test extends FunSuite {
 
   test("Part 1 examples") {
-    assert(isNice("ugknbfddgicrmopn"))
-    assert(isNice("aaa"))
-    assert(!isNice("jchzalrnumimnmhp"))
-    assert(!isNice("haegwjzuvuyypxyu"))
-    assert(!isNice("dvszwmarrgswjxmb"))
+    assert(Part1.isNice("ugknbfddgicrmopn"))
+    assert(Part1.isNice("aaa"))
+    assert(!Part1.isNice("jchzalrnumimnmhp"))
+    assert(!Part1.isNice("haegwjzuvuyypxyu"))
+    assert(!Part1.isNice("dvszwmarrgswjxmb"))
   }
 
   test("Part 1 input answer") {
-    assert(countNice(input) == 238)
+    assert(Part1.countNice(input) == 238)
+  }
+
+  test("Part 2 examples") {
+    assert(Part2.isNice("qjhvhtzxzqqjkmpb"))
+    assert(Part2.isNice("xxyxx"))
+    assert(!Part2.isNice("uurcxstgmygtbstg"))
+    assert(!Part2.isNice("ieodomkazucvgmuy"))
+  }
+
+  test("Part 2 input answer") {
+    assert(Part2.countNice(input) == 69)
   }
 }
