d854bc6f56346ffebf761117749b202ad28d60f1
Simmo Saan <simmo.saan@gmail.com>
2019-09-04 20:57:28 +0300

    Solve 2015 day 17 part 2

--- src/main/scala/eu/sim642/adventofcode2015/Day17.scala
+++ src/main/scala/eu/sim642/adventofcode2015/Day17.scala
@@ -17,14 +17,48 @@ object Day17 {
     }
   }
 
-  def countCombinations(input: String, remaining: Int = 150): Int = {
-    val sizes = input.linesIterator.map(_.toInt).toList
-    countCombinations(sizes, remaining)
+  def countCombinations(input: String, remaining: Int = 150): Int = countCombinations(parseSizes(input), remaining)
+
+  def countMinCombinations(sizes: List[Int], remaining: Int): Int = {
+    // TODO: prune by length exceeding found length
+    def helper(sizes: List[Int], length: Int, remaining: Int): Option[(Int, Int)] = {
+      if (remaining == 0)
+        Some((length, 1))
+      else if (remaining < 0)
+        None
+      else {
+        sizes match {
+          case Nil => None
+          case hd :: tl =>
+            // TODO: simplify the match and if
+            (helper(tl, length, remaining), helper(tl, length + 1, remaining - hd)) match {
+              case (None, None) => None
+              case (left, None) => left
+              case (None, right) => right
+              case (left@Some((leftLength, leftCount)), right@Some((rightLength, rightCount))) =>
+                if (leftLength < rightLength)
+                  left
+                else if (leftLength > rightLength)
+                  right
+                else
+                  Some((leftLength, leftCount + rightCount))
+            }
+        }
+      }
+    }
+
+    helper(sizes, 0, remaining).map(_._2).getOrElse(0)
   }
+
+  def countMinCombinations(input: String, remaining: Int = 150): Int = countMinCombinations(parseSizes(input), remaining)
+
+
+  def parseSizes(input: String): List[Int] = input.linesIterator.map(_.toInt).toList
 
   lazy val input: String = io.Source.fromInputStream(getClass.getResourceAsStream("day17.txt")).mkString.trim
 
   def main(args: Array[String]): Unit = {
     println(countCombinations(input))
+    println(countMinCombinations(input))
   }
 }
--- src/test/scala/eu/sim642/adventofcode2015/Day17Test.scala
+++ src/test/scala/eu/sim642/adventofcode2015/Day17Test.scala
@@ -12,4 +12,12 @@ class Day17Test extends FunSuite {
   test("Part 1 input answer") {
     assert(countCombinations(input) == 654)
   }
+
+  test("Part 2 examples") {
+    assert(countMinCombinations(List(20, 15, 10, 5, 5), 25) == 3)
+  }
+
+  test("Part 2 input answer") {
+    assert(countMinCombinations(input) == 57)
+  }
 }
