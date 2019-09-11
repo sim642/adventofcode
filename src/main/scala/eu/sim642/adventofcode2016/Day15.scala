e752e89f752ddf651e6122b103b592c8187669cd
Simmo Saan <simmo.saan@gmail.com>
2019-09-11 22:25:40 +0300

    Move extended GCD and CRT to library

--- src/main/scala/eu/sim642/adventofcode2016/Day15.scala
+++ src/main/scala/eu/sim642/adventofcode2016/Day15.scala
@@ -1,8 +1,7 @@
 package eu.sim642.adventofcode2016
 
-import scala.annotation.tailrec
-
 import eu.sim642.adventofcodelib.IntegralImplicits._
+import eu.sim642.adventofcodelib.NumberTheory
 
 object Day15 {
 
@@ -16,36 +15,6 @@ object Day15 {
   // t = 1 (mod 2)
   // t = 5
 
-  // TODO: move gcd things to library
-  def extendedGcd(a: Int, b: Int): ((Int, Int), Int, (Int, Int)) = {
-
-    @tailrec
-    def helper(s: Int, oldS: Int, t: Int, oldT: Int, r: Int, oldR: Int): ((Int, Int), Int, (Int, Int)) = {
-      if (r == 0)
-        ((oldS, oldT), oldR, (s, t))
-      else {
-        val q = oldR / r
-        helper(oldS - q * s, s, oldT - q * t, t, oldR - q * r, r)
-      }
-    }
-
-    helper(0, 1, 1, 0, b, a)
-  }
-
-  def bezoutCoefs(a: Int, b: Int): (Int, Int) = extendedGcd(a, b)._1
-
-  def crt2(an1: (Int, Int), an2: (Int, Int)): (Int, Int) = {
-    val (a1, n1) = an1
-    val (a2, n2) = an2
-    val (m1, m2) = bezoutCoefs(n1, n2)
-    val N = n1 * n2
-    // TODO: remove overflow avoiding hack, generalize to Numeric?
-    val x = a1.toLong * m2 * n2 + a2 * m1 * n1
-    ((x %+ N.toLong).toInt, N)
-  }
-
-  def crt(ans: Seq[(Int, Int)]): (Int, Int) = ans.reduce(crt2)
-
   trait Part {
     def firstPressTime(disks: Seq[Disk]): Int
 
@@ -55,7 +24,7 @@ object Day15 {
   object Part1 extends Part {
     override def firstPressTime(disks: Seq[Disk]): Int = {
       val ans = disks.map({ case Disk(i, posCount, initialPos) => ((-(initialPos + i)) %+ posCount, posCount) })
-      crt(ans)._1
+      NumberTheory.crt(ans)._1
     }
   }
 
---
+++ src/main/scala/eu/sim642/adventofcodelib/NumberTheory.scala
@@ -0,0 +1,36 @@
+package eu.sim642.adventofcodelib
+
+import scala.annotation.tailrec
+import eu.sim642.adventofcodelib.IntegralImplicits._
+
+object NumberTheory {
+
+  def extendedGcd(a: Int, b: Int): ((Int, Int), Int, (Int, Int)) = {
+
+    @tailrec
+    def helper(s: Int, oldS: Int, t: Int, oldT: Int, r: Int, oldR: Int): ((Int, Int), Int, (Int, Int)) = {
+      if (r == 0)
+        ((oldS, oldT), oldR, (s, t))
+      else {
+        val q = oldR / r
+        helper(oldS - q * s, s, oldT - q * t, t, oldR - q * r, r)
+      }
+    }
+
+    helper(0, 1, 1, 0, b, a)
+  }
+
+  def bezoutCoefs(a: Int, b: Int): (Int, Int) = extendedGcd(a, b)._1
+
+  private def crt2(an1: (Int, Int), an2: (Int, Int)): (Int, Int) = {
+    val (a1, n1) = an1
+    val (a2, n2) = an2
+    val (m1, m2) = bezoutCoefs(n1, n2)
+    val N = n1 * n2
+    // TODO: remove overflow avoiding hack, generalize to Numeric?
+    val x = a1.toLong * m2 * n2 + a2 * m1 * n1
+    ((x %+ N.toLong).toInt, N)
+  }
+
+  def crt(ans: Seq[(Int, Int)]): (Int, Int) = ans.reduce(crt2)
+}
