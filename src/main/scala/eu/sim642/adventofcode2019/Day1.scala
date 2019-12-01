6444ddabfdbaf34200744b976212a5fbd7bb6b62
Simmo Saan <simmo.saan@gmail.com>
2019-12-01 10:12:23 +0200

    Solve 2019 day 1 part 2

--- src/main/scala/eu/sim642/adventofcode2019/Day1.scala
+++ src/main/scala/eu/sim642/adventofcode2019/Day1.scala
@@ -2,15 +2,32 @@ package eu.sim642.adventofcode2019
 
 object Day1 {
 
-  def requiredFuel(mass: Int): Int = mass / 3 - 2
+  trait Part {
+    def requiredFuel(mass: Int): Int
 
-  def totalRequiredFuel(masses: Seq[Int]): Int = masses.map(requiredFuel).sum
+    def totalRequiredFuel(masses: Seq[Int]): Int = masses.map(requiredFuel).sum
+  }
+
+  object Part1 extends Part {
+    override def requiredFuel(mass: Int): Int = mass / 3 - 2
+  }
+
+  object Part2 extends Part {
+    override def requiredFuel(mass: Int): Int = {
+      val fuel = Part1.requiredFuel(mass)
+      if (fuel <= 0)
+        0
+      else
+        fuel + requiredFuel(fuel)
+    }
+  }
 
   def parseMasses(input: String): Seq[Int] = input.linesIterator.map(_.toInt).toSeq
 
   lazy val input: String = io.Source.fromInputStream(getClass.getResourceAsStream("day1.txt")).mkString.trim
 
   def main(args: Array[String]): Unit = {
-    println(totalRequiredFuel(parseMasses(input)))
+    println(Part1.totalRequiredFuel(parseMasses(input)))
+    println(Part2.totalRequiredFuel(parseMasses(input)))
   }
 }
--- src/test/scala/eu/sim642/adventofcode2019/Day1Test.scala
+++ src/test/scala/eu/sim642/adventofcode2019/Day1Test.scala
@@ -6,13 +6,23 @@ import Day1._
 class Day1Test extends FunSuite {
 
   test("Part 1 examples") {
-    assert(requiredFuel(12) == 2)
-    assert(requiredFuel(14) == 2)
-    assert(requiredFuel(1969) == 654)
-    assert(requiredFuel(100756) == 33583)
+    assert(Part1.requiredFuel(12) == 2)
+    assert(Part1.requiredFuel(14) == 2)
+    assert(Part1.requiredFuel(1969) == 654)
+    assert(Part1.requiredFuel(100756) == 33583)
   }
 
   test("Part 1 input answer") {
-    assert(totalRequiredFuel(parseMasses(input)) == 3371958)
+    assert(Part1.totalRequiredFuel(parseMasses(input)) == 3371958)
+  }
+
+  test("Part 2 examples") {
+    assert(Part2.requiredFuel(12) == 2)
+    assert(Part2.requiredFuel(1969) == 966)
+    assert(Part2.requiredFuel(100756) == 50346)
+  }
+
+  test("Part 2 input answer") {
+    assert(Part2.totalRequiredFuel(parseMasses(input)) == 5055050)
   }
 }
