69e9e73490ff6eb3f2820e2aa5b2bbf4d0680e58
Simmo Saan <simmo.saan@gmail.com>
2019-12-01 20:36:38 +0200

    Add 2019 day 1 part 2 closed form solution

--- src/main/scala/eu/sim642/adventofcode2019/Day1.scala
+++ src/main/scala/eu/sim642/adventofcode2019/Day1.scala
@@ -12,7 +12,9 @@ object Day1 {
     override def requiredFuel(mass: Int): Int = mass / 3 - 2
   }
 
-  object Part2 extends Part {
+  trait Part2Solution extends Part
+
+  object RecursivePart2Solution extends Part2Solution {
     override def requiredFuel(mass: Int): Int = {
       val fuel = Part1.requiredFuel(mass)
       if (fuel <= 0)
@@ -22,12 +24,23 @@ object Day1 {
     }
   }
 
+  object ClosedFormPart2Solution extends Part2Solution {
+    override def requiredFuel(mass: Int): Int = {
+      val cols = (math.log((mass + 3.0) / 4) / math.log(3)).floor.toInt
+      (1 to cols).map({ i =>
+        val p = math.pow(3, i).toInt
+        //0 max (((mass - (p - 3)) / p) - 2)
+        0 max (((mass + 3.0) / p).floor.toInt - 3)
+      }).sum
+    }
+  }
+
   def parseMasses(input: String): Seq[Int] = input.linesIterator.map(_.toInt).toSeq
 
   lazy val input: String = io.Source.fromInputStream(getClass.getResourceAsStream("day1.txt")).mkString.trim
 
   def main(args: Array[String]): Unit = {
     println(Part1.totalRequiredFuel(parseMasses(input)))
-    println(Part2.totalRequiredFuel(parseMasses(input)))
+    println(RecursivePart2Solution.totalRequiredFuel(parseMasses(input)))
   }
 }
--- src/test/scala/eu/sim642/adventofcode2019/Day1Test.scala
+++ src/test/scala/eu/sim642/adventofcode2019/Day1Test.scala
@@ -1,43 +1,69 @@
 package eu.sim642.adventofcode2019
 
-import org.scalatest.FunSuite
+import org.scalatest.{FunSuite, Suites}
 import Day1._
+import eu.sim642.adventofcode2019.Day1Test._
 import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
 
-class Day1Test extends FunSuite with ScalaCheckPropertyChecks {
+class Day1Test extends Suites(
+  new BaseTest,
+  new RecursivePart2SolutionTest,
+  new ClosedFormPart2SolutionTest,
+)
 
-  test("Part 1 examples") {
-    val massExpectedFuels = Table(
-      ("mass", "expectedFuel"),
-      (12, 2),
-      (14, 2),
-      (1969, 654),
-      (100756, 33583),
-    )
+object Day1Test {
 
-    forAll (massExpectedFuels) { (mass, expectedFuel) =>
-      assert(Part1.requiredFuel(mass) == expectedFuel)
+  class BaseTest extends FunSuite with ScalaCheckPropertyChecks {
+
+    test("Part 1 examples") {
+      val massExpectedFuels = Table(
+        ("mass", "expectedFuel"),
+        (12, 2),
+        (14, 2),
+        (1969, 654),
+        (100756, 33583),
+      )
+
+      forAll (massExpectedFuels) { (mass, expectedFuel) =>
+        assert(Part1.requiredFuel(mass) == expectedFuel)
+      }
+    }
+
+    test("Part 1 input answer") {
+      assert(Part1.totalRequiredFuel(parseMasses(input)) == 3371958)
     }
   }
 
-  test("Part 1 input answer") {
-    assert(Part1.totalRequiredFuel(parseMasses(input)) == 3371958)
-  }
+  sealed abstract class Part2SolutionTest(part2Solution: Part2Solution) extends FunSuite with ScalaCheckPropertyChecks {
 
-  test("Part 2 examples") {
-    val massExpectedFuels = Table(
-      ("mass", "expectedFuel"),
-      (12, 2),
-      (1969, 966),
-      (100756, 50346),
-    )
+    test("Part 2 examples") {
+      val massExpectedFuels = Table(
+        ("mass", "expectedFuel"),
+        (12, 2),
+        (1969, 966),
+        (100756, 50346),
+      )
 
-    forAll (massExpectedFuels) { (mass, expectedFuel) =>
-      assert(Part2.requiredFuel(mass) == expectedFuel)
+      forAll (massExpectedFuels) { (mass, expectedFuel) =>
+        assert(part2Solution.requiredFuel(mass) == expectedFuel)
+      }
+    }
+
+    test("Part 2 input answer") {
+      assert(part2Solution.totalRequiredFuel(parseMasses(input)) == 5055050)
     }
   }
 
-  test("Part 2 input answer") {
-    assert(Part2.totalRequiredFuel(parseMasses(input)) == 5055050)
+  class RecursivePart2SolutionTest extends Part2SolutionTest(RecursivePart2Solution)
+
+  class ClosedFormPart2SolutionTest extends Part2SolutionTest(ClosedFormPart2Solution) {
+
+    test("Equivalent to recursive") {
+      forAll ("mass") { mass: Int =>
+        whenever (mass >= 0) {
+          assert(ClosedFormPart2Solution.requiredFuel(mass) == RecursivePart2Solution.requiredFuel(mass))
+        }
+      }
+    }
   }
 }
