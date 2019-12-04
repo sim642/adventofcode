df56dd3817586796b245c407e7afd877f1340503
Simmo Saan <simmo.saan@gmail.com>
2019-12-04 10:07:30 +0200

    Solve 2019 day 4 part 2

--- src/main/scala/eu/sim642/adventofcode2019/Day4.scala
+++ src/main/scala/eu/sim642/adventofcode2019/Day4.scala
@@ -1,15 +1,36 @@
 package eu.sim642.adventofcode2019
 
+import scala.collection.immutable.ArraySeq
+
 object Day4 {
 
-  def isPassword(number: Int): Boolean = {
-    val digits = number.toString.toList.map(_.asDigit)
-    val deltas = (digits.tail lazyZip digits).map(_ - _)
-    deltas.contains(0) && // two adjacent are the same
-      deltas.forall(_ >= 0) // digits never decrease
+  trait Part {
+    def isPassword(number: Int): Boolean
+
+    def countPasswords(range: Range.Inclusive): Int = range.count(isPassword)
   }
 
-  def countPasswords(range: Range.Inclusive): Int = range.count(isPassword)
+  object Part1 extends Part {
+    override def isPassword(number: Int): Boolean = {
+      val digits = number.toString.toList.map(_.asDigit)
+      val deltas = (digits.tail lazyZip digits).map(_ - _)
+      deltas.contains(0) && // two adjacent are the same
+        deltas.forall(_ >= 0) // digits never decrease
+    }
+  }
+
+  object Part2 extends Part {
+    override def isPassword(number: Int): Boolean = {
+      val digits = number.toString.to(ArraySeq).map(_.asDigit)
+      val deltas = (digits.tail lazyZip digits).map(_ - _)
+      deltas.indices.exists({ i =>
+        deltas(i) == 0 && // two adjacent are the same
+          (i == 0 || deltas(i - 1) != 0) && // not same before
+          (i == deltas.size - 1 || deltas(i + 1) != 0) // not same after
+      }) && // TODO: make less ugly
+        deltas.forall(_ >= 0) // digits never decrease
+    }
+  }
 
   def parseRange(input: String): Range.Inclusive = {
     val Seq(lo, hi) = input.split('-').toSeq.map(_.toInt)
@@ -20,6 +41,10 @@ object Day4 {
   val input: String = "278384-824795"
 
   def main(args: Array[String]): Unit = {
-    println(countPasswords(parseRange(input)))
+    println(Part1.countPasswords(parseRange(input)))
+    println(Part2.countPasswords(parseRange(input)))
+
+    // 673 - too high
+    // 544 - too low
   }
 }
--- src/test/scala/eu/sim642/adventofcode2019/Day4Test.scala
+++ src/test/scala/eu/sim642/adventofcode2019/Day4Test.scala
@@ -6,12 +6,24 @@ import Day4._
 class Day4Test extends FunSuite {
 
   test("Part 1 examples") {
-    assert(isPassword(111111))
-    assert(!isPassword(223450))
-    assert(!isPassword(123789))
+    assert(Part1.isPassword(111111))
+    assert(!Part1.isPassword(223450))
+    assert(!Part1.isPassword(123789))
   }
 
   test("Part 1 input answer") {
-    assert(countPasswords(parseRange(input)) == 921)
+    assert(Part1.countPasswords(parseRange(input)) == 921)
+  }
+
+  test("Part 2 examples") {
+    assert(Part2.isPassword(112233))
+    assert(!Part2.isPassword(123444))
+    assert(Part2.isPassword(111122))
+
+    assert(Part2.isPassword(112222))
+  }
+
+  test("Part 2 input answer") {
+    assert(Part2.countPasswords(parseRange(input)) == 603)
   }
 }
