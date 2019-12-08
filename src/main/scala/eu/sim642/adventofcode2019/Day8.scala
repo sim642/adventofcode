2991b7c86230d37a4a606874a9bf320f3c6e2b00
Simmo Saan <simmo.saan@gmail.com>
2019-12-08 09:41:59 +0200

    Solve 2019 day 8 part 2

--- src/main/scala/eu/sim642/adventofcode2019/Day8.scala
+++ src/main/scala/eu/sim642/adventofcode2019/Day8.scala
@@ -11,6 +11,35 @@ object Day8 {
     layer.countGrid(_ == 1) * layer.countGrid(_ == 2)
   }
 
+  def stackLayers(layers: Seq[Grid[Int]]): Grid[Int] = {
+    def stack2(top: Grid[Int], bottom: Grid[Int]): Grid[Int] = {
+      // TODO: zip for Grid?
+      (for ((topRow, bottomRow) <- top lazyZip bottom)
+        yield (for ((topCell, bottomCell) <- topRow lazyZip bottomRow)
+          yield {
+            (topCell, bottomCell) match {
+              case (2, bottomCell) => bottomCell // top transparent
+              case (topCell, _) => topCell
+            }
+          }).toVector
+        ).toVector
+    }
+
+    layers.reduce(stack2)
+  }
+
+  // copied & modified from 2018 Day 18
+  def printGrid(grid: Grid[Int]): Unit = {
+    for (row <- grid) {
+      for (cell <- row)
+        print(cell match {
+          case 0 => '#' // black
+          case 1 => '.' // white
+        })
+      println()
+    }
+  }
+
   def parseLayers(input: String, width: Int = 25, height: Int = 6): Seq[Grid[Int]] = {
     input.toVector.map(_.asDigit).grouped(width * height).map(_.grouped(width).toVector).toSeq
   }
@@ -19,5 +48,6 @@ object Day8 {
 
   def main(args: Array[String]): Unit = {
     println(fewestZeroLayer(parseLayers(input)))
+    printGrid(stackLayers(parseLayers(input))) // EHRUE
   }
 }
--- src/test/scala/eu/sim642/adventofcode2019/Day8Test.scala
+++ src/test/scala/eu/sim642/adventofcode2019/Day8Test.scala
@@ -6,6 +6,7 @@ import Day8._
 class Day8Test extends FunSuite {
 
   val exampleInput = "123456789012"
+  val exampleInput2 = "0222112222120000"
 
   test("parseLayers") {
     assert(parseLayers(exampleInput, 3, 2) ==
@@ -24,5 +25,30 @@ class Day8Test extends FunSuite {
 
   test("Part 1 input answer") {
     assert(fewestZeroLayer(parseLayers(input)) == 2975)
+  }
+
+  test("Part 2 examples") {
+    assert(stackLayers(parseLayers(exampleInput2, 2, 2)) ==
+      Vector(
+        Vector(0, 1),
+        Vector(1, 0),
+      )
+    )
+  }
+
+  test("Part 2 input answer") {
+    val stacked = stackLayers(parseLayers(input))
+    assert(stacked ==
+      Vector(
+        Vector(1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0),
+        Vector(1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0),
+        Vector(1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 0),
+        Vector(1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0),
+        Vector(1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0),
+        Vector(1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0),
+      )
+    )
+
+    printGrid(stacked) // EHRUE
   }
 }
