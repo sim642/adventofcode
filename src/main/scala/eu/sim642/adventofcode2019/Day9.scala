f21de8be0e1d28da66f32a112b6f4a97890e90ef
Simmo Saan <simmo.saan@gmail.com>
2019-12-09 09:14:47 +0200

    Solve 2019 day 9 part 1

---
+++ src/main/scala/eu/sim642/adventofcode2019/Day9.scala
@@ -0,0 +1,93 @@
+package eu.sim642.adventofcode2019
+
+object Day9 {
+
+  // copied & modified from 2019 Day 7
+
+  type Value = Long
+  type Address = Int
+  type Memory = Map[Address, Value]
+
+  case class ProgramState(memory: Memory,
+                          inputs: LazyList[Value] = LazyList.empty,
+                          ip: Int = 0,
+                          relativeBase: Int = 0) {
+
+    def instruction(i: Int): Value = memory(ip + i)
+    def opcode: Int = (instruction(0) % 100).toInt
+    def param(i: Int): Value = instruction(i + 1)
+    def paramMode(i: Int): Int = ((instruction(0) / math.pow(10, 2 + i).toInt) % 10).toInt // TODO: Int pow
+    def readParam(i: Int): Value = paramMode(i) match {
+      case 0 => memory(param(i).toInt)
+      case 1 => param(i)
+      case 2 => memory((relativeBase + param(i)).toInt)
+      case _ => throw new IllegalArgumentException(s"Illegal parameter read mode ${paramMode(i)}")
+    }
+    def writeParam(i: Int, value: Value): Memory = paramMode(i) match {
+      case 0 => memory.updated(param(i).toInt, value)
+      case 2 => memory.updated((relativeBase + param(i)).toInt, value)
+      case _ => throw new IllegalArgumentException(s"Illegal parameter write mode ${paramMode(i)}")
+    }
+
+    def execOne: Option[(ProgramState, Option[Value])] = {
+      opcode match {
+        case 1 => // add
+          val newValue = readParam(0) + readParam(1)
+          val newMemory = writeParam(2, newValue)
+          Some((copy(memory = newMemory, ip = ip + 4), None))
+        case 2 => // multiply
+          val newValue = readParam(0) * readParam(1)
+          val newMemory = writeParam(2, newValue)
+          Some((copy(memory = newMemory, ip = ip + 4), None))
+        case 3 => // input
+          val (input #:: newInputs) = inputs
+          val newMemory = writeParam(0, input)
+          Some((copy(memory = newMemory, ip = ip + 2, inputs = newInputs), None))
+        case 4 => // output
+          val newValue = readParam(0)
+          Some((copy(ip = ip + 2), Some(newValue)))
+        case 5 => // jump if true
+          if (readParam(0) != 0)
+            Some((copy(ip = readParam(1).toInt), None))
+          else
+            Some((copy(ip = ip + 3), None))
+        case 6 => // jump if false
+          if (readParam(0) == 0)
+            Some((copy(ip = readParam(1).toInt), None))
+          else
+            Some((copy(ip = ip + 3), None))
+        case 7 => // less than
+          val newValue = if (readParam(0) < readParam(1)) 1 else 0
+          val newMemory = writeParam(2, newValue)
+          Some((copy(memory = newMemory, ip = ip + 4), None))
+        case 8 => // equal
+          val newValue = if (readParam(0) == readParam(1)) 1 else 0
+          val newMemory = writeParam(2, newValue)
+          Some((copy(memory = newMemory, ip = ip + 4), None))
+        case 9 => // adjust relative base
+          val newRelativeBase = relativeBase + readParam(0)
+          Some((copy(ip = ip + 2, relativeBase = newRelativeBase.toInt), None))
+        case 99 => None
+        case _ => throw new IllegalArgumentException("Unknown opcode")
+      }
+    }
+
+    def outputs: LazyList[Value] = {
+      LazyList.unfold(this)(_.execOne.map(_.swap)).flatten
+    }
+  }
+
+  def boostKeyCode(program: Memory): Value = {
+    val outputs = ProgramState(program, LazyList(1)).outputs
+    assert(outputs.lengthIs == 1)
+    outputs.head
+  }
+
+  def parseProgram(input: String): Memory = input.split(',').view.zipWithIndex.map({ case (value, i) => i -> value.toLong}).toMap.withDefaultValue(0)
+
+  lazy val input: String = io.Source.fromInputStream(getClass.getResourceAsStream("day9.txt")).mkString.trim
+
+  def main(args: Array[String]): Unit = {
+    println(boostKeyCode(parseProgram(input)))
+  }
+}
