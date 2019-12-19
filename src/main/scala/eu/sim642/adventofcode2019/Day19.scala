46aefd45dccfc534d5b98a27145922a29e1a7c10
Simmo Saan <simmo.saan@gmail.com>
2019-12-19 09:42:25 +0200

    Solve 2019 day 19 part 2

--- src/main/scala/eu/sim642/adventofcode2019/Day19.scala
+++ src/main/scala/eu/sim642/adventofcode2019/Day19.scala
@@ -5,20 +5,47 @@ import eu.sim642.adventofcodelib.Grid
 import eu.sim642.adventofcodelib.GridImplicits._
 import eu.sim642.adventofcodelib.pos.Pos
 
+import scala.annotation.tailrec
+
 object Day19 {
+
+  def isTractorBeam(program: Memory, pos: Pos): Boolean = {
+    ProgramState(program, inputs = LazyList(pos.x, pos.y)).outputs.head != 0
+  }
 
   def getGrid(program: Memory, size: Pos): Grid[Boolean] = {
     (for (y <- 0 until size.y)
       yield (for (x <- 0 until size.x)
-        yield {
-          val output = ProgramState(program, inputs = LazyList(x, y)).outputs.head != 0
-          output
-        }
+        yield isTractorBeam(program, Pos(x, y))
       ).toVector
     ).toVector
   }
 
   def countTractorBeam(program: Memory, size: Pos = Pos(50, 50)): Int = getGrid(program, size).countGrid(identity)
+
+  def findSquarePos(program: Memory, size: Pos): Pos = {
+    val sizeOffset = Pos(-(size.x - 1), size.y - 1)
+
+    @tailrec
+    def helper(pos: Pos): Pos = {
+      //assert(isTractorBeam(program, pos))
+
+      if (isTractorBeam(program, pos + sizeOffset))
+        pos + Pos(-(size.x - 1), 0) // rectangle found
+      else if (isTractorBeam(program, pos + Pos(1, 0)))
+        helper(pos + Pos(1, 0)) // right
+      else
+        helper(pos + Pos(0, 1)) // down
+    }
+
+    val start = Pos(16, 19) // TODO: don't hardcode contiguous start position
+    helper(start)
+  }
+
+  def findSquareValue(program: Memory, size: Pos = Pos(100, 100)): Int = {
+    val Pos(x, y) = findSquarePos(program, size)
+    10000 * x + y
+  }
 
   def printGrid(grid: Grid[Boolean]): Unit = {
     for (row <- grid) {
@@ -34,5 +61,6 @@ object Day19 {
     printGrid(getGrid(parseProgram(input), Pos(50, 50)))
 
     println(countTractorBeam(parseProgram(input)))
+    println(findSquareValue(parseProgram(input)))
   }
 }
--- src/test/scala/eu/sim642/adventofcode2019/Day19Test.scala
+++ src/test/scala/eu/sim642/adventofcode2019/Day19Test.scala
@@ -9,4 +9,8 @@ class Day19Test extends FunSuite {
   test("Part 1 input answer") {
     assert(countTractorBeam(parseProgram(input)) == 110)
   }
+
+  test("Part 2 input answer") {
+    assert(findSquareValue(parseProgram(input)) == 17302065)
+  }
 }
