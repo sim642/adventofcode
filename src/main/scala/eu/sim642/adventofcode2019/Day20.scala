ddccf1f839140895fe9ce3c39fc54920814645b1
Simmo Saan <simmo.saan@gmail.com>
2019-12-20 09:22:43 +0200

    Solve 2019 day 20 part 2

--- src/main/scala/eu/sim642/adventofcode2019/Day20.scala
+++ src/main/scala/eu/sim642/adventofcode2019/Day20.scala
@@ -2,6 +2,7 @@ package eu.sim642.adventofcode2019
 
 import eu.sim642.adventofcodelib.Grid
 import eu.sim642.adventofcodelib.GridImplicits._
+import eu.sim642.adventofcodelib.box.Box
 import eu.sim642.adventofcodelib.graph._
 import eu.sim642.adventofcodelib.pos.Pos
 
@@ -54,6 +55,61 @@ object Day20 {
     BFS.search(graphSearch).target.get._2
   }
 
+  def stepsRecursive(grid: Grid[Tile]): Int = {
+
+    val startPortal = Portal('A', 'A')
+    val targetPortal = Portal('Z', 'Z')
+
+    val innerBox = Box(Pos(3, 3), Pos(grid.map(_.size).max - 1 - 3, grid.size - 1 - 3))
+
+    case class Node(pos: Pos, level: Int)
+
+    val graphSearch = new GraphSearch[Node] with UnitNeighbors[Node] with TargetNode[Node] {
+      override val startNode: Node = Node(grid.posOf(startPortal), 0)
+
+      override def unitNeighbors(node: Node): IterableOnce[Node] = {
+        val Node(pos, level) = node
+
+        val moveNeighbors = for {
+          offset <- Pos.axisOffsets.iterator
+          newPos = pos + offset
+          if grid(newPos) != Wall
+        } yield Node(newPos, level)
+
+        val portalNeighbors = grid(pos) match {
+          case portal@Portal(_, _) if portal != startPortal && portal != targetPortal =>
+            //val portalPos = grid.posOf(Portal(to, from))
+            // TODO: Grid posWhere
+            val portalPos =
+              (for {
+                (row, y) <- grid.view.zipWithIndex
+                (cell, x) <- row.view.zipWithIndex
+                portalPos = Pos(x, y)
+                if portalPos != pos
+                if cell == portal
+              } yield Pos(x, y))
+            println(portalPos.toSeq)
+            for (pp <- portalPos) {
+              println(pp, grid(pp), portal)
+            }
+            assert(portalPos.size == 1)
+            val newLevel = if (innerBox.contains(pos)) level + 1 else level - 1
+            if (newLevel < 0)
+              Iterator.empty
+            else
+              Iterator(Node(portalPos.head, newLevel))
+          case _ => Iterator.empty
+        }
+
+        moveNeighbors ++ portalNeighbors
+      }
+
+      override val targetNode: Node = Node(grid.posOf(targetPortal), 0)
+    }
+
+    BFS.search(graphSearch).target.get._2
+  }
+
   def parseGrid(input: String): Grid[Tile] = {
     val charGrid = input.linesIterator.map(_.toVector).toVector
 
@@ -88,6 +144,7 @@ object Day20 {
 
   def main(args: Array[String]): Unit = {
     println(steps(parseGrid(input)))
+    println(stepsRecursive(parseGrid(input)))
 
     // 700 - too high
   }
