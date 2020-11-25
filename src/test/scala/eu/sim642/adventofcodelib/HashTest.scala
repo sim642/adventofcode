833df33dd949ae03cc61379b40a76fcbdd4bf2c7
Simmo Saan <simmo.saan@gmail.com>
2020-11-25 20:11:12 +0200

    Add test suite for Hash days

--- src/test/scala/eu/sim642/adventofcodelib/HashTest.scala
+++ src/test/scala/eu/sim642/adventofcodelib/HashTest.scala
@@ -1,6 +1,9 @@
 package eu.sim642.adventofcodelib
 
 import Hash._
+import eu.sim642.adventofcode2015.Day4Test
+import eu.sim642.adventofcode2016.{Day14Test, Day17Test, Day5Test}
+import org.scalatest.Suites
 import org.scalatest.funsuite.AnyFunSuite
 
 class HashTest extends AnyFunSuite {
@@ -9,3 +12,10 @@ class HashTest extends AnyFunSuite {
     assert(md5("The quick brown fox jumps over the lazy dog") == "9e107d9d372bb6826bd81d3542a419d6")
   }
 }
+
+class HashSpeedTest extends Suites(
+  new Day4Test,
+  new Day5Test,
+  new Day14Test,
+  new Day17Test,
+)
