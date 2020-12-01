67536d47a951c11e3e5f736dbf7f907342cf5ad9
Simmo Saan <simmo.saan@gmail.com>
2020-12-01 09:40:32 +0200

    Add alternative optimized solution to 2020 day 1

--- src/main/scala/eu/sim642/adventofcode2020/Day1.scala
+++ src/main/scala/eu/sim642/adventofcode2020/Day1.scala
@@ -4,8 +4,22 @@ import eu.sim642.adventofcodelib.IteratorImplicits._
 
 object Day1 {
 
-  def entryProduct2020(entries: Seq[Int], n: Int): Int = {
-    entries.combinations(n).filter(_.sum == 2020).head.product
+  sealed trait Solution {
+    def entryProduct2020(entries: Seq[Int], n: Int): Int
+  }
+
+  object NaiveSolution extends Solution {
+    override def entryProduct2020(entries: Seq[Int], n: Int): Int = {
+      entries.combinations(n).filter(_.sum == 2020).head.product
+    }
+  }
+
+  object SetContainsSolution extends Solution {
+    override def entryProduct2020(entries: Seq[Int], n: Int): Int = {
+      val entriesSet = entries.toSet
+      val init = entries.combinations(n - 1).filter(c => entriesSet.contains(2020 - c.sum)).head
+      init.product * (2020 - init.sum)
+    }
   }
 
   def parseEntries(input: String): Seq[Int] = input.linesIterator.map(_.toInt).toSeq
@@ -13,6 +27,8 @@ object Day1 {
   lazy val input: String = io.Source.fromInputStream(getClass.getResourceAsStream("day1.txt")).mkString.trim
 
   def main(args: Array[String]): Unit = {
+    import SetContainsSolution._
+
     println(entryProduct2020(parseEntries(input), 2))
     println(entryProduct2020(parseEntries(input), 3))
   }
--- src/test/scala/eu/sim642/adventofcode2020/Day1Test.scala
+++ src/test/scala/eu/sim642/adventofcode2020/Day1Test.scala
@@ -1,31 +1,45 @@
 package eu.sim642.adventofcode2020
 
 import Day1._
+import eu.sim642.adventofcode2020.Day1Test._
+import org.scalatest.Suites
 import org.scalatest.funsuite.AnyFunSuite
 
-class Day1Test extends AnyFunSuite {
+class Day1Test extends Suites(
+  new NaiveSolutionTest,
+  new SetContainsSolutionTest,
+)
 
-  val exampleInput =
-    """1721
-      |979
-      |366
-      |299
-      |675
-      |1456""".stripMargin
+object Day1Test {
 
-  test("Part 1 examples") {
-    assert(entryProduct2020(parseEntries(exampleInput), 2) == 514579)
+  sealed abstract class SolutionTest(solution: Solution) extends AnyFunSuite {
+
+    val exampleInput =
+      """1721
+        |979
+        |366
+        |299
+        |675
+        |1456""".stripMargin
+
+    test("Part 1 examples") {
+      assert(solution.entryProduct2020(parseEntries(exampleInput), 2) == 514579)
+    }
+
+    test("Part 1 input answer") {
+      assert(solution.entryProduct2020(parseEntries(input), 2) == 713184)
+    }
+
+    test("Part 2 examples") {
+      assert(solution.entryProduct2020(parseEntries(exampleInput), 3) == 241861950)
+    }
+
+    test("Part 2 input answer") {
+      assert(solution.entryProduct2020(parseEntries(input), 3) == 261244452)
+    }
   }
 
-  test("Part 1 input answer") {
-    assert(entryProduct2020(parseEntries(input), 2) == 713184)
-  }
+  class NaiveSolutionTest extends SolutionTest(NaiveSolution)
 
-  test("Part 2 examples") {
-    assert(entryProduct2020(parseEntries(exampleInput), 3) == 241861950)
-  }
-
-  test("Part 2 input answer") {
-    assert(entryProduct2020(parseEntries(input), 3) == 261244452)
-  }
+  class SetContainsSolutionTest extends SolutionTest(SetContainsSolution)
 }
