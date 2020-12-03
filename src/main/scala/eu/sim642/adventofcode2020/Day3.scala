3283069e7e8ca9ecd06e97a96da1629d34386c63
Simmo Saan <simmo.saan@gmail.com>
2020-12-03 10:11:00 +0200

    Refactor 2020 day 3 to use Boolean grid

--- src/main/scala/eu/sim642/adventofcode2020/Day3.scala
+++ src/main/scala/eu/sim642/adventofcode2020/Day3.scala
@@ -1,14 +1,15 @@
 package eu.sim642.adventofcode2020
 
 import eu.sim642.adventofcodelib.Grid
+import eu.sim642.adventofcodelib.GridImplicits._
 import eu.sim642.adventofcodelib.pos.Pos
 
 import scala.annotation.tailrec
 
 object Day3 {
 
-  def countSlopeTrees(grid: Grid[Char], slope: Pos = Pos(3, 1)): Int = {
-    def gridRepeat(pos: Pos): Char = {
+  def countSlopeTrees(grid: Grid[Boolean], slope: Pos = Pos(3, 1)): Int = {
+    def gridRepeat(pos: Pos): Boolean = {
       val row = grid(pos.y)
       row(pos.x % row.size)
     }
@@ -16,18 +17,14 @@ object Day3 {
     // original row-by-row solution generalized to bigger y steps
     (grid.indices by slope.y).view.zipWithIndex.count({ case (y, i) =>
       val x = slope.x * i
-      gridRepeat(Pos(x, y)) == '#'
+      gridRepeat(Pos(x, y))
     })
 
     // alternative iterative solution
     /*@tailrec
     def helper(pos: Pos, count: Int): Int = {
       if (pos.y < grid.size) {
-        val countDelta = gridRepeat(pos) match {
-          case '#' => 1
-          case '.' => 0
-          case _ => throw new IllegalArgumentException("illegal grid char")
-        }
+        val countDelta = if (gridRepeat(pos)) 1 else 0
         helper(pos + slope, count + countDelta)
       } else
         count
@@ -44,12 +41,15 @@ object Day3 {
     Pos(1, 2),
   )
 
-  def multiplySlopeTrees(grid: Grid[Char]): Long = {
+  def multiplySlopeTrees(grid: Grid[Boolean]): Long = {
     multiplySlopes.map(countSlopeTrees(grid, _)).map(_.toLong).product
   }
 
 
-  def parseGrid(input: String): Grid[Char] = input.linesIterator.map(_.toVector).toVector
+  def parseGrid(input: String): Grid[Boolean] = input.linesIterator.map(_.toVector).toVector.mapGrid({
+    case '#' => true
+    case '.' => false
+  })
 
   lazy val input: String = io.Source.fromInputStream(getClass.getResourceAsStream("day3.txt")).mkString.trim
 
