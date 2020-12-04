81287819988e21b97e4ee4109f1ec1e7f99b0660
Simmo Saan <simmo.saan@gmail.com>
2020-12-04 09:42:39 +0200

    Solve 2020 day 4 part 2

--- src/main/scala/eu/sim642/adventofcode2020/Day4.scala
+++ src/main/scala/eu/sim642/adventofcode2020/Day4.scala
@@ -4,11 +4,44 @@ object Day4 {
 
   type Passport = Map[String, String]
 
-  private val requiredFields = Set("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid")
+  sealed trait Part {
+    def isValid(passport: Passport): Boolean
 
-  def isValid(passport: Passport): Boolean = requiredFields.subsetOf(passport.keySet)
+    def countValid(passports: Seq[Passport]): Int = passports.count(isValid)
+  }
 
-  def countValid(passports: Seq[Passport]): Int = passports.count(isValid)
+  object Part1 extends Part {
+    private val requiredFields = Set("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid")
+
+    override def isValid(passport: Passport): Boolean = requiredFields.subsetOf(passport.keySet)
+  }
+
+  object Part2 extends Part {
+    private val hgtRegex = """(\d+)(cm|in)""".r
+    private val hclRegex = """#[0-9a-f]{6}""".r
+    private val eclValues = Set("amb", "blu", "brn", "gry", "grn", "hzl", "oth")
+    private val pidRegex = """[0-9]{9}""".r
+
+    val fieldValidators = Map(
+      "byr" -> ((_: String).toIntOption.exists((1920 to 2002).contains)),
+      "iyr" -> ((_: String).toIntOption.exists((2010 to 2020).contains)),
+      "eyr" -> ((_: String).toIntOption.exists((2020 to 2030).contains)),
+      "hgt" -> ((_: String) match {
+        case hgtRegex(num, "cm") => (150 to 193).contains(num.toInt)
+        case hgtRegex(num, "in") => (59 to 76).contains(num.toInt)
+        case _ => false
+      }),
+      "hcl" -> (hclRegex.matches(_)),
+      "ecl" -> (eclValues.contains(_)),
+      "pid" -> (pidRegex.matches(_)),
+    )
+
+    override def isValid(passport: Passport): Boolean = {
+      fieldValidators.forall({ case (field, validator) =>
+        passport.contains(field) && validator(passport(field))
+      })
+    }
+  }
 
 
   private val fieldRegex = """(.*):(.*)""".r
@@ -24,6 +57,7 @@ object Day4 {
   lazy val input: String = io.Source.fromInputStream(getClass.getResourceAsStream("day4.txt")).mkString.trim
 
   def main(args: Array[String]): Unit = {
-    println(countValid(parsePassports(input)))
+    println(Part1.countValid(parsePassports(input)))
+    println(Part2.countValid(parsePassports(input)))
   }
 }
