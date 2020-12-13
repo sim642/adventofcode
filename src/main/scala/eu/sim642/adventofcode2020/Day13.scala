50bdc1f003d018c9f9e1157f3f1bf74627a51ac0
Simmo Saan <simmo.saan@gmail.com>
2020-12-13 11:46:19 +0200

    Solve 2020 day 13 part 2

--- src/main/scala/eu/sim642/adventofcode2020/Day13.scala
+++ src/main/scala/eu/sim642/adventofcode2020/Day13.scala
@@ -1,8 +1,12 @@
 package eu.sim642.adventofcode2020
 
+import eu.sim642.adventofcodelib.IntegralImplicits._
+import eu.sim642.adventofcodelib.NumberTheory
+
 object Day13 {
 
-  case class Notes(earliestDepart: Int, buses: Seq[Option[Int]])
+  type Buses = Seq[Option[Int]]
+  case class Notes(earliestDepart: Int, buses: Buses)
 
   def earliestBusWaitTime(notes: Notes): Int = {
     val (earliestBus, waitTime) = notes.buses.view
@@ -12,19 +16,42 @@ object Day13 {
     earliestBus * waitTime
   }
 
+  def earliestSubsequentDepart(buses: Buses): BigInt = {
+    // x buses can be ignored (choose bus ID to be t + i)
 
-  def parseNotes(input: String): Notes = {
-    val Seq(line1, line2) = input.linesIterator.take(2).toSeq
-    val buses = line2.split(",").toSeq.map({
+    // t + i = 0 (mod bus)
+    // t = -i (mod bus)
+
+    val ans = buses.view
+      .zipWithIndex
+      .flatMap({ case (bus, i) => bus.map((_, i)) })
+      .map({ case (bus, i) => (BigInt((-i) %+ bus), BigInt(bus)) })
+      .toSeq
+    NumberTheory.crt(ans)._1
+    // TODO: answer always just Long?
+  }
+
+  def earliestSubsequentDepart(notes: Notes): BigInt = earliestSubsequentDepart(notes.buses)
+
+
+  def parseBuses(s: String): Buses = {
+    s.split(",").toSeq.map({
       case "x" => None
       case s => Some(s.toInt)
     })
-    Notes(line1.toInt, buses)
+  }
+
+  def parseNotes(input: String): Notes = {
+    val Seq(line1, line2) = input.linesIterator.take(2).toSeq
+    Notes(line1.toInt, parseBuses(line2))
   }
 
   lazy val input: String = io.Source.fromInputStream(getClass.getResourceAsStream("day13.txt")).mkString.trim
 
   def main(args: Array[String]): Unit = {
     println(earliestBusWaitTime(parseNotes(input)))
+    println(earliestSubsequentDepart(parseNotes(input)))
+
+    // part 2: 1191202959664391 - too high (Long overflowed in crt)
   }
 }
--- src/test/scala/eu/sim642/adventofcode2020/Day13Test.scala
+++ src/test/scala/eu/sim642/adventofcode2020/Day13Test.scala
@@ -2,8 +2,9 @@ package eu.sim642.adventofcode2020
 
 import Day13._
 import org.scalatest.funsuite.AnyFunSuite
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
 
-class Day13Test extends AnyFunSuite {
+class Day13Test extends AnyFunSuite with ScalaCheckPropertyChecks {
 
   val exampleInput =
     """939
@@ -15,5 +16,25 @@ class Day13Test extends AnyFunSuite {
 
   test("Part 1 input answer") {
     assert(earliestBusWaitTime(parseNotes(input)) == 203)
+  }
+
+  test("Part 2 examples") {
+    val busesExpectedDepart = Table(
+      ("buses", "expectedDepart"),
+      ("7,13,x,x,59,x,31,19", 1068781),
+      ("17,x,13,19", 3417),
+      ("67,7,59,61", 754018),
+      ("67,x,7,59,61", 779210),
+      ("67,7,x,59,61", 1261476),
+      ("1789,37,47,1889", 1202161486),
+    )
+
+    forAll(busesExpectedDepart) { (buses, expectedDepart) =>
+      assert(earliestSubsequentDepart(parseBuses(buses)) == expectedDepart)
+    }
+  }
+
+  test("Part 2 input answer") {
+    assert(earliestSubsequentDepart(parseNotes(input)) == 905694340256752L)
   }
 }
