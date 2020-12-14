57d07980723d927807a9143be7b3cbca7397c6ca
Simmo Saan <simmo.saan@gmail.com>
2020-12-14 10:11:33 +0200

    Solve 2020 day 14 part 2

--- src/main/scala/eu/sim642/adventofcode2020/Day14.scala
+++ src/main/scala/eu/sim642/adventofcode2020/Day14.scala
@@ -5,7 +5,7 @@ import eu.sim642.adventofcodelib.LazyListImplicits._
 
 object Day14 {
 
-  type Address = Int
+  type Address = Long
   type Integer = Long
 
   sealed trait Instruction
@@ -16,38 +16,94 @@ object Day14 {
 
   type Memory = Map[Address, Integer]
 
-  case class ProgramState(instructions: Instructions, memory: Memory = Map.empty.withDefaultValue(0L), mask0: Integer = Long.MinValue, mask1: Integer = 0L) {
-    def execOne: Option[ProgramState] = instructions match {
-      case Nil => None
-      case instruction :: newInstructions =>
-        Some(
-          instruction match {
-            case Mask(mask) =>
-              val mask0 = mask.map({
-                case '0' => '0'
-                case _ => '1'
-              }).toLongRadix(2)
-              val mask1 = mask.map({
-                case '1' => '1'
-                case _ => '0'
-              }).toLongRadix(2)
-              copy(instructions = newInstructions, mask0 = mask0, mask1 = mask1)
-            case Mem(address, value) =>
-              val newValue = (value & mask0) | mask1
-              copy(instructions = newInstructions, memory = memory + (address -> newValue))
-          }
-        )
-    }
-
-    def execs: LazyList[ProgramState] = LazyList.unfold0(this)(_.execOne)
+  sealed trait Part {
+    def sumFinalMemory(instructions: Instructions): Integer
   }
 
-  def sumFinalMemory(instructions: Instructions): Integer = {
-    ProgramState(instructions).execs
-      .last
-      .memory
-      .values
-      .sum
+  // TODO: reduce duplication
+
+  object Part1 extends Part {
+
+    case class ProgramState(instructions: Instructions, memory: Memory = Map.empty.withDefaultValue(0L), mask0: Integer = Long.MinValue, mask1: Integer = 0L) {
+      def execOne: Option[ProgramState] = instructions match {
+        case Nil => None
+        case instruction :: newInstructions =>
+          Some(
+            instruction match {
+              case Mask(mask) =>
+                val mask0 = mask.map({
+                  case '0' => '0'
+                  case _ => '1'
+                }).toLongRadix(2)
+                val mask1 = mask.map({
+                  case '1' => '1'
+                  case _ => '0'
+                }).toLongRadix(2)
+                copy(instructions = newInstructions, mask0 = mask0, mask1 = mask1)
+              case Mem(address, value) =>
+                val newValue = (value & mask0) | mask1
+                copy(instructions = newInstructions, memory = memory + (address -> newValue))
+            }
+          )
+      }
+
+      def execs: LazyList[ProgramState] = LazyList.unfold0(this)(_.execOne)
+    }
+
+    override def sumFinalMemory(instructions: Instructions): Integer = {
+      ProgramState(instructions).execs
+        .last
+        .memory
+        .values
+        .sum
+    }
+  }
+
+  object Part2 extends Part {
+
+    case class ProgramState(instructions: Instructions, memory: Memory = Map.empty.withDefaultValue(0L), mask0: Address = Long.MinValue, mask1s: Seq[Address] = Seq(0L)) {
+      def execOne: Option[ProgramState] = instructions match {
+        case Nil => None
+        case instruction :: newInstructions =>
+          Some(
+            instruction match {
+              case Mask(mask) =>
+                val mask0 = mask.map({
+                  case 'X' => '0' // different!
+                  case _ => '1'
+                }).toLongRadix(2)
+                val mask1 = mask.map({
+                  case '1' => '1'
+                  case _ => '0'
+                }).toLongRadix(2)
+                val maskXs =
+                  mask
+                    .zipWithIndex
+                    .filter(_._1 == 'X')
+                    .map(mask.length - 1 - _._2)
+                    .toSet
+                    .subsets()
+                    .map(_.foldLeft(0L)({ case (acc, i) => acc | (1L << i) }))
+                    .toSeq
+                val mask1s = maskXs.map(_ | mask1)
+                copy(instructions = newInstructions, mask0 = mask0, mask1s = mask1s)
+              case Mem(address, value) =>
+                val addresses = mask1s.map(mask1 => (address & mask0) | mask1)
+                copy(instructions = newInstructions, memory = memory ++ addresses.map(_ -> value))
+            }
+          )
+      }
+
+      def execs: LazyList[ProgramState] = LazyList.unfold0(this)(_.execOne)
+    }
+
+    override def sumFinalMemory(instructions: Instructions): Integer = {
+      ProgramState(instructions).execs
+        .last
+        .memory
+        .values
+        .sum
+    }
   }
 
 
@@ -64,6 +120,9 @@ object Day14 {
   lazy val input: String = io.Source.fromInputStream(getClass.getResourceAsStream("day14.txt")).mkString.trim
 
   def main(args: Array[String]): Unit = {
-    println(sumFinalMemory(parseInstructions(input)))
+    println(Part1.sumFinalMemory(parseInstructions(input)))
+    println(Part2.sumFinalMemory(parseInstructions(input)))
+
+    // part 2: 1994223606982 - too low
   }
 }
--- src/test/scala/eu/sim642/adventofcode2020/Day14Test.scala
+++ src/test/scala/eu/sim642/adventofcode2020/Day14Test.scala
@@ -11,11 +11,25 @@ class Day14Test extends AnyFunSuite {
       |mem[7] = 101
       |mem[8] = 0""".stripMargin
 
+  val exampleInput2 =
+    """mask = 000000000000000000000000000000X1001X
+      |mem[42] = 100
+      |mask = 00000000000000000000000000000000X0XX
+      |mem[26] = 1""".stripMargin
+
   test("Part 1 examples") {
-    assert(sumFinalMemory(parseInstructions(exampleInput)) == 165)
+    assert(Part1.sumFinalMemory(parseInstructions(exampleInput)) == 165)
   }
 
   test("Part 1 input answer") {
-    assert(sumFinalMemory(parseInstructions(input)) == 10452688630537L)
+    assert(Part1.sumFinalMemory(parseInstructions(input)) == 10452688630537L)
+  }
+
+  test("Part 2 examples") {
+    assert(Part2.sumFinalMemory(parseInstructions(exampleInput2)) == 208)
+  }
+
+  test("Part 2 input answer") {
+    assert(Part2.sumFinalMemory(parseInstructions(input)) == 2881082759597L)
   }
 }
