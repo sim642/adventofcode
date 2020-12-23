f6fa2f81dcb6a7e80d6c97701d294ba08a84fa76
Simmo Saan <simmo.saan@gmail.com>
2020-12-23 11:25:31 +0200

    Solve 2020 day 23 part 2

--- src/main/scala/eu/sim642/adventofcode2020/Day23.scala
+++ src/main/scala/eu/sim642/adventofcode2020/Day23.scala
@@ -3,27 +3,63 @@ package eu.sim642.adventofcode2020
 import eu.sim642.adventofcodelib.IteratorImplicits._
 import eu.sim642.adventofcodelib.SeqImplicits._
 import eu.sim642.adventofcodelib.IntegralImplicits._
+import eu.sim642.adventofcodelib.LazyListImplicits._
 
 object Day23 {
 
   type Cups = Vector[Int]
 
-  def simulateMove(cups: Cups): Cups = {
-    val (current +: picked, rest) = cups.splitAt(4)
-    val destination = Iterator.from(current - 1, -1).map(_ %+ 10).find(rest.contains).get
-    val (beforeDestination, afterDestination) = rest.splitAt(rest.indexOf(destination) + 1)
-    val newRest = beforeDestination ++ picked ++ afterDestination
-    (current +: newRest).rotateLeft(1)
+  case class State(next: Map[Int, Int], current: Int, max: Int) {
+
+    def toCups(from: Int) = {
+      from #:: LazyList.unfold0(from)(i => Some(next(i))).takeWhile(_ != from)
+    }
+
+    override def toString: String = toCups(current).toString
   }
 
-  def simulateMoves(cups: Cups, moves: Int = 100): Cups = {
-    Iterator.iterate(cups)(simulateMove)(moves)
+  def simulateMove(state: State): State = {
+    val State(next, current, max) = state
+    val pick1 = next(current)
+    val pick2 = next(pick1)
+    val pick3 = next(pick2)
+
+    val destination =
+      Iterator.iterate(current)(i => (i - 1 - 1) %+ max + 1)
+        .find(i => i != current && i != pick1 && i != pick2 && i != pick3)
+        .get
+
+    val newNext = next + (current -> next(pick3)) + (destination -> pick1) + (pick3 -> next(destination))
+    State(newNext, newNext(current), max)
+  }
+
+  def simulateMoves(state: State, moves: Int = 100): State = {
+    Iterator.iterate(state)(simulateMove)(moves)
+  }
+
+  def cups2State(cups: Cups): State = {
+    val next =
+      cups
+        .iterator
+        .zipWithTail
+        .toMap
+    val nextLoop = next + (cups.last -> cups.head)
+    State(nextLoop, cups.head, cups.max)
   }
 
   def simulateMovesLabels(cups: Cups, moves: Int = 100): String = {
-    val finalCups = simulateMoves(cups, moves)
-    val 1 +: restCups = finalCups.rotateLeft(finalCups.indexOf(1))
-    restCups.mkString("")
+    val finalState = simulateMoves(cups2State(cups), moves)
+    finalState.toCups(1).tail.mkString("")
+  }
+
+  def simulateMovesPart2(cups: Cups, moves: Int = 10000000): State = {
+    val newCups = cups ++ ((cups.max + 1) to 1000000)
+    simulateMoves(cups2State(newCups), moves)
+  }
+
+  def simulateMovesLabelsPart2(cups: Cups, moves: Int = 10000000): Long = {
+    val finalState = simulateMovesPart2(cups, moves)
+    finalState.toCups(1).tail.take(2).map(_.toLong).tapEach(println).product
   }
 
 
@@ -34,5 +70,6 @@ object Day23 {
 
   def main(args: Array[String]): Unit = {
     println(simulateMovesLabels(parseCups(input)))
+    println(simulateMovesLabelsPart2(parseCups(input)))
   }
 }
--- src/test/scala/eu/sim642/adventofcode2020/Day23Test.scala
+++ src/test/scala/eu/sim642/adventofcode2020/Day23Test.scala
@@ -8,7 +8,7 @@ class Day23Test extends AnyFunSuite {
   val exampleInput = "389125467"
 
   test("Part 1 examples") {
-    assert(simulateMove(parseCups(exampleInput)) == parseCups("289154673"))
+    //assert(simulateMove(parseCups(exampleInput)) == parseCups("289154673"))
 
     assert(simulateMovesLabels(parseCups(exampleInput), 10) == "92658374")
     assert(simulateMovesLabels(parseCups(exampleInput)) == "67384529")
@@ -16,5 +16,15 @@ class Day23Test extends AnyFunSuite {
 
   test("Part 1 input answer") {
     assert(simulateMovesLabels(parseCups(input)) == "52864379")
+  }
+
+  // TODO: optimize (51s)
+  ignore("Part 2 examples") {
+    assert(simulateMovesLabelsPart2(parseCups(exampleInput)) == 149245887792L)
+  }
+
+  // TODO: optimize (41s)
+  ignore("Part 2 input answer") {
+    assert(simulateMovesLabelsPart2(parseCups(input)) == 11591415792L)
   }
 }
