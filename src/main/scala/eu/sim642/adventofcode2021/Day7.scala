ea4abea2e18db9644a78e43603af8fd2b0d8e7fd
Simmo Saan <simmo.saan@gmail.com>
2021-12-07 21:35:21 +0200

    Add binary search solution to 2021 day 7

--- src/main/scala/eu/sim642/adventofcode2021/Day7.scala
+++ src/main/scala/eu/sim642/adventofcode2021/Day7.scala
@@ -1,29 +1,71 @@
 package eu.sim642.adventofcode2021
 
+import eu.sim642.adventofcodelib.OrderedSearch
+
 object Day7 {
 
-  sealed trait Part {
-
+  sealed trait PartFuel {
     def fuel(crab: Int, pos: Int): Int
 
     def alignPosFuel(crabs: Seq[Int], pos: Int): Int = {
       crabs.view.map(fuel(_, pos)).sum
     }
-
-    def minAlignPosFuel(crabs: Seq[Int]): Int = {
-      (crabs.min to crabs.max).iterator.map(alignPosFuel(crabs, _)).min
-    }
   }
 
-  object Part1 extends Part {
+  sealed trait Part1Fuel extends PartFuel {
     override def fuel(crab: Int, pos: Int): Int = (crab - pos).abs
   }
 
-  object Part2 extends Part {
+  object Part1Fuel extends Part1Fuel // for testing
+
+  sealed trait Part2Fuel extends PartFuel {
     override def fuel(crab: Int, pos: Int): Int = {
-      val diff = Part1.fuel(crab, pos)
+      val diff = Part1Fuel.fuel(crab, pos)
       (diff * (diff + 1)) / 2
     }
+  }
+
+  object Part2Fuel extends Part2Fuel // for testing
+
+  sealed trait PartSolution extends PartFuel {
+    def minAlignPosFuel(crabs: Seq[Int]): Int
+  }
+
+  sealed trait Solution {
+    val Part1: PartSolution
+    val Part2: PartSolution
+  }
+
+  /**
+   * Solution, which linearly searches the min-max range for the optimum.
+   */
+  object LinearSolution extends Solution {
+
+    sealed trait LinearPartSolution extends PartSolution {
+      override def minAlignPosFuel(crabs: Seq[Int]): Int = {
+        (crabs.min to crabs.max).iterator.map(alignPosFuel(crabs, _)).min
+      }
+    }
+
+    override object Part1 extends LinearPartSolution with Part1Fuel
+    override object Part2 extends LinearPartSolution with Part2Fuel
+  }
+
+  /**
+   * Solution, which binary searches the min-max range for the optimum using difference.
+   */
+  object BinarySolution extends Solution {
+
+    sealed trait BinaryPartSolution extends PartSolution {
+      override def minAlignPosFuel(crabs: Seq[Int]): Int = {
+        def f(pos: Int): Int = alignPosFuel(crabs, pos + 1) - alignPosFuel(crabs, pos)
+        val pos = OrderedSearch.binaryLower(f, crabs.min, crabs.max + 1)(0)
+        alignPosFuel(crabs, pos)
+      }
+    }
+
+    override object Part1 extends BinaryPartSolution with Part1Fuel
+    override object Part2 extends BinaryPartSolution with Part2Fuel
   }
 
 
@@ -32,6 +74,8 @@ object Day7 {
   lazy val input: String = io.Source.fromInputStream(getClass.getResourceAsStream("day7.txt")).mkString.trim
 
   def main(args: Array[String]): Unit = {
+    import LinearSolution._
+
     println(Part1.minAlignPosFuel(parseCrabs(input)))
     println(Part2.minAlignPosFuel(parseCrabs(input)))
   }
--- src/test/scala/eu/sim642/adventofcode2021/Day7Test.scala
+++ src/test/scala/eu/sim642/adventofcode2021/Day7Test.scala
@@ -1,33 +1,55 @@
 package eu.sim642.adventofcode2021
 
 import Day7._
+import Day7Test._
+import org.scalatest.Suites
 import org.scalatest.funsuite.AnyFunSuite
 
-class Day7Test extends AnyFunSuite {
+class Day7Test extends Suites(
+  new BaseTest,
+  new LinearSolutionTest,
+  new BinarySolutionTest,
+)
+
+object Day7Test {
 
   val exampleInput = "16,1,2,0,4,2,7,1,2,14"
 
-  test("Part 1 examples") {
-    val exampleCrabs = parseCrabs(exampleInput)
-    assert(Part1.alignPosFuel(exampleCrabs, 2) == 37)
-    assert(Part1.alignPosFuel(exampleCrabs, 1) == 41)
-    assert(Part1.alignPosFuel(exampleCrabs, 3) == 39)
-    assert(Part1.alignPosFuel(exampleCrabs, 10) == 71)
-    assert(Part1.minAlignPosFuel(exampleCrabs) == 37)
+  class BaseTest extends AnyFunSuite {
+    test("Part 1 examples") {
+      val exampleCrabs = parseCrabs(exampleInput)
+      assert(Part1Fuel.alignPosFuel(exampleCrabs, 2) == 37)
+      assert(Part1Fuel.alignPosFuel(exampleCrabs, 1) == 41)
+      assert(Part1Fuel.alignPosFuel(exampleCrabs, 3) == 39)
+      assert(Part1Fuel.alignPosFuel(exampleCrabs, 10) == 71)
+    }
+
+    test("Part 2 examples") {
+      val exampleCrabs = parseCrabs(exampleInput)
+      assert(Part2Fuel.alignPosFuel(exampleCrabs, 5) == 168)
+      assert(Part2Fuel.alignPosFuel(exampleCrabs, 2) == 206)
+    }
   }
 
-  test("Part 1 input answer") {
-    assert(Part1.minAlignPosFuel(parseCrabs(input)) == 336721)
+  sealed abstract class SolutionTest(solution: Solution) extends AnyFunSuite {
+    test("Part 1 examples") {
+      assert(solution.Part1.minAlignPosFuel(parseCrabs(exampleInput)) == 37)
+    }
+
+    test("Part 1 input answer") {
+      assert(solution.Part1.minAlignPosFuel(parseCrabs(input)) == 336721)
+    }
+
+    test("Part 2 examples") {
+      assert(solution.Part2.minAlignPosFuel(parseCrabs(exampleInput)) == 168)
+    }
+
+    test("Part 2 input answer") {
+      assert(solution.Part2.minAlignPosFuel(parseCrabs(input)) == 91638945)
+    }
   }
 
-  test("Part 2 examples") {
-    val exampleCrabs = parseCrabs(exampleInput)
-    assert(Part2.alignPosFuel(exampleCrabs, 5) == 168)
-    assert(Part2.alignPosFuel(exampleCrabs, 2) == 206)
-    assert(Part2.minAlignPosFuel(exampleCrabs) == 168)
-  }
+  class LinearSolutionTest extends SolutionTest(LinearSolution)
 
-  test("Part 2 input answer") {
-    assert(Part2.minAlignPosFuel(parseCrabs(input)) == 91638945)
-  }
+  class BinarySolutionTest extends SolutionTest(BinarySolution)
 }
