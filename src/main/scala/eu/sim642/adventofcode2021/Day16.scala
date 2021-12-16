8d999a20845799d7d13cb315d3d18a87e56c546a
Simmo Saan <simmo.saan@gmail.com>
2021-12-16 07:50:59 +0200

    Solve 2021 day 16 part 1

---
+++ src/main/scala/eu/sim642/adventofcode2021/Day16.scala
@@ -0,0 +1,123 @@
+package eu.sim642.adventofcode2021
+
+import eu.sim642.adventofcodelib.Grid
+import eu.sim642.adventofcodelib.GridImplicits.*
+import eu.sim642.adventofcodelib.graph.{Dijkstra, GraphSearch, TargetNode}
+import eu.sim642.adventofcodelib.pos.Pos
+
+object Day16 {
+
+  type Bits = List[Boolean]
+
+  def bits2int(bits: Bits): Int = {
+    bits.foldLeft(0)({ case (acc, bit) => (acc << 1) | (if bit then 1 else 0) })
+  }
+
+
+  sealed trait Packet {
+    val version: Int
+  }
+  case class Literal(version: Int, value: Int) extends Packet
+  case class Operator(version: Int, typeId: Int, subpackets: List[Packet]) extends Packet
+
+
+  def parseLiteralValue(bits: Bits): (Bits, Bits) = {
+    val (prefixBit :: groupBits, bits2) = bits.splitAt(5)
+    if (prefixBit) {
+      val (tailBits, bits3) = parseLiteralValue(bits2)
+      (groupBits ++ tailBits, bits3)
+    } else
+      (groupBits, bits2)
+  }
+
+  def parseSubpacketsLength(bits: Bits): List[Packet] = bits match {
+    case Nil =>
+      Nil
+    case _ =>
+      val (packet, bits2) = parsePacket(bits)
+      val packets = parseSubpacketsLength(bits2)
+      packet :: packets
+  }
+
+  def parseSubpacketsNumber(bits: Bits, n: Int): (List[Packet], Bits) = {
+    if (n == 0)
+      (Nil, bits)
+    else {
+      val (packet, bits2) = parsePacket(bits)
+      val (packets, bits3) = parseSubpacketsNumber(bits2, n - 1)
+      (packet :: packets, bits3)
+    }
+  }
+
+  def parsePacket(bits: Bits): (Packet, Bits) = {
+    val (versionBits, bits2) = bits.splitAt(3)
+    val version = bits2int(versionBits)
+    val (typeIdBits, bits3) = bits2.splitAt(3)
+    val typeId = bits2int(typeIdBits)
+    typeId match {
+      case 4 =>
+        val (valueBits, bits4) = parseLiteralValue(bits3)
+        val value = bits2int(valueBits)
+        (Literal(version, value), bits4)
+      case _ =>
+        val lengthTypeBit :: bits4 = bits3
+        if (lengthTypeBit) {
+          val (numberBits, bits5) = bits4.splitAt(11)
+          val number = bits2int(numberBits)
+          val (subpackets, bits6) = parseSubpacketsNumber(bits5, number)
+          (Operator(version, typeId, subpackets), bits6)
+        }
+        else {
+          val (lengthBits, bits5) = bits4.splitAt(15)
+          val length = bits2int(lengthBits)
+          val (subpacketBits, bits6) = bits5.splitAt(length)
+          val subpackets = parseSubpacketsLength(subpacketBits)
+          (Operator(version, typeId, subpackets), bits6)
+        }
+    }
+  }
+
+  // TODO: rename
+  def parsePacket2(bits: Bits): Packet = parsePacket(bits)._1
+
+
+  def sumVersions(packet: Packet): Int = packet match {
+    case Literal(version, _) =>
+      version
+    case Operator(version, _, subpackets) =>
+      version + subpackets.map(sumVersions).sum
+  }
+
+
+  private val hexBits = Map(
+    '0' -> List(0, 0, 0, 0),
+    '1' -> List(0, 0, 0, 1),
+    '2' -> List(0, 0, 1, 0),
+    '3' -> List(0, 0, 1, 1),
+    '4' -> List(0, 1, 0, 0),
+    '5' -> List(0, 1, 0, 1),
+    '6' -> List(0, 1, 1, 0),
+    '7' -> List(0, 1, 1, 1),
+    '8' -> List(1, 0, 0, 0),
+    '9' -> List(1, 0, 0, 1),
+    'A' -> List(1, 0, 1, 0),
+    'B' -> List(1, 0, 1, 1),
+    'C' -> List(1, 1, 0, 0),
+    'D' -> List(1, 1, 0, 1),
+    'E' -> List(1, 1, 1, 0),
+    'F' -> List(1, 1, 1, 1),
+  ).view.mapValues(_.map({ case 0 => false case 1 => true}))
+
+  def parseHexBits(input: String): Bits = input.view.flatMap(hexBits).toList
+
+  // for testing
+  def parseBinBits(input: String): Bits = input.view.map({ case '0' => false case '1' => true}).toList
+
+  def parseHexPacket(input: String): Packet = parsePacket2(parseHexBits(input))
+
+  lazy val input: String = io.Source.fromInputStream(getClass.getResourceAsStream("day16.txt")).mkString.trim
+
+  def main(args: Array[String]): Unit = {
+    println(sumVersions(parseHexPacket(input)))
+  }
+}
---
+++ src/test/scala/eu/sim642/adventofcode2021/Day16Test.scala
@@ -0,0 +1,28 @@
+package eu.sim642.adventofcode2021
+
+import Day16._
+import org.scalatest.funsuite.AnyFunSuite
+
+class Day16Test extends AnyFunSuite {
+
+  test("Part 1 examples") {
+    assert(parseHexBits("D2FE28") == parseBinBits("110100101111111000101000"))
+    assert(parsePacket2(parseBinBits("110100101111111000101000")) == Literal(6, 2021))
+
+    assert(parseHexBits("38006F45291200") == parseBinBits("00111000000000000110111101000101001010010001001000000000"))
+    assert(parsePacket2(parseBinBits("00111000000000000110111101000101001010010001001000000000")) == Operator(1, 6, List(Literal(6, 10), Literal(2, 20))))
+
+    assert(parseHexBits("EE00D40C823060") == parseBinBits("11101110000000001101010000001100100000100011000001100000"))
+    assert(parsePacket2(parseBinBits("11101110000000001101010000001100100000100011000001100000")) == Operator(7, 3, List(Literal(2, 1), Literal(4, 2), Literal(1, 3))))
+
+    // TODO: version examples structure tests
+    assert(sumVersions(parseHexPacket("8A004A801A8002F478")) == 16)
+    assert(sumVersions(parseHexPacket("620080001611562C8802118E34")) == 12)
+    assert(sumVersions(parseHexPacket("C0015000016115A2E0802F182340")) == 23)
+    assert(sumVersions(parseHexPacket("A0016C880162017C3686B18A3D4780")) == 31)
+  }
+
+  test("Part 1 input answer") {
+    assert(sumVersions(parseHexPacket(input)) == 991)
+  }
+}
