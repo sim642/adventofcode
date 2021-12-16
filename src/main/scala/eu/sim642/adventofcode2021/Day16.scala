ff4df4016de7a2c7d88644efa60055dbc55b7ce7
Simmo Saan <simmo.saan@gmail.com>
2021-12-16 22:19:03 +0200

    Add parser combinator based solution to 2021 day 16

--- src/main/scala/eu/sim642/adventofcode2021/Day16.scala
+++ src/main/scala/eu/sim642/adventofcode2021/Day16.scala
@@ -1,9 +1,7 @@
 package eu.sim642.adventofcode2021
 
-import eu.sim642.adventofcodelib.Grid
-import eu.sim642.adventofcodelib.GridImplicits.*
-import eu.sim642.adventofcodelib.graph.{Dijkstra, GraphSearch, TargetNode}
-import eu.sim642.adventofcodelib.pos.Pos
+import scala.util.parsing.combinator.Parsers
+import scala.util.parsing.input.{NoPosition, Position, Reader}
 
 object Day16 {
 
@@ -21,64 +19,125 @@ object Day16 {
   case class Operator(version: Int, typeId: Int, subpackets: List[Packet]) extends Packet
 
 
-  def parseLiteralValue(bits: Bits): (Bits, Bits) = {
-    val (prefixBit :: groupBits, bits2) = bits.splitAt(5)
-    if (prefixBit) {
-      val (tailBits, bits3) = parseLiteralValue(bits2)
-      (groupBits ++ tailBits, bits3)
-    } else
-      (groupBits, bits2)
+  sealed trait Solution {
+    def parse(bits: Bits): Packet
+
+    def parseHex(input: String): Packet = parse(parseHexBits(input))
   }
 
-  def parseSubpacketsLength(bits: Bits): List[Packet] = bits match {
-    case Nil =>
-      Nil
-    case _ =>
-      val (packet, bits2) = parsePacket(bits)
-      val packets = parseSubpacketsLength(bits2)
-      packet :: packets
-  }
+  object RecursiveDescentSolution extends Solution {
 
-  def parseSubpacketsNumber(bits: Bits, n: Int): (List[Packet], Bits) = {
-    if (n == 0)
-      (Nil, bits)
-    else {
-      val (packet, bits2) = parsePacket(bits)
-      val (packets, bits3) = parseSubpacketsNumber(bits2, n - 1)
-      (packet :: packets, bits3)
+    def parseLiteralValue(bits: Bits): (Bits, Bits) = {
+      val (prefixBit :: groupBits, bits2) = bits.splitAt(5)
+      if (prefixBit) {
+        val (tailBits, bits3) = parseLiteralValue(bits2)
+        (groupBits ++ tailBits, bits3)
+      } else
+        (groupBits, bits2)
     }
-  }
 
-  def parsePacket(bits: Bits): (Packet, Bits) = {
-    val (versionBits, bits2) = bits.splitAt(3)
-    val version = bits2int(versionBits).toInt
-    val (typeIdBits, bits3) = bits2.splitAt(3)
-    val typeId = bits2int(typeIdBits).toInt
-    typeId match {
-      case 4 =>
-        val (valueBits, bits4) = parseLiteralValue(bits3)
-        val value = bits2int(valueBits)
-        (Literal(version, value), bits4)
+    def parseSubpacketsLength(bits: Bits): List[Packet] = bits match {
+      case Nil =>
+        Nil
       case _ =>
-        val lengthTypeBit :: bits4 = bits3
-        if (lengthTypeBit) {
-          val (numberBits, bits5) = bits4.splitAt(11)
-          val number = bits2int(numberBits).toInt
-          val (subpackets, bits6) = parseSubpacketsNumber(bits5, number)
-          (Operator(version, typeId, subpackets), bits6)
-        }
-        else {
-          val (lengthBits, bits5) = bits4.splitAt(15)
-          val length = bits2int(lengthBits).toInt
-          val (subpacketBits, bits6) = bits5.splitAt(length)
-          val subpackets = parseSubpacketsLength(subpacketBits)
-          (Operator(version, typeId, subpackets), bits6)
-        }
+        val (packet, bits2) = parsePacket(bits)
+        val packets = parseSubpacketsLength(bits2)
+        packet :: packets
     }
+
+    def parseSubpacketsNumber(bits: Bits, n: Int): (List[Packet], Bits) = {
+      if (n == 0)
+        (Nil, bits)
+      else {
+        val (packet, bits2) = parsePacket(bits)
+        val (packets, bits3) = parseSubpacketsNumber(bits2, n - 1)
+        (packet :: packets, bits3)
+      }
+    }
+
+    def parsePacket(bits: Bits): (Packet, Bits) = {
+      val (versionBits, bits2) = bits.splitAt(3)
+      val version = bits2int(versionBits).toInt
+      val (typeIdBits, bits3) = bits2.splitAt(3)
+      val typeId = bits2int(typeIdBits).toInt
+      typeId match {
+        case 4 =>
+          val (valueBits, bits4) = parseLiteralValue(bits3)
+          val value = bits2int(valueBits)
+          (Literal(version, value), bits4)
+        case _ =>
+          val lengthTypeBit :: bits4 = bits3
+          if (lengthTypeBit) {
+            val (numberBits, bits5) = bits4.splitAt(11)
+            val number = bits2int(numberBits).toInt
+            val (subpackets, bits6) = parseSubpacketsNumber(bits5, number)
+            (Operator(version, typeId, subpackets), bits6)
+          }
+          else {
+            val (lengthBits, bits5) = bits4.splitAt(15)
+            val length = bits2int(lengthBits).toInt
+            val (subpacketBits, bits6) = bits5.splitAt(length)
+            val subpackets = parseSubpacketsLength(subpacketBits)
+            (Operator(version, typeId, subpackets), bits6)
+          }
+      }
+    }
+
+    override def parse(bits: Bits): Packet = parsePacket(bits)._1
   }
 
-  // TODO: rename
-  def parsePacket2(bits: Bits): Packet = parsePacket(bits)._1
+  object ParserCombinatorSolution extends Solution with Parsers {
+
+    case class ListReader[A](list: List[A]) extends Reader[A] {
+      override def first: A = list.head
+
+      override def rest: Reader[A] = ListReader(list.tail)
+
+      override def pos: Position = NoPosition // TODO: something better
+
+      override def atEnd: Boolean = list.isEmpty
+    }
+
+    override type Elem = Boolean
+
+    def bit: Parser[Boolean] = (in: Input) => {
+      if (in.atEnd)
+        Failure("bit", in)
+      else
+        Success(in.first, in.rest)
+    }
+
+    def long(n: Int): Parser[Long] = repN(n, bit) ^^ bits2int
+
+    def int(n: Int): Parser[Int] = long(n) ^^ (_.toInt)
+
+    def valueBlock(b: Boolean): Parser[Long] = elem(b) ~> long(4)
+
+    def literalValue: Parser[Long] = rep(valueBlock(true)) ~ valueBlock(false) ^^ {
+      case xs ~ x => (xs.foldLeft(0L)((acc, x) => (acc << 4) | x) << 4) | x
+    }
+
+    def packet: Parser[Packet] = (int(3) ~ int(3)).flatMap({
+      case version ~ 4 =>
+        literalValue ^^ (Literal(version, _))
+      case version ~ typeId =>
+        bit.flatMap({
+          case true =>
+            int(11).flatMap(number =>
+              repN(number, packet)
+            ) ^^ (Operator(version, typeId, _))
+          case false =>
+            int(15).flatMap(bits =>
+              repN(bits, bit) ^^ (bits =>
+                  val subpackets = rep(packet)(ListReader(bits)).get
+                  Operator(version, typeId, subpackets)
+                )
+            )
+        })
+    })
+
+    override def parse(bits: Bits): Packet = packet(ListReader(bits)).get
+  }
 
 
   def sumVersions(packet: Packet): Int = packet match {
@@ -144,13 +203,13 @@ object Day16 {
   // for testing
   def parseBinBits(input: String): Bits = input.view.map({ case '0' => false case '1' => true}).toList
 
-  def parseHexPacket(input: String): Packet = parsePacket2(parseHexBits(input))
-
   lazy val input: String = io.Source.fromInputStream(getClass.getResourceAsStream("day16.txt")).mkString.trim
 
   def main(args: Array[String]): Unit = {
-    println(sumVersions(parseHexPacket(input)))
-    println(eval(parseHexPacket(input)))
+    import ParserCombinatorSolution._
+
+    println(sumVersions(parseHex(input)))
+    println(eval(parseHex(input)))
 
     // part 2: 2904570676 - wrong (need Long for Literal values and eval)
   }
--- src/test/scala/eu/sim642/adventofcode2021/Day16Test.scala
+++ src/test/scala/eu/sim642/adventofcode2021/Day16Test.scala
@@ -1,43 +1,67 @@
 package eu.sim642.adventofcode2021
 
 import Day16._
+import Day16Test._
+import org.scalatest.Suites
 import org.scalatest.funsuite.AnyFunSuite
 
-class Day16Test extends AnyFunSuite {
+class Day16Test extends Suites(
+  new BaseTest,
+  new RecursiveDescentSolutionTest,
+  new ParserCombinatorSolutionTest,
+)
 
-  test("Part 1 examples") {
-    assert(parseHexBits("D2FE28") == parseBinBits("110100101111111000101000"))
-    assert(parsePacket2(parseBinBits("110100101111111000101000")) == Literal(6, 2021))
+object Day16Test {
 
-    assert(parseHexBits("38006F45291200") == parseBinBits("00111000000000000110111101000101001010010001001000000000"))
-    assert(parsePacket2(parseBinBits("00111000000000000110111101000101001010010001001000000000")) == Operator(1, 6, List(Literal(6, 10), Literal(2, 20))))
-
-    assert(parseHexBits("EE00D40C823060") == parseBinBits("11101110000000001101010000001100100000100011000001100000"))
-    assert(parsePacket2(parseBinBits("11101110000000001101010000001100100000100011000001100000")) == Operator(7, 3, List(Literal(2, 1), Literal(4, 2), Literal(1, 3))))
-
-    // TODO: version examples structure tests
-    assert(sumVersions(parseHexPacket("8A004A801A8002F478")) == 16)
-    assert(sumVersions(parseHexPacket("620080001611562C8802118E34")) == 12)
-    assert(sumVersions(parseHexPacket("C0015000016115A2E0802F182340")) == 23)
-    assert(sumVersions(parseHexPacket("A0016C880162017C3686B18A3D4780")) == 31)
+  class BaseTest extends AnyFunSuite {
+    test("Part 1 examples") {
+      assert(parseHexBits("D2FE28") == parseBinBits("110100101111111000101000"))
+      assert(parseHexBits("38006F45291200") == parseBinBits("00111000000000000110111101000101001010010001001000000000"))
+      assert(parseHexBits("EE00D40C823060") == parseBinBits("11101110000000001101010000001100100000100011000001100000"))
+    }
   }
 
-  test("Part 1 input answer") {
-    assert(sumVersions(parseHexPacket(input)) == 991)
+  sealed abstract class SolutionTest(solution: Solution) extends AnyFunSuite {
+    import solution._
+
+    test("Part 1 examples") {
+      assert(parseHexBits("D2FE28") == parseBinBits("110100101111111000101000"))
+      assert(parse(parseBinBits("110100101111111000101000")) == Literal(6, 2021))
+
+      assert(parseHexBits("38006F45291200") == parseBinBits("00111000000000000110111101000101001010010001001000000000"))
+      assert(parse(parseBinBits("00111000000000000110111101000101001010010001001000000000")) == Operator(1, 6, List(Literal(6, 10), Literal(2, 20))))
+
+      assert(parseHexBits("EE00D40C823060") == parseBinBits("11101110000000001101010000001100100000100011000001100000"))
+      assert(parse(parseBinBits("11101110000000001101010000001100100000100011000001100000")) == Operator(7, 3, List(Literal(2, 1), Literal(4, 2), Literal(1, 3))))
+
+      // TODO: version examples structure tests
+      assert(sumVersions(parseHex("8A004A801A8002F478")) == 16)
+      assert(sumVersions(parseHex("620080001611562C8802118E34")) == 12)
+      assert(sumVersions(parseHex("C0015000016115A2E0802F182340")) == 23)
+      assert(sumVersions(parseHex("A0016C880162017C3686B18A3D4780")) == 31)
+    }
+
+    test("Part 1 input answer") {
+      assert(sumVersions(parseHex(input)) == 991)
+    }
+
+    test("Part 2 examples") {
+      assert(eval(parseHex("C200B40A82")) == 3)
+      assert(eval(parseHex("04005AC33890")) == 54)
+      assert(eval(parseHex("880086C3E88112")) == 7)
+      assert(eval(parseHex("CE00C43D881120")) == 9)
+      assert(eval(parseHex("D8005AC2A8F0")) == 1)
+      assert(eval(parseHex("F600BC2D8F")) == 0)
+      assert(eval(parseHex("9C005AC2F8F0")) == 0)
+      assert(eval(parseHex("9C0141080250320F1802104A08")) == 1)
+    }
+
+    test("Part 2 input answer") {
+      assert(eval(parseHex(input)) == 1264485568252L)
+    }
   }
 
-  test("Part 2 examples") {
-    assert(eval(parseHexPacket("C200B40A82")) == 3)
-    assert(eval(parseHexPacket("04005AC33890")) == 54)
-    assert(eval(parseHexPacket("880086C3E88112")) == 7)
-    assert(eval(parseHexPacket("CE00C43D881120")) == 9)
-    assert(eval(parseHexPacket("D8005AC2A8F0")) == 1)
-    assert(eval(parseHexPacket("F600BC2D8F")) == 0)
-    assert(eval(parseHexPacket("9C005AC2F8F0")) == 0)
-    assert(eval(parseHexPacket("9C0141080250320F1802104A08")) == 1)
-  }
+  class RecursiveDescentSolutionTest extends SolutionTest(RecursiveDescentSolution)
 
-  test("Part 2 input answer") {
-    assert(eval(parseHexPacket(input)) == 1264485568252L)
-  }
+  class ParserCombinatorSolutionTest extends SolutionTest(ParserCombinatorSolution)
 }
