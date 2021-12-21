ce068ad31a81b8ec51f434c14af968a4a8175370
Simmo Saan <simmo.saan@gmail.com>
2021-12-21 08:38:24 +0200

    Generalize 2021 day 21 part 2

--- src/main/scala/eu/sim642/adventofcode2021/Day21.scala
+++ src/main/scala/eu/sim642/adventofcode2021/Day21.scala
@@ -53,16 +53,18 @@ object Day21 {
 
     def helper(p1: Player, p2: Player): (Long, Long) = {
       memo.getOrElseUpdate((p1, p2), {
-        val rollsIt = for {
-          (roll, rollCount) <- diracRollCounts.iterator
-          newP1 = p1.move(roll)
-          (newP1Cnt, p2Cnt) =
-            if (newP1.score >= 21)
-              (1L, 0L)
-            else
-              helper(p2, newP1).swap
-        } yield (rollCount * newP1Cnt, rollCount * p2Cnt)
-        rollsIt.reduce({ case ((a, b), (c, d)) => (a + c, b + d) })
+        if (p1.score >= 21)
+          (1L, 0L)
+        else if (p2.score >= 21)
+          (0L, 1L)
+        else {
+          val rollsIt = for {
+            (roll, rollCount) <- diracRollCounts.iterator
+            newP1 = p1.move(roll)
+            (p2Cnt, newP1Cnt) = helper(p2, newP1) // swapped
+          } yield (rollCount * newP1Cnt, rollCount * p2Cnt)
+          rollsIt.reduce({ case ((a, b), (c, d)) => (a + c, b + d) })
+        }
       })
     }
 
