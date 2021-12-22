965c4932f8e033cf09e1c6ad5099876d088f5305
Simmo Saan <simmo.saan@gmail.com>
2021-12-22 09:03:09 +0200

    Solve 2021 day 22 part 2

--- src/main/scala/eu/sim642/adventofcode2021/Day22.scala
+++ src/main/scala/eu/sim642/adventofcode2021/Day22.scala
@@ -26,6 +26,64 @@ object Day22 {
     simulateReboot(smallSteps).size
   }
 
+  def countReboot(steps: Seq[Step]): Long = {
+    /*val pairoverlaps = steps.combinations(2).count({
+      case Seq((_, box1), (_, box2)) =>
+        (box1 intersect box2).isDefined
+    })
+    val tripleoverlaps = steps.combinations(3).count({
+      case Seq((_, box1), (_, box2), (_, box3)) =>
+        (box1 intersect box2).flatMap(_ intersect box3).isDefined
+    })
+    val quadoverlaps = steps.combinations(4).count({
+      case Seq((_, box1), (_, box2), (_, box3), (_, box4)) =>
+        (box1 intersect box2).flatMap(_ intersect box3).flatMap(_ intersect box4).isDefined
+    })
+    println(steps.size)
+    println(pairoverlaps)
+    println(steps.size * (steps.size - 1) / 2)
+    println(tripleoverlaps)
+    println(quadoverlaps)*/
+
+    type Section = (Step, Set[Int])
+
+    def box3Size(box: Box3): BigInt = {
+      val Box3(min, max) = box
+      val r = BigInt(max.x - min.x + 1) * BigInt(max.y - min.y + 1) * BigInt(max.z - min.z + 1)
+      r
+    }
+
+    def helper(sections: Set[Section], sign: Int, acc: BigInt): Long = {
+      //println(sections)
+      val thing = sign * sections.view.map({ case ((on, box), _) =>
+        val r = if (on) box3Size(box) else BigInt(0)
+        //println(r)
+        r
+      }).sum
+      val newAcc = acc + thing
+      //println(s"$acc $thing")
+      if (sections.isEmpty) {
+        //println("----------------------")
+        newAcc.toLong
+      }
+      else {
+        //println(sections.size)
+        val newSections = for {
+          ((sectionOn, sectionBox), combinedSteps) <- sections
+          ((stepOn, stepBox), i) <- steps.zipWithIndex
+          //if !combinedSteps.contains(i)
+          if i > combinedSteps.max
+          if !(!sectionOn && stepOn)
+          interBox <- sectionBox intersect stepBox
+        //} yield ((if (i > combinedSteps.max) stepOn else sectionOn, interBox), combinedSteps + i)
+        } yield ((stepOn || sectionOn, interBox), combinedSteps + i)
+        helper(newSections, -sign, newAcc)
+      }
+    }
+
+    helper(steps.zipWithIndex.map((step, i) => (step, Set(i))).toSet, +1, 0L)
+  }
+
 
   private val stepRegex = """(on|off) x=(-?\d+)\.\.(-?\d+),y=(-?\d+)\.\.(-?\d+),z=(-?\d+)\.\.(-?\d+)""".r
 
@@ -40,5 +98,6 @@ object Day22 {
 
   def main(args: Array[String]): Unit = {
     println(countRebootSmall(parseSteps(input)))
+    println(countReboot(parseSteps(input)))
   }
 }
