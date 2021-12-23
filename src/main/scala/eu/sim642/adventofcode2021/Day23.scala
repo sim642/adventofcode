2145141472b0e1cd604f4003d324979e7777483c
Simmo Saan <simmo.saan@gmail.com>
2021-12-23 11:07:34 +0200

    Refactor 2021 day 23 to not use abstract locations

--- src/main/scala/eu/sim642/adventofcode2021/Day23.scala
+++ src/main/scala/eu/sim642/adventofcode2021/Day23.scala
@@ -1,13 +1,10 @@
 package eu.sim642.adventofcode2021
 
 import eu.sim642.adventofcode2021.Day23.Amphipod.*
-import eu.sim642.adventofcode2021.Day23.Loc.*
 import eu.sim642.adventofcodelib.Grid
 import eu.sim642.adventofcodelib.graph.{BFS, Dijkstra, GraphSearch, GraphTraversal, TargetNode, UnitNeighbors}
 import eu.sim642.adventofcodelib.pos.Pos
 import eu.sim642.adventofcodelib.GridImplicits.*
-
-import scala.collection.mutable
 
 object Day23 {
 
@@ -21,66 +18,46 @@ object Day23 {
   // TODO: restore part 1
   // TODO: optimize part 2
 
-  enum Loc(val pos: Pos) {
-    case AL2 extends Loc(Pos(3, 5))
-    case AL1 extends Loc(Pos(3, 4))
-    case AL extends Loc(Pos(3, 3))
-    case AH extends Loc(Pos(3, 2))
-    case BL2 extends Loc(Pos(5, 5))
-    case BL1 extends Loc(Pos(5, 4))
-    case BL extends Loc(Pos(5, 3))
-    case BH extends Loc(Pos(5, 2))
-    case CL2 extends Loc(Pos(7, 5))
-    case CL1 extends Loc(Pos(7, 4))
-    case CL extends Loc(Pos(7, 3))
-    case CH extends Loc(Pos(7, 2))
-    case DL2 extends Loc(Pos(9, 5))
-    case DL1 extends Loc(Pos(9, 4))
-    case DL extends Loc(Pos(9, 3))
-    case DH extends Loc(Pos(9, 2))
-
-    case HLL extends Loc(Pos(1, 1))
-    case HL extends Loc(Pos(2, 1))
-    case HAB extends Loc(Pos(4, 1))
-    case HBC extends Loc(Pos(6, 1))
-    case HCD extends Loc(Pos(8, 1))
-    case HR extends Loc(Pos(10, 1))
-    case HRR extends Loc(Pos(11, 1))
-  }
-
-  val hallwayLocs = Seq(HLL, HL, HAB, HBC, HCD, HR, HRR)
-  val roomLocs = Map(
-    Amber -> Seq(AL2, AL1, AL, AH),
-    Bronze -> Seq(BL2, BL1, BL, BH),
-    Copper -> Seq(CL2, CL1, CL, CH),
-    Desert -> Seq(DL2, DL1, DL, DH),
-  )
-  val locRooms = for {
-    (amphipod, locs) <- roomLocs
-    loc <- locs
-  } yield loc -> amphipod
-
-  type State = Map[Loc, Amphipod]
-
   private val map =
     """#############
-      |#...........#
-      |###.#.#.#.###
-      |  #.#.#.#.#
-      |  #.#.#.#.#
-      |  #.#.#.#.#
+      |#HH.H.H.H.HH#
+      |###A#B#C#D###
+      |  #A#B#C#D#
+      |  #A#B#C#D#
+      |  #A#B#C#D#
       |  #########""".stripMargin
 
-  private val mapGrid: Grid[Boolean] = map.linesIterator.map(_.toVector).toVector.mapGrid(_ == '#')
+  private val mapGrid = map.linesIterator.map(_.toVector).toVector
 
-  case class FreePathData(length: Int, blockers: Set[Loc])
-  private val freePathMap: collection.Map[Loc, collection.Map[Loc, FreePathData]] = {
-    Loc.values.view.map({ fromLoc =>
+  val hallwayPoss: Set[Pos] = (for {
+    (row, y) <- mapGrid.view.zipWithIndex
+    (cell, x) <- row.view.zipWithIndex
+    if cell == 'H'
+  } yield Pos(x, y)).toSet
 
-      case class PathPos(pos: Pos)(val blockers: Set[Loc])
+  val roomPoss: Map[Amphipod, Set[Pos]] = (for {
+    (row, y) <- mapGrid.view.zipWithIndex
+    (cell, x) <- row.view.zipWithIndex
+    if "ABCD".contains(cell)
+  } yield parseAmphipod(cell.toString) -> Pos(x, y)).toSet.groupMap(_._1)(_._2)
+
+  val posRooms: Map[Pos, Amphipod] = for {
+    (amphipod, poss) <- roomPoss
+    pos <- poss
+  } yield pos -> amphipod
+
+  val allPoss: Set[Pos] = hallwayPoss ++ roomPoss.values.flatten
+
+  type State = Map[Pos, Amphipod]
+
+  case class FreePathData(length: Int, blockers: Set[Pos])
+  private val freePathMap: collection.Map[Pos, collection.Map[Pos, FreePathData]] = {
+    allPoss.view.map({ fromPos =>
+
+      case class PathPos(pos: Pos)(val blockers: Set[Pos])
 
       val graphTraversal = new GraphTraversal[PathPos] with UnitNeighbors[PathPos] {
-        override val startNode: PathPos = PathPos(fromLoc.pos)(Set.empty)
+        override val startNode: PathPos = PathPos(fromPos)(Set.empty)
 
         override def unitNeighbors(pathPos: PathPos): IterableOnce[PathPos] = {
           val PathPos(pos) = pathPos
@@ -89,8 +66,8 @@ object Day23 {
           for {
             offset <- Pos.axisOffsets.iterator
             newPos = pos + offset
-            if !mapGrid(newPos)
-            newBlockers = Loc.values.find(_.pos == newPos).map(blockers + _).getOrElse(blockers)
+            if mapGrid(newPos) != '#'
+            newBlockers = if (allPoss.contains(newPos)) blockers + newPos else blockers
           } yield PathPos(newPos)(newBlockers)
         }
       }
@@ -99,15 +76,15 @@ object Day23 {
       val distances2 =
         for {
           (pathPos@PathPos(pos), length) <- distances
-          loc <- Loc.values.find(_.pos == pos)
-        } yield loc -> FreePathData(length, pathPos.blockers)
-      fromLoc -> distances2
+          if allPoss.contains(pos)
+        } yield pos -> FreePathData(length, pathPos.blockers)
+      fromPos -> distances2
     }).toMap
   }
 
-  def freePathLength(occupied: Set[Pos], fromLoc: Loc, toLoc: Loc): Option[Int] = {
-    val FreePathData(length, blockers) = freePathMap(fromLoc)(toLoc)
-    if (blockers.exists(b => occupied.contains(b.pos)))
+  def freePathLength(occupied: Set[Pos], fromPos: Pos, toPos: Pos): Option[Int] = {
+    val FreePathData(length, blockers) = freePathMap(fromPos)(toPos)
+    if (blockers.exists(occupied.contains))
       None
     else
       Some(length)
@@ -120,55 +97,44 @@ object Day23 {
 
       override def neighbors(state: State): IterableOnce[(State, Int)] = {
 
-        val occupied = state.keys.map(_.pos).toSet
+        val occupied = state.keySet
 
-        def room2hallway(fromLoc: Loc, amphipod: Amphipod): Seq[(Loc, Int)] = {
-          if (locRooms(fromLoc) == amphipod && roomLocs(amphipod).flatMap(state.get).forall(_ == amphipod)) // forbid useless move out of final (correct) room
-            Seq.empty
+        def room2hallway(fromPos: Pos, amphipod: Amphipod): Set[(Pos, Int)] = {
+          if (posRooms(fromPos) == amphipod && roomPoss(amphipod).flatMap(state.get).forall(_ == amphipod)) // forbid useless move out of final (correct) room
+            Set.empty
           else {
             for {
-              toLoc <- hallwayLocs
-              length <- freePathLength(occupied, fromLoc, toLoc)
-            } yield toLoc -> length
+              toPos <- hallwayPoss
+              length <- freePathLength(occupied, fromPos, toPos)
+            } yield toPos -> length
           }
         }
 
-        def hallway2room(fromLoc: Loc, amphipod: Amphipod): Seq[(Loc, Int)] = {
-          if (roomLocs(amphipod).flatMap(state.get).forall(_ == amphipod)) {
+        def hallway2room(fromPos: Pos, amphipod: Amphipod): Set[(Pos, Int)] = {
+          if (roomPoss(amphipod).flatMap(state.get).forall(_ == amphipod)) {
             for {
-              toLoc <- roomLocs(amphipod)
-              length <- freePathLength(occupied, fromLoc, toLoc)
-            } yield toLoc -> length
+              toPos <- roomPoss(amphipod)
+              length <- freePathLength(occupied, fromPos, toPos)
+            } yield toPos -> length
           }
           else
-            Seq.empty
+            Set.empty
         }
 
         for {
-          (fromLoc, amphipod) <- state.iterator
-          (toLoc, length) <- if (hallwayLocs.contains(fromLoc)) hallway2room(fromLoc, amphipod) else room2hallway(fromLoc, amphipod)
-          newState = state - fromLoc + (toLoc -> amphipod)
+          (fromPos, amphipod) <- state.iterator
+          (toLoc, length) <- if (hallwayPoss.contains(fromPos)) hallway2room(fromPos, amphipod) else room2hallway(fromPos, amphipod)
+          newState = state - fromPos + (toLoc -> amphipod)
         } yield newState -> amphipod.energy * length
       }
 
-      override val targetNode: State = Map(
-        AH -> Amber,
-        AL -> Amber,
-        AL1 -> Amber,
-        AL2 -> Amber,
-        BH -> Bronze,
-        BL -> Bronze,
-        BL1 -> Bronze,
-        BL2 -> Bronze,
-        CH -> Copper,
-        CL -> Copper,
-        CL1 -> Copper,
-        CL2 -> Copper,
-        DH -> Desert,
-        DL -> Desert,
-        DL1 -> Desert,
-        DL2 -> Desert,
-      )
+      override val targetNode: State = {
+        (for {
+          (row, y) <- mapGrid.view.zipWithIndex
+          (cell, x) <- row.view.zipWithIndex
+          if "ABCD".contains(cell)
+        } yield Pos(x, y) -> parseAmphipod(cell.toString)).toMap
+      }
     }
 
     Dijkstra.search(graphSearch).target.get._2
@@ -182,33 +148,19 @@ object Day23 {
     case "D" => Desert
   }
 
-  private val stateRegex =
-    """#############
-      |#\.\.\.\.\.\.\.\.\.\.\.#
-      |###(.)#(.)#(.)#(.)###
-      |  #(.)#(.)#(.)#(.)#
-      |  #########""".stripMargin.r
+  private val part2Lines =
+    """  #D#C#B#A#
+      |  #D#B#A#C#""".stripMargin
 
-  def parseState(input: String): State = input match {
-    case stateRegex(ah, bh, ch, dh, al, bl, cl, dl) =>
-      Map(
-        AH -> parseAmphipod(ah),
-        AL -> Desert,
-        AL1 -> Desert,
-        AL2 -> parseAmphipod(al),
-        BH -> parseAmphipod(bh),
-        BL -> Copper,
-        BL1 -> Bronze,
-        BL2 -> parseAmphipod(bl),
-        CH -> parseAmphipod(ch),
-        CL -> Bronze,
-        CL1 -> Amber,
-        CL2 -> parseAmphipod(cl),
-        DH -> parseAmphipod(dh),
-        DL -> Amber,
-        DL1 -> Copper,
-        DL2 -> parseAmphipod(dl),
-      )
+  def parseState(input: String): State = {
+    val lines = input.linesIterator.toVector
+    val lines2 = lines.take(3) ++ part2Lines.linesIterator.toVector ++ lines.drop(3)
+    val grid = lines2.map(_.toVector)
+    (for {
+      (row, y) <- grid.view.zipWithIndex
+      (cell, x) <- row.view.zipWithIndex
+      if "ABCD".contains(cell)
+    } yield Pos(x, y) -> parseAmphipod(cell.toString)).toMap
   }
 
   lazy val input: String = io.Source.fromInputStream(getClass.getResourceAsStream("day23.txt")).mkString.trim
