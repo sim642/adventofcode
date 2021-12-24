554eafeda68da0a426f3b1eff2ec9fc57e4dc704
Simmo Saan <simmo.saan@gmail.com>
2021-12-24 12:44:29 +0200

    Add other inputs with answers to 2021 day 24

--- README.md
+++ README.md
@@ -10,4 +10,4 @@
 | 2018 | [Solutions](src/main/scala/eu/sim642/adventofcode2018) | [My inputs](src/main/resources/eu/sim642/adventofcode2018), [other inputs](src/test/resources/eu/sim642/adventofcode2018) | [Tests](src/test/scala/eu/sim642/adventofcode2018) |
 | 2019 | [Solutions](src/main/scala/eu/sim642/adventofcode2019) | [My inputs](src/main/resources/eu/sim642/adventofcode2019) | [Tests](src/test/scala/eu/sim642/adventofcode2019) |
 | 2020 | [Solutions](src/main/scala/eu/sim642/adventofcode2020) | [My inputs](src/main/resources/eu/sim642/adventofcode2020), [other inputs](src/test/resources/eu/sim642/adventofcode2020) | [Tests](src/test/scala/eu/sim642/adventofcode2020) |
-| 2021 | [Solutions](src/main/scala/eu/sim642/adventofcode2021) | [My inputs](src/main/resources/eu/sim642/adventofcode2021) | [Tests](src/test/scala/eu/sim642/adventofcode2021) |
+| 2021 | [Solutions](src/main/scala/eu/sim642/adventofcode2021) | [My inputs](src/main/resources/eu/sim642/adventofcode2021), [other inputs](src/test/resources/eu/sim642/adventofcode2021) | [Tests](src/test/scala/eu/sim642/adventofcode2021) |
--- src/test/scala/eu/sim642/adventofcode2021/Day24Test.scala
+++ src/test/scala/eu/sim642/adventofcode2021/Day24Test.scala
@@ -5,11 +5,39 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class Day24Test extends AnyFunSuite {
 
+  lazy val emilyskidsisterInput: String = io.Source.fromInputStream(getClass.getResourceAsStream("day24emilyskidsister.txt")).mkString.trim
+  lazy val dphilipsonInput: String = io.Source.fromInputStream(getClass.getResourceAsStream("day24dphilipson.txt")).mkString.trim
+  lazy val seligmanInput: String = io.Source.fromInputStream(getClass.getResourceAsStream("day24seligman.txt")).mkString.trim
+
   test("Part 1 input answer") {
     assert(maxModelNumber(parseSteps(input)) == "97919997299495")
   }
 
   test("Part 2 input answer") {
     assert(minModelNumber(parseSteps(input)) == "51619131181131")
+  }
+
+  test("Part 1 emilyskidsister") {
+    assert(maxModelNumber(parseSteps(emilyskidsisterInput)) == "79997391969649")
+  }
+
+  test("Part 2 emilyskidsister") {
+    assert(minModelNumber(parseSteps(emilyskidsisterInput)) == "16931171414113")
+  }
+
+  test("Part 1 dphilipson") {
+    assert(maxModelNumber(parseSteps(dphilipsonInput)) == "98491959997994")
+  }
+
+  test("Part 2 dphilipson") {
+    assert(minModelNumber(parseSteps(dphilipsonInput)) == "61191516111321")
+  }
+
+  test("Part 1 seligman") {
+    assert(maxModelNumber(parseSteps(seligmanInput)) == "96299896449997")
+  }
+
+  test("Part 2 seligman") {
+    assert(minModelNumber(parseSteps(seligmanInput)) == "31162141116841")
   }
 }
