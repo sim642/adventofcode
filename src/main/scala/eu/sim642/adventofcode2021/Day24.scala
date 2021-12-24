2d1d847abb2e8b532ce8680ea80c5275fe662785
Simmo Saan <simmo.saan@gmail.com>
2021-12-24 09:12:59 +0200

    Solve 2021 day 24

---
+++ src/main/scala/eu/sim642/adventofcode2021/Day24.scala
@@ -0,0 +1,50 @@
+package eu.sim642.adventofcode2021
+
+object Day24 {
+
+  //lazy val input: String = io.Source.fromInputStream(getClass.getResourceAsStream("day24.txt")).mkString.trim
+
+  def main(args: Array[String]): Unit = {
+    /*val w1 = 9
+    val w2 = 9
+    val w3 = 9
+    val w4 = 9
+    val z1 = w1 + 7
+    val z2 = 26 * z1 + w2 + 8
+    val z3 = 26 * z2 + w3 + 2
+    val z4 = 26 * z3 + w4 + 11
+    //println(z4 % 26)
+    val w5 = 9
+    val z5 = 26 * (z4 / 26) + w5 + 6
+    //println(z5 % 26)
+    val w6 = 9
+    val w7 = 9
+    val z6 = 26 * z5 + w6 + 12
+    val z7 = 26 * z6 + w7 + 14
+    //println(z7 % 26)
+    val w8 = 7
+    val z8 = z7 / 26
+    val w9 = 9
+    val z9 = 26 * z8 + w9 + 15
+    //println(z9 % 26)
+    val w10 = 9
+    val z10 = 26 * (z9 / 26) + w10 + 10
+    //println(z10 % 26)
+    val w11 = 7
+    val z11 = z10 / 26
+    //println(z11 % 26)
+    val w12 = 9
+    val z12 = 26 * (z11 / 26) + w12 + 10
+    //println(z12 % 26)
+    val w13 = 9
+    val z13 = 26 * (z12 / 26) + w13 + 8
+    //println(z13 % 26)
+    val w14 = 6
+    val z14 = z13 / 26
+    println(z14)*/
+
+
+    // Part 1: 97919997299495
+    // Part 2: 51619131181131
+  }
+}
