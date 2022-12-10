b87273e5aa53e58c2d26afafc48b43556d7a6285
Simmo Saan <simmo.saan@gmail.com>
2022-12-10 08:45:06 +0200

    Solve 2022 day 10 part 1

---
+++ src/main/scala/eu/sim642/adventofcode2022/Day10.scala
@@ -0,0 +1,48 @@
+package eu.sim642.adventofcode2022
+
+object Day10 {
+
+  enum Instruction(val cycles: Int) {
+    case Addx(v: Int) extends Instruction(2)
+    case Noop extends Instruction(1)
+  }
+
+  case class CPU(instructions: Seq[Instruction], instructionCycle: Int = 0, x: Int = 1) {
+
+    def tick: CPU = {
+      val newInstructionCycle = instructionCycle + 1
+      val instruction = instructions.head
+      if (newInstructionCycle == instruction.cycles) { // complete instruction
+        val newCpu = instruction match {
+          case Instruction.Addx(v) => copy(x = x + v)
+          case Instruction.Noop => this
+        }
+        newCpu.copy(instructions = instructions.tail, instructionCycle = 0)
+      }
+      else
+        copy(instructionCycle = newInstructionCycle)
+    }
+
+    def run(): Iterator[CPU] = Iterator.iterate(this)(_.tick)
+  }
+
+  def sumSignalStrengths(instructions: Seq[Instruction]): Int = {
+    val cpus = CPU(instructions).run().take(220).toIndexedSeq
+    (20 to 220 by 40).map(i => i * cpus(i - 1).x).sum
+  }
+
+
+  def parseInstruction(s: String): Instruction = s match {
+    case s"addx $v" => Instruction.Addx(v.toInt)
+    case "noop" => Instruction.Noop
+  }
+
+  def parseInstructions(input: String): Seq[Instruction] = input.linesIterator.map(parseInstruction).toSeq
+
+
+  lazy val input: String = io.Source.fromInputStream(getClass.getResourceAsStream("day10.txt")).mkString.trim
+
+  def main(args: Array[String]): Unit = {
+    println(sumSignalStrengths(parseInstructions(input)))
+  }
+}
---
+++ src/test/scala/eu/sim642/adventofcode2022/Day10Test.scala
@@ -0,0 +1,169 @@
+package eu.sim642.adventofcode2022
+
+import Day10._
+import org.scalatest.funsuite.AnyFunSuite
+
+class Day10Test extends AnyFunSuite {
+
+  val exampleInput =
+    """noop
+      |addx 3
+      |addx -5""".stripMargin
+
+  val exampleInput2 =
+    """addx 15
+      |addx -11
+      |addx 6
+      |addx -3
+      |addx 5
+      |addx -1
+      |addx -8
+      |addx 13
+      |addx 4
+      |noop
+      |addx -1
+      |addx 5
+      |addx -1
+      |addx 5
+      |addx -1
+      |addx 5
+      |addx -1
+      |addx 5
+      |addx -1
+      |addx -35
+      |addx 1
+      |addx 24
+      |addx -19
+      |addx 1
+      |addx 16
+      |addx -11
+      |noop
+      |noop
+      |addx 21
+      |addx -15
+      |noop
+      |noop
+      |addx -3
+      |addx 9
+      |addx 1
+      |addx -3
+      |addx 8
+      |addx 1
+      |addx 5
+      |noop
+      |noop
+      |noop
+      |noop
+      |noop
+      |addx -36
+      |noop
+      |addx 1
+      |addx 7
+      |noop
+      |noop
+      |noop
+      |addx 2
+      |addx 6
+      |noop
+      |noop
+      |noop
+      |noop
+      |noop
+      |addx 1
+      |noop
+      |noop
+      |addx 7
+      |addx 1
+      |noop
+      |addx -13
+      |addx 13
+      |addx 7
+      |noop
+      |addx 1
+      |addx -33
+      |noop
+      |noop
+      |noop
+      |addx 2
+      |noop
+      |noop
+      |noop
+      |addx 8
+      |noop
+      |addx -1
+      |addx 2
+      |addx 1
+      |noop
+      |addx 17
+      |addx -9
+      |addx 1
+      |addx 1
+      |addx -3
+      |addx 11
+      |noop
+      |noop
+      |addx 1
+      |noop
+      |addx 1
+      |noop
+      |noop
+      |addx -13
+      |addx -19
+      |addx 1
+      |addx 3
+      |addx 26
+      |addx -30
+      |addx 12
+      |addx -1
+      |addx 3
+      |addx 1
+      |noop
+      |noop
+      |noop
+      |addx -9
+      |addx 18
+      |addx 1
+      |addx 2
+      |noop
+      |noop
+      |addx 9
+      |noop
+      |noop
+      |noop
+      |addx -1
+      |addx 2
+      |addx -37
+      |addx 1
+      |addx 3
+      |noop
+      |addx 15
+      |addx -21
+      |addx 22
+      |addx -6
+      |addx 1
+      |noop
+      |addx 2
+      |addx 1
+      |noop
+      |addx -10
+      |noop
+      |noop
+      |addx 20
+      |addx 1
+      |addx 2
+      |addx 2
+      |addx -6
+      |addx -11
+      |noop
+      |noop
+      |noop""".stripMargin
+
+  test("Part 1 examples") {
+    //CPU(parseInstructions(exampleInput)).run().foreach(println)
+    assert(sumSignalStrengths(parseInstructions(exampleInput2)) == 13140)
+  }
+
+  test("Part 1 input answer") {
+    assert(sumSignalStrengths(parseInstructions(input)) == 11960)
+  }
+}
