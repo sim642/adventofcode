7f40a90e74f358b80a760cfb30c0275bd8bf070d
Simmo Saan <simmo.saan@gmail.com>
2022-12-20 10:52:35 +0200

    Solve 2022 day 20 part 2

--- src/main/scala/eu/sim642/adventofcode2022/Day20.scala
+++ src/main/scala/eu/sim642/adventofcode2022/Day20.scala
@@ -1,48 +1,124 @@
 package eu.sim642.adventofcode2022
 
-import eu.sim642.adventofcodelib.IteratorImplicits._
-import eu.sim642.adventofcodelib.IntegralImplicits._
-import eu.sim642.adventofcodelib.SeqImplicits._
+import eu.sim642.adventofcode2018.Day9.CircularZipper
+import eu.sim642.adventofcodelib.IteratorImplicits.*
+import eu.sim642.adventofcodelib.IntegralImplicits.*
+import eu.sim642.adventofcodelib.SeqImplicits.*
+
+import scala.collection.mutable
 
 object Day20 {
 
-  def mixOne(fileIndex: Vector[(Int, Int)], i0: Int): Vector[(Int, Int)] = {
-    /*val i = file.indexOf(value)
+  object Part1 {
+
+    def mixOne(fileIndex: Vector[(Int, Int)], i0: Int): Vector[(Int, Int)] = {
+      val i = fileIndex.indexWhere(_._2 == i0)
+      val value = fileIndex(i)._1
+      val (init, _ +: tail) = fileIndex.splitAt(i): @unchecked
+      val newI = (i + value) %+ (fileIndex.size - 1)
+      val (init2, tail2) = (init ++ tail).splitAt(newI)
+      val r = init2 ++ ((value, i0) +: tail2)
+      //println((init, tail))
+      //println(newI)
+      //println((init2, tail2))
+      //println(r)
+      r
+      //val i = file.indexOf(value)
+      /*val i = fileIndex.indexWhere(_._2 == i0)
+      val value = fileIndex(i)._1
+      val newI = (i + value + (if (value < 0) -1 else 0)) %+ fileIndex.size
+      //println(s"$i $newI $value")
+      //(value +: file.rotateLeft(i).tail.rotateLeft(value)).rotateRight(newI + (if (value < 0) -1 else 0))
+      val v1 = fileIndex.rotateLeft(i)
+      val v2 = v1.tail
+      val v3 = v2.rotateLeft(value)
+      val v4 = (value, i0) +: v3
+      val v5 = v4.rotateRight(newI + (if (i + value >= fileIndex.size) 1 else 0))
+      //println(v1)
+      //println(v2)
+      //println(v3)
+      //println(v4)
+      //println(v5)
+      v5*/
+    }
+
+    def iterateMix(file: Vector[Int]): Iterator[Vector[Int]] = {
+      file.indices.iterator.scanLeft(file.zipWithIndex)(mixOne).map(_.map(_._1))
+    }
+
+    def mix(file: Vector[Int]): Vector[Int] = iterateMix(file).last
+
+    def groveCoordinates(file: Vector[Int]): Int = {
+      val i = file.indexOf(0)
+      (1000 to 3000 by 1000).view.map(o => (i + o) %+ file.size).map(file).sum
+    }
+
+    def mixGroveCoordinates(file: Vector[Int]): Int = groveCoordinates(mix(file))
+  }
+
+  object Part2 {
+
+    def mixOne(fileIndex: Vector[(Long, Int)], i0: Int): Vector[(Long, Int)] = {
+      val i = fileIndex.indexWhere(_._2 == i0)
+      val value = fileIndex(i)._1
+      val (init, _ +: tail) = fileIndex.splitAt(i): @unchecked
+      val newI = (i.toLong + value) %+ (fileIndex.size.toLong - 1)
+      val (init2, tail2) = (init ++ tail).splitAt(newI.toInt)
+      val r = init2 ++ ((value, i0) +: tail2)
+      //println((init, tail))
+      //println(newI)
+      //println((init2, tail2))
+      //println(r)
+      r
+      /*val i = file.indexOf(value)
     val (init, _ +: tail) = file.splitAt(i): @unchecked
     val newI = (i + value) %+ file.size
     val (init2, tail2) = (init ++ tail).splitAt(newI + (if (value < 0) -1 else 0))
     init2 ++ (value +: tail2)*/
-    //val i = file.indexOf(value)
-    val i = fileIndex.indexWhere(_._2 == i0)
-    val value = fileIndex(i)._1
-    val newI = (i + value + (if (value < 0) -1 else 0)) %+ fileIndex.size
-    //println(s"$i $newI $value")
-    //(value +: file.rotateLeft(i).tail.rotateLeft(value)).rotateRight(newI + (if (value < 0) -1 else 0))
-    val v1 = fileIndex.rotateLeft(i)
-    val v2 = v1.tail
-    val v3 = v2.rotateLeft(value)
-    val v4 = (value, i0) +: v3
-    val v5 = v4.rotateRight(newI + (if (i + value >= fileIndex.size) 1 else 0))
-    //println(v1)
-    //println(v2)
-    //println(v3)
-    //println(v4)
-    //println(v5)
-    v5
+      //val i = file.indexOf(value)
+      /*val i = fileIndex.indexWhere(_._2 == i0)
+      val value = fileIndex(i)._1
+      val newI = (i + value + (if (value < 0) -1 else 0)) %+ fileIndex.size
+      //println(s"$i $newI $value")
+      //(value +: file.rotateLeft(i).tail.rotateLeft(value)).rotateRight(newI + (if (value < 0) -1 else 0))
+      val v1 = fileIndex.rotateLeft(i)
+      val v2 = v1.tail
+      val v3 = v2.rotateLeft((value %+ v2.size).toInt)
+      val v4 = (value, i0) +: v3
+      val v5 = v4.rotateRight(newI.toInt + (if (i + value >= fileIndex.size) (i + value) / fileIndex.size else 0).toInt)
+      //println(v1)
+      //println(v2)
+      //println(v3)
+      //println(v4)
+      //println(v5)
+      v5*/
+    }
+
+    def iterateMix(file: Vector[(Long, Int)]): Iterator[Vector[(Long, Int)]] = {
+      file.indices.iterator.scanLeft(file)(mixOne)
+    }
+
+    def mix(file: Vector[(Long, Int)]): Vector[(Long, Int)] = {
+      //println(s"before: $file")
+      val r = iterateMix(file).last
+      //println(s"after: $r")
+      r
+    }
+
+    def iterateMixRounds(file: Vector[Int]): Iterator[Vector[(Long, Int)]] = {
+      val init = file.map(_ * 811589153L).zipWithIndex
+      Iterator.iterate(init)(mix)
+    }
+
+    def mixRounds(file: Vector[Int], rounds: Int = 10): Vector[Long] = iterateMixRounds(file)(rounds).map(_._1)
+
+    def groveCoordinates(file: Vector[Long]): Long = {
+      val i = file.indexOf(0)
+      (1000 to 3000 by 1000).view.map(o => (i + o) %+ file.size).map(file).sum
+    }
+
+    def mixGroveCoordinates(file: Vector[Int]): Long = groveCoordinates(mixRounds(file))
   }
-
-  def iterateMix(file: Vector[Int]): Iterator[Vector[Int]] = {
-    file.indices.iterator.scanLeft(file.zipWithIndex)(mixOne).map(_.map(_._1))
-  }
-
-  def mix(file: Vector[Int]): Vector[Int] = iterateMix(file).last
-
-  def groveCoordinates(file: Vector[Int]): Int = {
-    val i = file.indexOf(0)
-    (1000 to 3000 by 1000).view.map(o => (i + o) %+ file.size).map(file).sum
-  }
-
-  def mixGroveCoordinates(file: Vector[Int]): Int = groveCoordinates(mix(file))
 
 
   def parseFile(input: String): Vector[Int] = input.linesIterator.map(_.toInt).toVector
@@ -50,8 +126,10 @@ object Day20 {
   lazy val input: String = io.Source.fromInputStream(getClass.getResourceAsStream("day20.txt")).mkString.trim
 
   def main(args: Array[String]): Unit = {
-    println(mixGroveCoordinates(parseFile(input)))
+    println(Part1.mixGroveCoordinates(parseFile(input)))
+    println(Part2.mixGroveCoordinates(parseFile(input)))
 
     // part 1: 3460 - too low (assuming distinct)
+    // part 2: -14083506572009 - not right
   }
 }
--- src/test/scala/eu/sim642/adventofcode2022/Day20Test.scala
+++ src/test/scala/eu/sim642/adventofcode2022/Day20Test.scala
@@ -15,15 +15,15 @@ class Day20Test extends AnyFunSuite with ScalaCheckPropertyChecks {
       |0
       |4""".stripMargin
 
-  test("mix") {
+  /*test("mix") {
     //assert(mixOne(Vector(4, 5, 6, 1, 7, 8, 9), 1) == Vector(4, 5, 6, 7, 1, 8, 9))
     //assert(mixOne(Vector(4, -2, 5, 6, 7, 8, 9), -2) == Vector(4, 5, 6, 7, 8, -2, 9))
-  }
+  }*/
 
   test("Part 1 examples") {
     val exampleFile = parseFile(exampleInput)
 
-    val arrangements = Table(
+    /*val arrangements = Table(
       "arrangement",
       Seq(1, 2, -3, 3, -2, 0, 4),
       Seq(2, 1, -3, 3, -2, 0, 4),
@@ -35,16 +35,47 @@ class Day20Test extends AnyFunSuite with ScalaCheckPropertyChecks {
       Seq(1, 2, -3, 4, 0, 3, -2),
     )
 
-    val it = iterateMix(exampleFile)
+    val it = Part1.iterateMix(exampleFile)
     forAll (arrangements) { arrangement =>
       assert(it.next() == arrangement)
     }
 
-    assert(mix(exampleFile) == Vector(1, 2, -3, 4, 0, 3, -2))
-    assert(mixGroveCoordinates(exampleFile) == 3)
+    assert(Part1.mix(exampleFile) == Vector(1, 2, -3, 4, 0, 3, -2))*/
+    assert(Part1.mixGroveCoordinates(exampleFile) == 3)
   }
 
   test("Part 1 input answer") {
-    assert(mixGroveCoordinates(parseFile(input)) == 4224) // TODO: optimize
+    assert(Part1.mixGroveCoordinates(parseFile(input)) == 4224) // TODO: optimize
+  }
+
+  test("Part 2 examples") {
+    val exampleFile = parseFile(exampleInput)
+
+    /*val arrangements = Table(
+      "arrangement",
+      Seq(811589153, 1623178306, -2434767459L, 2434767459L, -1623178306, 0, 3246356612L),
+      Seq(0, -2434767459L, 3246356612L, -1623178306, 2434767459L, 1623178306, 811589153),
+      Seq(0, 2434767459L, 1623178306, 3246356612L, -2434767459L, -1623178306, 811589153),
+      Seq(0, 811589153, 2434767459L, 3246356612L, 1623178306, -1623178306, -2434767459L),
+      Seq(0, 1623178306, -2434767459L, 811589153, 2434767459L, 3246356612L, -1623178306),
+      Seq(0, 811589153, -1623178306, 1623178306, -2434767459L, 3246356612L, 2434767459L),
+      Seq(0, 811589153, -1623178306, 3246356612L, -2434767459L, 1623178306, 2434767459L),
+      Seq(0, -2434767459L, 2434767459L, 1623178306, -1623178306, 811589153, 324635661),
+      Seq(0, 1623178306, 3246356612L, 811589153, -2434767459L, 2434767459L, -1623178306),
+      Seq(0, 811589153, 1623178306, -2434767459L, 3246356612L, 2434767459L, -1623178306),
+      Seq(0, -2434767459L, 1623178306, 3246356612L, -1623178306, 2434767459L, 811589153),
+    )*/
+
+    //val it = Part2.iterateMixRounds(exampleFile)
+    //forAll (arrangements) { arrangement =>
+    //  assert(it.next() == arrangement)
+    //}
+    //
+    //assert(Part2.mixRounds(exampleFile) == Vector(0, -2434767459L, 1623178306, 3246356612L, -1623178306, 2434767459L, 811589153))
+    assert(Part2.mixGroveCoordinates(exampleFile) == 1623178306)
+  }
+
+  test("Part 2 input answer") {
+    assert(Part2.mixGroveCoordinates(parseFile(input)) == 861907680486L) // TODO: optimize
   }
 }
