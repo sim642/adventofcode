a92a67c2bcf04c84d9f07715d622efaa0c7e8f7f
Simmo Saan <simmo.saan@gmail.com>
2022-12-25 13:37:12 +0200

    Simplify 2022 day 25

--- src/main/scala/eu/sim642/adventofcode2022/Day25.scala
+++ src/main/scala/eu/sim642/adventofcode2022/Day25.scala
@@ -1,13 +1,12 @@
 package eu.sim642.adventofcode2022
 
 import Integral.Implicits.*
-import scala.annotation.tailrec
 
 object Day25 {
 
   type Snafu = String
 
-  def snafuDigit2int(c: Char): Int = c match {
+  val snafuDigit2int: PartialFunction[Char, Int] = {
     case '=' => -2
     case '-' => -1
     case '0' => 0
@@ -15,13 +14,10 @@ object Day25 {
     case '2' => 2
   }
 
-  def snafu2int(snafu: Snafu): Long = {
-    snafu.foldLeft(0L)({ (acc, c) =>
-      5 * acc + snafuDigit2int(c)
-    })
-  }
+  def snafu2int(snafu: Snafu): Long =
+    snafu.foldLeft(0L)(5 * _ + snafuDigit2int(_))
 
-  def int2snafuDigit(d: Int): Char = d match {
+  val int2snafuDigit: PartialFunction[Int, Char] = {
     case -2 => '='
     case -1 => '-'
     case 0 => '0'
@@ -30,39 +26,22 @@ object Day25 {
   }
 
   def int2snafu(int: Long): Snafu = {
-
-    @tailrec
-    def int2base5(int: Long, acc: List[Int]): List[Int] = {
-      val (q, r) = int /% 5
-      val newAcc = r.toInt :: acc
-      if (q == 0)
-        newAcc
-      else
-        int2base5(q, newAcc)
-    }
-
-    @tailrec
-    def base52balBase5(digits: List[Int], carry: Int, acc: List[Int]): List[Int] = digits match {
-      case Nil if carry == 0 =>
-        acc
-      case Nil =>
-        carry :: acc
-      case digit :: newDigits =>
-        val (newDigit, newCarry) = digit + carry match {
-          case 0 => (0, 0)
-          case 1 => (1, 0)
-          case 2 => (2, 0)
-          case 3 => (-2, 1)
-          case 4 => (-1, 1)
-          case 5 => (0, 1)
-        }
-        val newAcc = newDigit :: acc
-        base52balBase5(newDigits, newCarry, newAcc)
-    }
-
-    val base5 = int2base5(int, Nil)
-    val balBase5 = base52balBase5(base5.reverse, 0, Nil)
-    balBase5.map(int2snafuDigit).mkString
+    Seq.unfold(int)({ int =>
+      if (int == 0)
+        None
+      else {
+        val (q, r) = int /% 5
+        if (r <= 2)
+          Some((r, q))
+        else
+          Some((r - 5, q + 1)) // subtract from carried instead
+      }
+    })
+      .view
+      .reverse
+      .map(_.toInt)
+      .map(int2snafuDigit)
+      .mkString
   }
 
   def sumSnafus(snafus: Seq[Snafu]): Snafu =
