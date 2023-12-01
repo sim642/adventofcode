f3f5fe5c4a167d270b672867778dfc3724dfb167
Simmo Saan <simmo.saan@gmail.com>
2023-12-01 08:37:29 +0200

    Solve 2023 day 1 part 2

--- src/main/scala/eu/sim642/adventofcode2023/Day1.scala
+++ src/main/scala/eu/sim642/adventofcode2023/Day1.scala
@@ -2,18 +2,73 @@ package eu.sim642.adventofcode2023
 
 object Day1 {
 
-  def recoverCalibrationValue(line: String): Int = {
-    val digits = line.filter(_.isDigit).map(_.asDigit)
-    10 * digits.head + digits.last
+  trait Part {
+    def recoverCalibrationValue(line: String): Int
+
+    def sumCalibrationValues(document: Seq[String]): Int = document.map(recoverCalibrationValue).sum
   }
 
-  def sumCalibrationValues(document: Seq[String]): Int = document.map(recoverCalibrationValue).sum
+  object Part1 extends Part {
+    def recoverCalibrationValue(line: String): Int = {
+      val digits = line.filter(_.isDigit).map(_.asDigit)
+      10 * digits.head + digits.last
+    }
+  }
+
+  object Part2 extends Part {
+
+    private val digitStrings = Map(
+      1 -> "one",
+      2 -> "two",
+      3 -> "three",
+      4 -> "four",
+      5 -> "five",
+      6 -> "six",
+      7 -> "seven",
+      8 -> "eight",
+      9 -> "nine",
+    )
+
+    private val map2: Map[String, Int] = for {
+      (d, s) <- digitStrings
+    } yield s -> d
+
+    /*def recoverCalibrationValue(line: String): Int = {
+      val line2 = digitStrings.foldLeft(line)({ case (line, (digit, string)) =>
+        line.replaceAll(string, digit.toString)
+      })
+      println(Part1.recoverCalibrationValue(line2))
+      Part1.recoverCalibrationValue(line2)
+    }*/
+
+    def recoverCalibrationValue(line: String): Int = {
+      val first = digitStrings.values.filter(line.contains).map(line.indexOf).minOption
+      val last = digitStrings.values.map(line.lastIndexOf).maxOption
+      val digit1 = line.indexWhere(_.isDigit)
+      val digit2 = line.lastIndexWhere(_.isDigit)
+      val x =
+        if (digit1 >= 0 && (first.isEmpty || digit1 < first.get))
+          line(digit1).asDigit
+        else {
+          map2(digitStrings.values.filter(line.contains).minBy(line.indexOf))
+        }
+      val y =
+        if (last.isEmpty || digit2 > last.get)
+          line(digit2).asDigit
+        else {
+          map2(digitStrings.values.maxBy(line.lastIndexOf))
+        }
+      //println(10 * x + y)
+      10 * x + y
+    }
+  }
 
   def parseDocument(input: String): Seq[String] = input.linesIterator.toSeq
 
   lazy val input: String = io.Source.fromInputStream(getClass.getResourceAsStream("day1.txt")).mkString.trim
 
   def main(args: Array[String]): Unit = {
-    println(sumCalibrationValues(parseDocument(input)))
+    println(Part1.sumCalibrationValues(parseDocument(input)))
+    println(Part2.sumCalibrationValues(parseDocument(input)))
   }
 }
--- src/test/scala/eu/sim642/adventofcode2023/Day1Test.scala
+++ src/test/scala/eu/sim642/adventofcode2023/Day1Test.scala
@@ -11,11 +11,28 @@ class Day1Test extends AnyFunSuite {
       |a1b2c3d4e5f
       |treb7uchet""".stripMargin
 
+  val exampleInput2 =
+    """two1nine
+      |eightwothree
+      |abcone2threexyz
+      |xtwone3four
+      |4nineeightseven2
+      |zoneight234
+      |7pqrstsixteen""".stripMargin
+
   test("Part 1 examples") {
-    assert(sumCalibrationValues(parseDocument(exampleInput)) == 142)
+    assert(Part1.sumCalibrationValues(parseDocument(exampleInput)) == 142)
   }
 
   test("Part 1 input answer") {
-    assert(sumCalibrationValues(parseDocument(input)) == 55002)
+    assert(Part1.sumCalibrationValues(parseDocument(input)) == 55002)
+  }
+
+  test("Part 2 examples") {
+    assert(Part2.sumCalibrationValues(parseDocument(exampleInput2)) == 281)
+  }
+
+  test("Part 2 input answer") {
+    assert(Part2.sumCalibrationValues(parseDocument(input)) == 55093)
   }
 }
