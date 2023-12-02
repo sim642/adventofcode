56fada0908a37875289c0ef75050a8c56f0f724b
Simmo Saan <simmo.saan@gmail.com>
2023-12-02 09:23:27 +0200

    Reuse Pos3 in 2023 day 2

--- src/main/scala/eu/sim642/adventofcode2023/Day2.scala
+++ src/main/scala/eu/sim642/adventofcode2023/Day2.scala
@@ -1,19 +1,18 @@
 package eu.sim642.adventofcode2023
 
+import eu.sim642.adventofcodelib.box.Box3
+import eu.sim642.adventofcodelib.pos.Pos3
+
 object Day2 {
 
-  // TODO: reuse Pos3?
-  case class Cubes(red: Int = 0, green: Int = 0, blue: Int = 0) {
-    def +(that: Cubes): Cubes =
-      Cubes(red + that.red, green + that.green, blue + that.blue)
+  type Cubes = Pos3
 
-    def <=(that: Cubes): Boolean =
-      red <= that.red && green <= that.green && blue <= that.blue
+  object Cubes {
+    def apply(red: Int = 0, green: Int = 0, blue: Int = 0): Cubes = Pos3(red, green, blue)
+  }
 
-    def max(that: Cubes): Cubes =
-      Cubes(red max that.red, green max that.green, blue max that.blue)
-
-    def power: Int = red * green * blue
+  extension (cubes: Cubes) {
+    def power: Int = cubes.x * cubes.y * cubes.z
   }
 
   type Game = Set[Cubes]
