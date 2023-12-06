09f4b81e4ba59c6cbca0b1788ba946278f4abca2
Simmo Saan <simmo.saan@gmail.com>
2023-12-06 09:00:26 +0200

    Add quadratic solution to 2023 day 6

--- src/main/scala/eu/sim642/adventofcode2023/Day6.scala
+++ src/main/scala/eu/sim642/adventofcode2023/Day6.scala
@@ -3,9 +3,6 @@ package eu.sim642.adventofcode2023
 object Day6 {
 
   case class Race(time: Long, recordDistance: Long) {
-    def wins: Long = // TODO: optimize with math
-      (0L to time).count(t => t * (time - t) > recordDistance)
-
     def ++(that: Race): Race =
       Race(
         (time.toString + that.time.toString).toLong,
@@ -13,9 +10,48 @@ object Day6 {
       )
   }
 
-  def multiplyRaceWins(races: Seq[Race]): Long = races.map(_.wins).product
+  trait Solution {
+    def raceWins(race: Race): Long
 
-  def concatenatedRaceWin(races: Seq[Race]): Long = races.reduce(_ ++ _).wins
+    def multiplyRaceWins(races: Seq[Race]): Long = races.map(raceWins).product
+
+    def concatenatedRaceWin(races: Seq[Race]): Long = raceWins(races.reduce(_ ++ _))
+  }
+
+  /**
+   * Solution, which counts race wins by brute force.
+   */
+  object NaiveSolution extends Solution {
+    override def raceWins(race: Race): Long =
+      (0L to race.time).count(t => t * (race.time - t) > race.recordDistance)
+  }
+
+  extension (d: Double) {
+    def floorExclusive: Double = {
+      val dFloor = d.floor
+      if (d == dFloor) d - 1 else dFloor
+    }
+
+    def ceilExclusive: Double = {
+      val dCeil = d.ceil
+      if (d == dCeil) d + 1 else dCeil
+    }
+  }
+
+  /**
+   * Solution, which counts race wins by solving a quadratic inequality.
+   */
+  object QuadraticSolution extends Solution {
+    override def raceWins(race: Race): Long = {
+      // t * (time - t) > recordDistance
+      // -t^2 + time * t - recordDistance > 0
+      val d = math.sqrt(race.time * race.time - 4.0 * race.recordDistance)
+      // exclusive ceil and floor because inequality is strict
+      val minWinTime = ((race.time - d) / 2).ceilExclusive.toLong
+      val maxWinTime = ((race.time + d) / 2).floorExclusive.toLong
+      maxWinTime - minWinTime + 1
+    }
+  }
 
 
   def parseRaces(input: String): Seq[Race] = input.linesIterator.toSeq match {
@@ -28,6 +64,8 @@ object Day6 {
   lazy val input: String = io.Source.fromInputStream(getClass.getResourceAsStream("day6.txt")).mkString.trim
 
   def main(args: Array[String]): Unit = {
+    import QuadraticSolution._
+
     println(multiplyRaceWins(parseRaces(input)))
     println(concatenatedRaceWin(parseRaces(input)))
   }
--- src/test/scala/eu/sim642/adventofcode2023/Day6Test.scala
+++ src/test/scala/eu/sim642/adventofcode2023/Day6Test.scala
@@ -1,27 +1,47 @@
 package eu.sim642.adventofcode2023
 
-import Day6._
+import Day6.*
+import Day6Test.*
+import org.scalatest.Suites
 import org.scalatest.funsuite.AnyFunSuite
 
-class Day6Test extends AnyFunSuite {
+class Day6Test extends Suites(
+  new NaiveSolutionTest,
+  new QuadraticSolutionTest,
+)
+
+object Day6Test {
 
   private val exampleInput =
     """Time:      7  15   30
       |Distance:  9  40  200""".stripMargin
 
-  test("Part 1 examples") {
-    assert(multiplyRaceWins(parseRaces(exampleInput)) == 288)
+
+  abstract class SolutionTest(solution: Solution) extends AnyFunSuite {
+
+    test("Part 1 examples") {
+      val races = parseRaces(exampleInput)
+
+      assert(solution.raceWins(races(0)) == 4)
+      assert(solution.raceWins(races(1)) == 8)
+      assert(solution.raceWins(races(2)) == 9)
+
+      assert(solution.multiplyRaceWins(races) == 288)
+    }
+
+    test("Part 1 input answer") {
+      assert(solution.multiplyRaceWins(parseRaces(input)) == 2756160)
+    }
+
+    test("Part 2 examples") {
+      assert(solution.concatenatedRaceWin(parseRaces(exampleInput)) == 71503)
+    }
+
+    test("Part 2 input answer") {
+      assert(solution.concatenatedRaceWin(parseRaces(input)) == 34788142)
+    }
   }
 
-  test("Part 1 input answer") {
-    assert(multiplyRaceWins(parseRaces(input)) == 2756160)
-  }
-
-  test("Part 2 examples") {
-    assert(concatenatedRaceWin(parseRaces(exampleInput)) == 71503)
-  }
-
-  test("Part 2 input answer") {
-    assert(concatenatedRaceWin(parseRaces(input)) == 34788142)
-  }
+  class NaiveSolutionTest extends SolutionTest(NaiveSolution)
+  class QuadraticSolutionTest extends SolutionTest(QuadraticSolution)
 }
