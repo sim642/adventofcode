2f82448ccfc2221379eb6b6cb8abcba01f7a231b
Simmo Saan <simmo.saan@gmail.com>
2023-12-08 08:25:59 +0200

    Solve 2023 day 8 part 2

--- src/main/scala/eu/sim642/adventofcode2023/Day8.scala
+++ src/main/scala/eu/sim642/adventofcode2023/Day8.scala
@@ -1,6 +1,8 @@
 package eu.sim642.adventofcode2023
 
-import eu.sim642.adventofcodelib.IterableImplicits._
+import eu.sim642.adventofcodelib.IterableImplicits.*
+import eu.sim642.adventofcodelib.NumberTheory
+import eu.sim642.adventofcodelib.cycle.NaiveCycleFinder
 
 object Day8 {
 
@@ -17,6 +19,42 @@ object Day8 {
     }).indexOf("ZZZ")
   }
 
+  //def followInstructionsStepsGhost(input: Input): Int = {
+  //  val Input(instructions, network) = input
+  //  val startNodes = network.keys.filter(_.endsWith("A")).toSeq
+  //  instructions.cycle.scanLeft(startNodes)({ (nodes, instruction) =>
+  //    nodes.map({ node => (node, instruction) match {
+  //      case (node, 'L') => network(node)._1
+  //      case (node, 'R') => network(node)._2
+  //    }})
+  //  }).indexWhere(_.forall(_.endsWith("Z")))
+  //}
+
+  def followInstructionsStepsGhost(input: Input): Long = {
+    val Input(instructions, network) = input
+
+    def helper(startNode: Node): Int = {
+      //var nodes = instructions.zipWithIndex.cycle.scanLeft((instructions.size - 1, startNode))({
+      //  case ((i, node), ('L', j)) => j -> network(node)._1
+      //  case ((i, node), ('R', j)) => j -> network(node)._2
+      //})
+      //println(NaiveCycleFinder.find(nodes).get)
+      val nodes = instructions.cycle.scanLeft(startNode)({
+        case (node, 'L') => network(node)._1
+        case (node, 'R') => network(node)._2
+      })
+      val x = nodes.indexWhere(_.endsWith("Z"))
+      println(s"$startNode -> $x")
+      x
+      //???
+    }
+
+    val startNodes = network.keys.filter(_.endsWith("A")).toSeq
+    println(startNodes)
+    val steps = startNodes.map(helper)
+    NumberTheory.lcm(steps.map(_.toLong))
+  }
+
 
   def parseNode(s: String): (Node, (Node, Node)) = s match {
     case s"$node = ($left, $right)" => node -> (left, right)
@@ -31,5 +69,8 @@ object Day8 {
 
   def main(args: Array[String]): Unit = {
     println(followInstructionsSteps(parseInput(input)))
+    println(followInstructionsStepsGhost(parseInput(input)))
+
+    // part 2: 1457348313 - too low (Int overflowed in lcm)
   }
 }
--- src/test/scala/eu/sim642/adventofcode2023/Day8Test.scala
+++ src/test/scala/eu/sim642/adventofcode2023/Day8Test.scala
@@ -23,6 +23,18 @@ class Day8Test extends AnyFunSuite {
       |BBB = (AAA, ZZZ)
       |ZZZ = (ZZZ, ZZZ)""".stripMargin
 
+  private val exampleInput3 =
+    """LR
+      |
+      |11A = (11B, XXX)
+      |11B = (XXX, 11Z)
+      |11Z = (11B, XXX)
+      |22A = (22B, XXX)
+      |22B = (22C, 22C)
+      |22C = (22Z, 22Z)
+      |22Z = (22B, 22B)
+      |XXX = (XXX, XXX)""".stripMargin
+
   test("Part 1 examples") {
     assert(followInstructionsSteps(parseInput(exampleInput)) == 2)
     assert(followInstructionsSteps(parseInput(exampleInput2)) == 6)
@@ -30,5 +42,13 @@ class Day8Test extends AnyFunSuite {
 
   test("Part 1 input answer") {
     assert(followInstructionsSteps(parseInput(input)) == 21389)
+  }
+
+  test("Part 2 examples") {
+    assert(followInstructionsStepsGhost(parseInput(exampleInput3)) == 6)
+  }
+
+  test("Part 2 input answer") {
+    assert(followInstructionsStepsGhost(parseInput(input)) == 21083806112641L)
   }
 }
