8914ef3caa2c9d6be252123d0547bca93183ae9c
Simmo Saan <simmo.saan@gmail.com>
2023-12-10 10:05:27 +0200

    Solve 2023 day 10 part 2

--- src/main/scala/eu/sim642/adventofcode2023/Day10.scala
+++ src/main/scala/eu/sim642/adventofcode2023/Day10.scala
@@ -1,9 +1,11 @@
 package eu.sim642.adventofcode2023
 
 import eu.sim642.adventofcodelib.Grid
-import eu.sim642.adventofcodelib.graph.{BFS, GraphTraversal, UnitNeighbors}
+import eu.sim642.adventofcodelib.graph.{BFS, Distances, GraphTraversal, UnitNeighbors}
 import eu.sim642.adventofcodelib.pos.Pos
 import eu.sim642.adventofcodelib.GridImplicits.*
+
+import scala.annotation.tailrec
 
 object Day10 {
 
@@ -17,8 +19,7 @@ object Day10 {
     'S' -> Pos.axisOffsets.toSet,
   )
 
-  def farthestDistance(grid: Grid[Char]): Int = {
-
+  def findLoop(grid: Grid[Char]): Distances[Pos] = {
     val graphTraversal = new GraphTraversal[Pos] with UnitNeighbors[Pos] {
       override val startNode: Pos = grid.posOf('S')
 
@@ -33,7 +34,106 @@ object Day10 {
       }
     }
 
-    BFS.traverse(graphTraversal).distances.values.max
+    BFS.traverse(graphTraversal)
+  }
+
+  def farthestDistance(grid: Grid[Char]): Int = findLoop(grid).distances.values.max
+
+  def enclosedTiles(grid: Grid[Char]): Int = {
+    val loop = findLoop(grid).nodes
+
+    def flood(startPos: Pos): collection.Set[Pos] = {
+
+      val graphTraversal = new GraphTraversal[Pos] with UnitNeighbors[Pos] {
+        override val startNode: Pos = startPos
+
+        override def unitNeighbors(pos: Pos): IterableOnce[Pos] = {
+          for {
+            offset <- Pos.axisOffsets
+            newPos = pos + offset
+            if grid.containsPos(newPos)
+            if !loop.contains(newPos)
+          } yield newPos
+        }
+      }
+
+      BFS.traverse(graphTraversal).nodes
+    }
+
+    //val outside = flood(Pos.zero)  // assuming outside
+    //
+    //val inside = for {
+    //  outPos <- outside
+    //  offset <- Pos.axisOffsets
+    //  loopPos = outPos + offset
+    //  if grid.containsPos(loopPos)
+    //  if loop.contains(loopPos)
+    //  if grid(loopPos) == '|' || grid(loopPos) == '-'
+    //  inPos = loopPos + offset
+    //  if grid.containsPos(inPos)
+    //  if !loop.contains(inPos) && !outside.contains(inPos)
+    //} yield inPos
+    //
+    //val inside2 = inside.flatMap(flood)
+
+    //println(inside)
+
+    //grid.sizeGrid - loop.size - outside.size
+    //inside2.size
+
+    val startAlternative = {
+      val pos = grid.posOf('S')
+      val newOffsets = for {
+        offset <- pipeDirections(grid(pos))
+        newPos = pos + offset
+        if grid.containsPos(newPos)
+        if grid(newPos) != '.'
+        if pipeDirections(grid(newPos)).contains(-offset)
+      } yield offset
+      pipeDirections.find(_._2 == newOffsets).get._1
+    }
+
+    def isInside(pos: Pos): Boolean = {
+      //println(pos)
+
+      @tailrec
+      def helper(pos: Pos, count: Int, enterEdge: Char): Boolean = {
+        val newPos = pos + Pos(1, 0)
+        //println(s"  $newPos")
+        if (grid.containsPos(newPos)) {
+          val c = if grid(newPos) == 'S' then startAlternative else if loop.contains(newPos) then grid(newPos) else '.'
+          (c, enterEdge) match {
+            case ('|', _) => helper(newPos, count + 1, enterEdge = '?')
+            case ('.', _) => helper(newPos, count, enterEdge = '?')
+
+            case ('-', _) => helper(newPos, count, enterEdge = enterEdge)
+
+            case ('F', _) => helper(newPos, count, enterEdge = 'F')
+            case ('7', 'F') => helper(newPos, count, enterEdge = '?')
+            case ('J', 'F') => helper(newPos, count + 1, enterEdge = '?')
+
+            case ('L', _) => helper(newPos, count, enterEdge = 'L')
+            case ('7', 'L') => helper(newPos, count + 1, enterEdge = '?')
+            case ('J', 'L') => helper(newPos, count, enterEdge = '?')
+
+            //case (_, _) => helper(newPos, count, enterEdge = '?')
+            case (a, b) => throw IllegalArgumentException(s"$a $b")
+          }
+        }
+        else {
+          count % 2 == 1
+        }
+      }
+
+      helper(pos, 0, '?')
+    }
+
+    (for {
+      (row, y) <- grid.view.zipWithIndex
+      (cell, x) <- row.view.zipWithIndex
+      if !loop.contains(Pos(x, y))
+      if isInside(Pos(x, y))
+    } yield 1).sum
   }
 
   def parseGrid(input: String): Grid[Char] = input.linesIterator.map(_.toVector).toVector
@@ -42,5 +142,6 @@ object Day10 {
 
   def main(args: Array[String]): Unit = {
     println(farthestDistance(parseGrid(input)))
+    println(enclosedTiles(parseGrid(input)))
   }
 }
--- src/test/scala/eu/sim642/adventofcode2023/Day10Test.scala
+++ src/test/scala/eu/sim642/adventofcode2023/Day10Test.scala
@@ -33,6 +33,52 @@ class Day10Test extends AnyFunSuite {
       ||F--J
       |LJ.LJ""".stripMargin
 
+  private val exampleInput5 =
+    """...........
+      |.S-------7.
+      |.|F-----7|.
+      |.||.....||.
+      |.||.....||.
+      |.|L-7.F-J|.
+      |.|..|.|..|.
+      |.L--J.L--J.
+      |...........""".stripMargin
+
+  private val exampleInput6 =
+    """..........
+      |.S------7.
+      |.|F----7|.
+      |.||....||.
+      |.||....||.
+      |.|L-7F-J|.
+      |.|..||..|.
+      |.L--JL--J.
+      |..........""".stripMargin
+
+  private val exampleInput7 =
+    """.F----7F7F7F7F-7....
+      |.|F--7||||||||FJ....
+      |.||.FJ||||||||L7....
+      |FJL7L7LJLJ||LJ.L-7..
+      |L--J.L7...LJS7F-7L7.
+      |....F-J..F7FJ|L7L7L7
+      |....L7.F7||L7|.L7L7|
+      |.....|FJLJ|FJ|F7|.LJ
+      |....FJL-7.||.||||...
+      |....L---J.LJ.LJLJ...""".stripMargin
+
+  private val exampleInput8 =
+    """FF7FSF7F7F7F7F7F---7
+      |L|LJ||||||||||||F--J
+      |FL-7LJLJ||||||LJL-77
+      |F--JF--7||LJLJ7F7FJ-
+      |L---JF-JLJ.||-FJLJJ7
+      ||F|F-JF---7F7-L7L|7|
+      ||FFJF7L7F-JF7|JL---7
+      |7-L-JL7||F7|L7F-7F7|
+      |L.L7LFJ|||||FJL7||LJ
+      |L7JLJL-JLJLJL--JLJ.L""".stripMargin
+
   test("Part 1 examples") {
     assert(farthestDistance(parseGrid(exampleInput)) == 4)
     assert(farthestDistance(parseGrid(exampleInput2)) == 4)
@@ -42,5 +88,16 @@ class Day10Test extends AnyFunSuite {
 
   test("Part 1 input answer") {
     assert(farthestDistance(parseGrid(input)) == 7063)
+  }
+
+  test("Part 2 examples") {
+    assert(enclosedTiles(parseGrid(exampleInput5)) == 4)
+    assert(enclosedTiles(parseGrid(exampleInput6)) == 4)
+    assert(enclosedTiles(parseGrid(exampleInput7)) == 8)
+    assert(enclosedTiles(parseGrid(exampleInput8)) == 10)
+  }
+
+  test("Part 2 input answer") {
+    assert(enclosedTiles(parseGrid(input)) == 589)
   }
 }
