6399e0420ba398c400cdce525f42dbbb4ee5659e
Simmo Saan <simmo.saan@gmail.com>
2023-12-13 08:12:57 +0200

    Refactor 2023 day 13

--- src/main/scala/eu/sim642/adventofcode2023/Day13.scala
+++ src/main/scala/eu/sim642/adventofcode2023/Day13.scala
@@ -5,60 +5,36 @@ import eu.sim642.adventofcodelib.Grid
 object Day13 {
 
   trait Part {
-    def summarizeMirror(grid: Grid[Boolean]): Int
-
-    def summarizeMirrors(grids: Seq[Grid[Boolean]]): Int = grids.map(summarizeMirror).sum
-  }
-
-  object Part1 extends Part {
+    val mismatches: Int
 
     def findMirrorHorizontal(grid: Grid[Boolean]): Option[Int] = {
-      (1 to (grid.size - 1))
-        .find(mirrorY => {
-          (1 to (mirrorY min (grid.size - mirrorY)))
-            .forall(d =>
-              grid(mirrorY - d) == grid(mirrorY + d - 1)
-            )
-        })
-    }
-
-    var i = -1
-
-    def summarizeMirror(grid: Grid[Boolean]): Int = {
-      i += 1
-      findMirrorHorizontal(grid) match
-        case Some(y) => 100 * y
-        case None =>
-          findMirrorHorizontal(grid.transpose) match
-            case Some(x) => x
-            case None => throw new IllegalArgumentException(i.toString)
-    }
-  }
-
-  object Part2 extends Part {
-    def findMirrorHorizontal(grid: Grid[Boolean]): Option[Int] = {
-      (1 to (grid.size - 1))
+      (1 until grid.size)
         .find(mirrorY => {
           (1 to (mirrorY min (grid.size - mirrorY)))
             .map(d =>
               (grid(mirrorY - d) lazyZip grid(mirrorY + d - 1))
                 .count(_ != _)
             )
-            .sum == 1
+            .sum == mismatches
         })
     }
 
-    var i = -1
-
     def summarizeMirror(grid: Grid[Boolean]): Int = {
-      i += 1
-      findMirrorHorizontal(grid) match
+      findMirrorHorizontal(grid) match {
         case Some(y) => 100 * y
-        case None =>
-          findMirrorHorizontal(grid.transpose) match
-            case Some(x) => x
-            case None => throw new IllegalArgumentException(i.toString)
+        case None => findMirrorHorizontal(grid.transpose).get
+      }
     }
+
+    def summarizeMirrors(grids: Seq[Grid[Boolean]]): Int = grids.map(summarizeMirror).sum
+  }
+
+  object Part1 extends Part {
+    override val mismatches: Int = 0
+  }
+
+  object Part2 extends Part {
+    override val mismatches: Int = 1
   }
 
   def parseGrid(s: String): Grid[Boolean] = s.linesIterator.map(_.map(_ == '#').toVector).toVector
