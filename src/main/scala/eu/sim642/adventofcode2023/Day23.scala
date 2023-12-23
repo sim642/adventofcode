4d77f31b7af725fc9535f50bfefdf43071568c73
Simmo Saan <simmo.saan@gmail.com>
2023-12-23 17:48:17 +0200

    Refactor parts in 2023 day 23

--- src/main/scala/eu/sim642/adventofcode2023/Day23.scala
+++ src/main/scala/eu/sim642/adventofcode2023/Day23.scala
@@ -1,16 +1,16 @@
 package eu.sim642.adventofcode2023
 
 import eu.sim642.adventofcodelib.Grid
-import eu.sim642.adventofcodelib.graph.{BFS, Dijkstra, GraphTraversal, UnitNeighbors}
-import eu.sim642.adventofcodelib.pos.Pos
 import eu.sim642.adventofcodelib.GridImplicits.*
 import eu.sim642.adventofcodelib.box.Box
+import eu.sim642.adventofcodelib.graph.{BFS, GraphTraversal, UnitNeighbors}
+import eu.sim642.adventofcodelib.pos.Pos
 
 import scala.annotation.tailrec
 
 object Day23 {
 
-  case class HikePos(pos: Pos, path: List[Pos], pathSet: Set[Pos])
+  case class HikePos(pos: Pos, pathSet: Set[Pos])
 
   private val slopeOffsets = Map(
     '^' -> Pos(0, -1),
@@ -19,107 +19,99 @@ object Day23 {
     '<' -> Pos(-1, 0),
   )
 
-  def longestHike(grid: Grid[Char], slopes: Boolean = true): Int = {
+  trait Part {
+    protected val slopes: Boolean
 
-    val startPos = Pos(1, 0) // TODO: don't hardcode
-    val targetPos = Pos(grid(0).size - 2, grid.size - 1)
+    def longestHike(grid: Grid[Char]): Int = {
+      val startPos = Pos(grid.head.indexOf('.'), 0)
+      val targetPos = Pos(grid.last.indexOf('.'), grid.size - 1)
 
-    val branchPoss = (for {
-      pos <- Box(Pos(1, 1), Pos(grid(0).size - 2, grid.size - 2)).iterator
-      if grid(pos) != '#'
-      if Pos.axisOffsets.count(offset => grid(pos + offset) != '#') > 2
-    } yield pos).toSet
+      val branchPoss = (for {
+        pos <- Box(Pos(1, 1), Pos(grid(0).size - 2, grid.size - 2)).iterator
+        if grid(pos) != '#'
+        if Pos.axisOffsets.count(offset => grid(pos + offset) != '#') > 2
+      } yield pos).toSet
 
-    val keyPoss = branchPoss + startPos + targetPos
-    println(keyPoss)
+      val keyPoss = branchPoss + startPos + targetPos
 
-    val keyNeighbors: Map[Pos, Map[Pos, Int]] = {
-      keyPoss.view.map({ fromPos =>
+      val keyNeighbors: Map[Pos, Map[Pos, Int]] = {
+        keyPoss.view.map({ fromPos =>
+          val graphTraversal = new GraphTraversal[Pos] with UnitNeighbors[Pos] {
+            override val startNode: Pos = fromPos
 
-        val graphTraversal = new GraphTraversal[Pos] with UnitNeighbors[Pos] {
-          override val startNode: Pos = fromPos
-
-          override def unitNeighbors(pos: Pos): IterableOnce[Pos] = {
-            if (pos != fromPos && keyPoss(pos))
-              Iterator.empty
-            else {
-              for {
-                offset <- if (!slopes || grid(pos) == '.') Pos.axisOffsets else Seq(slopeOffsets(grid(pos)))
-                newPos = pos + offset
-                if grid.containsPos(newPos)
-                if grid(newPos) != '#'
-              } yield newPos
+            override def unitNeighbors(pos: Pos): IterableOnce[Pos] = {
+              if (pos != fromPos && keyPoss(pos))
+                Iterator.empty
+              else {
+                for {
+                  offset <- if (!slopes || grid(pos) == '.') Pos.axisOffsets else Seq(slopeOffsets(grid(pos)))
+                  newPos = pos + offset
+                  if grid.containsPos(newPos)
+                  if grid(newPos) != '#'
+                } yield newPos
+              }
             }
           }
-        }
 
-        val distances = BFS.traverse(graphTraversal).distances.toMap
-        fromPos -> distances.filter(p => p._1 != fromPos && keyPoss(p._1))
-      }).toMap
-    }
-
-    val graphTraversal = new GraphTraversal[HikePos] {
-      //override val startNode: HikePos = HikePos(startPos, List(startPos), Set(startPos))
-      override val startNode: HikePos = HikePos(startPos, List(), Set(startPos))
-
-      override def neighbors(hikePos: HikePos): IterableOnce[(HikePos, Int)] = {
-        if (hikePos.pos != targetPos) {
-          val HikePos(pos, path, pathSet) = hikePos
-          for {
-            (newPos, dist) <- keyNeighbors(pos)
-            //() = println((pos, newPos, dist))
-            //() = assert(slopes || keyNeighbors(newPos)(pos) == dist)
-            if !pathSet.contains(newPos)
-          } yield HikePos(newPos, path, pathSet + newPos) -> dist
-        }
-        else
-          Iterator.empty
+          val distances = BFS.traverse(graphTraversal).distances.toMap
+          fromPos -> distances.filter(p => p._1 != fromPos && keyPoss(p._1))
+        }).toMap
       }
-    }
 
+      val graphTraversal = new GraphTraversal[HikePos] {
+        override val startNode: HikePos = HikePos(startPos, Set(startPos))
 
-    //val asd = Dijkstra.traverse(graphTraversal).distances
-    //  .filter(_._1.pos == targetPos)
+        override def neighbors(hikePos: HikePos): IterableOnce[(HikePos, Int)] = {
+          if (hikePos.pos != targetPos) {
+            val HikePos(pos, pathSet) = hikePos
+            for {
+              (newPos, dist) <- keyNeighbors(pos)
+              if !pathSet.contains(newPos)
+            } yield HikePos(newPos, pathSet + newPos) -> dist
+          }
+          else
+            Iterator.empty
+        }
+      }
 
-    @tailrec
-    def helper(todo: Set[HikePos], visited: Map[HikePos, Int]): Map[HikePos, Int] = {
-      if (todo.isEmpty)
-        visited.filter(_._1.pathSet.contains(targetPos))
-      else {
-        val hikePos = todo.head
-        val newTodo = todo - hikePos
-        val visited0 = visited
-        //if (hikePos.pathSet == Set(Pos(21,11), Pos(1,0), Pos(19,19), Pos(11,3), Pos(13,13), Pos(3,5), Pos(5,13), Pos(13,19)))
-        val oldDist = visited0(hikePos)
-        //if (hikePos.pos == Pos(19, 19))
-        //println(s"from $hikePos ($oldDist)")
-        val (newTodo2, newVisited) = graphTraversal.neighbors(hikePos).iterator.foldLeft((newTodo, visited))({ case ((todo, visited), (newHikePos2, dist)) =>
-          //println(s"  to $newHikePos2 -> $dist")
-          val newDist = oldDist + dist
-          if (visited0.contains(newHikePos2)) {
-            if (visited0(newHikePos2) < newDist) {
+      @tailrec
+      def helper(todo: Set[HikePos], visited: Map[HikePos, Int]): Map[HikePos, Int] = {
+        if (todo.isEmpty)
+          visited.filter(_._1.pathSet.contains(targetPos))
+        else {
+          val hikePos = todo.head
+          val newTodo = todo - hikePos
+          val visited0 = visited
+          val oldDist = visited0(hikePos)
+          val (newTodo2, newVisited) = graphTraversal.neighbors(hikePos).iterator.foldLeft((newTodo, visited))({ case ((todo, visited), (newHikePos2, dist)) =>
+            val newDist = oldDist + dist
+            if (visited0.contains(newHikePos2)) {
+              if (visited0(newHikePos2) < newDist) {
+                (todo + newHikePos2, visited + (newHikePos2 -> newDist))
+              }
+              else {
+                (todo, visited)
+              }
+            }
+            else {
               (todo + newHikePos2, visited + (newHikePos2 -> newDist))
             }
-            else {
-              (todo, visited)
-            }
-          }
-          else {
-            (todo + newHikePos2, visited + (newHikePos2 -> newDist))
-          }
-        })
-        helper(newTodo2, newVisited)
+          })
+          helper(newTodo2, newVisited)
+        }
       }
+
+      val distances = helper(Set(graphTraversal.startNode), Map(graphTraversal.startNode -> 0))
+      distances.values.max
     }
+  }
 
-    val asd = helper(Set(graphTraversal.startNode), Map(graphTraversal.startNode -> 0))
+  object Part1 extends Part {
+    override protected val slopes: Boolean = true
+  }
 
-    //asd.values.toSet.foreach(println)
-    asd.foreach(println)
-    println(asd.maxBy(_._2)._1)
-    asd
-      .values
-      .max
+  object Part2 extends Part {
+    override protected val slopes: Boolean = false
   }
 
   def parseGrid(input: String): Grid[Char] = input.linesIterator.map(_.toVector).toVector
@@ -127,7 +119,7 @@ object Day23 {
   lazy val input: String = scala.io.Source.fromInputStream(getClass.getResourceAsStream("day23.txt")).mkString.trim
 
   def main(args: Array[String]): Unit = {
-    //println(longestHike(parseGrid(input)))
-    println(longestHike(parseGrid(input), slopes = false))
+    println(Part1.longestHike(parseGrid(input)))
+    println(Part2.longestHike(parseGrid(input)))
   }
 }
--- src/test/scala/eu/sim642/adventofcode2023/Day23Test.scala
+++ src/test/scala/eu/sim642/adventofcode2023/Day23Test.scala
@@ -31,18 +31,18 @@ class Day23Test extends AnyFunSuite {
       |#####################.#""".stripMargin
 
   test("Part 1 examples") {
-    assert(longestHike(parseGrid(exampleInput)) == 94)
+    assert(Part1.longestHike(parseGrid(exampleInput)) == 94)
   }
 
-  test("Part 1 input answer") { // TODO: optimize
-    assert(longestHike(parseGrid(input)) == 2414)
+  test("Part 1 input answer") {
+    assert(Part1.longestHike(parseGrid(input)) == 2414)
   }
 
   test("Part 2 examples") {
-    assert(longestHike(parseGrid(exampleInput), slopes = false) == 154)
+    assert(Part2.longestHike(parseGrid(exampleInput)) == 154)
   }
 
   ignore("Part 2 input answer") { // TODO: optimize
-    assert(longestHike(parseGrid(input), slopes = false) == 6598)
+    assert(Part2.longestHike(parseGrid(input)) == 6598)
   }
 }
