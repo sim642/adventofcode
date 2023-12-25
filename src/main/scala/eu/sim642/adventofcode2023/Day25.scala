0d765f5f1fe3af7e68d69cbde58806a841ca182a
Simmo Saan <simmo.saan@gmail.com>
2023-12-25 09:44:35 +0200

    Solve 2023 day 25 part 1 by brute force

---
+++ src/main/scala/eu/sim642/adventofcode2023/Day25.scala
@@ -0,0 +1,44 @@
+package eu.sim642.adventofcode2023
+
+import eu.sim642.adventofcodelib.graph.{BFS, GraphComponents}
+
+object Day25 {
+
+  type Edge = (String, String)
+
+  def connectedComponents(edges: Set[Edge]): collection.Set[collection.Set[String]] = {
+
+    val forwardEdges = edges.groupMap(_._1)(_._2).withDefaultValue(Set.empty)
+    val backwardEdges = edges.groupMap(_._2)(_._1).withDefaultValue(Set.empty)
+
+    val graphComponents = new GraphComponents[String] {
+      override def nodes: IterableOnce[String] = forwardEdges.keySet ++ backwardEdges.keySet
+
+      override def unitNeighbors(node: String): IterableOnce[String] = forwardEdges(node) ++ backwardEdges(node)
+    }
+
+    BFS.components(graphComponents)
+  }
+
+  def disconnectComponents(edges: Set[Edge]): Int = {
+    val foo = edges.toSeq.combinations(3)
+      .map(edges -- _)
+      .map(connectedComponents)
+      .find(_.size == 2)
+      .get
+    foo.toSeq.map(_.size).product
+  }
+
+  def parseEdges(input: String): Set[Edge] = {
+    (for {
+      case s"$from: $tos" <- input.linesIterator
+      to <- tos.split(' ')
+    } yield (from, to)).toSet
+  }
+
+  lazy val input: String = scala.io.Source.fromInputStream(getClass.getResourceAsStream("day25.txt")).mkString.trim
+
+  def main(args: Array[String]): Unit = {
+    println(disconnectComponents(parseEdges(input)))
+  }
+}
---
+++ src/test/scala/eu/sim642/adventofcode2023/Day25Test.scala
@@ -0,0 +1,30 @@
+package eu.sim642.adventofcode2023
+
+import Day25._
+import org.scalatest.funsuite.AnyFunSuite
+
+class Day25Test extends AnyFunSuite {
+
+  private val exampleInput =
+    """jqt: rhn xhk nvd
+      |rsh: frs pzl lsr
+      |xhk: hfx
+      |cmg: qnr nvd lhk bvb
+      |rhn: xhk bvb hfx
+      |bvb: xhk hfx
+      |pzl: lsr hfx nvd
+      |qnr: nvd
+      |ntq: jqt hfx bvb xhk
+      |nvd: lhk
+      |lsr: lhk
+      |rzs: qnr cmg lsr rsh
+      |frs: qnr lhk lsr""".stripMargin
+
+  test("Part 1 examples") {
+    assert(disconnectComponents(parseEdges(exampleInput)) == 54)
+  }
+
+  test("Part 1 input answer") {
+    //assert(disconnectComponents(parseEdges(input)) == 54)
+  }
+}
