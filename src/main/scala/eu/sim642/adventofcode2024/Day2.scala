6d6791ba8130a13cdff30ea6589f514eb08096f7
Simmo Saan <simmo.saan@gmail.com>
2024-12-02 07:23:43 +0200

    Refactor parts in 2024 day 2

--- src/main/scala/eu/sim642/adventofcode2024/Day2.scala
+++ src/main/scala/eu/sim642/adventofcode2024/Day2.scala
@@ -6,28 +6,29 @@ object Day2 {
 
   type Report = Seq[Int]
 
-  def isSafe(report: Report): Boolean = {
-    val sorted = report.sorted
-    val increasing = report == sorted
-    val decreasing = report.reverse == sorted
-    val monotonic = increasing || decreasing
-    val safeDifferences = report.iterator.zipWithTail.forall({ case (a, b) =>
-      val diff = (a - b).abs
-      1 <= diff && diff <= 3
-    })
-    monotonic && safeDifferences
-  }
+  trait Part {
+    def isSafe(report: Report): Boolean
 
-  object Part1 {
     def countSafe(reports: Seq[Report]): Int = reports.count(isSafe)
   }
 
-  object Part2 {
-    private def isSafe2(report: Report): Boolean = {
-      isSafe(report) || report.indices.exists(i => isSafe(report.slice(0, i) ++ report.slice(i + 1, report.size))) // TODO: better seq removal? optimize?
+  object Part1 extends Part {
+    override def isSafe(report: Report): Boolean = {
+      val sorted = report.sorted
+      val increasing = report == sorted // TODO: add isSorted to library
+      val decreasing = report.reverse == sorted // TODO: add isSorted to library (with Ordering)
+      val monotonic = increasing || decreasing
+      val safeDifferences = report.iterator.zipWithTail.forall({ case (a, b) =>
+        (1 to 3).contains((a - b).abs)
+      })
+      monotonic && safeDifferences
     }
+  }
 
-    def countSafe(reports: Seq[Report]): Int = reports.count(isSafe2)
+  object Part2 extends Part {
+    override def isSafe(report: Report): Boolean = {
+      Part1.isSafe(report) || report.indices.exists(i => Part1.isSafe(report.slice(0, i) ++ report.slice(i + 1, report.size))) // TODO: better seq removal? optimize?
+    }
   }
 
   def parseReports(input: String): Seq[Report] = input.linesIterator.map(_.split(" ").map(_.toInt).toSeq).toSeq
