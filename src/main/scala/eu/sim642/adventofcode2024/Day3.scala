008a769274f6daa455446db8aab6e24c1e9d718c
Simmo Saan <simmo.saan@gmail.com>
2024-12-03 09:20:07 +0200

    Solve 2024 day 3 part 2

--- src/main/scala/eu/sim642/adventofcode2024/Day3.scala
+++ src/main/scala/eu/sim642/adventofcode2024/Day3.scala
@@ -10,9 +10,25 @@ object Day3 {
       .sum
   }
 
+  private val mulRegex2 = """mul\((\d{1,3}),(\d{1,3})\)|do(n't)?\(\)""".r
+
+  def sumUncorruptedMuls2(s: String): Int = {
+    mulRegex2.findAllMatchIn(s)
+      .foldLeft((true, 0))({ case ((enabled, sum), m) =>
+        (enabled, m.matched) match {
+          case (_, "do()") => (true, sum)
+          case (_, "don't()") => (false, sum)
+          case (true, _) => (enabled, sum + m.group(1).toInt * m.group(2).toInt)
+          case (false, _) => (enabled, sum)
+        }
+      })
+      ._2
+  }
+
   lazy val input: String = scala.io.Source.fromInputStream(getClass.getResourceAsStream("day3.txt")).mkString.trim
 
   def main(args: Array[String]): Unit = {
     println(sumUncorruptedMuls(input))
+    println(sumUncorruptedMuls2(input))
   }
 }
--- src/test/scala/eu/sim642/adventofcode2024/Day3Test.scala
+++ src/test/scala/eu/sim642/adventofcode2024/Day3Test.scala
@@ -6,6 +6,7 @@ import org.scalatest.funsuite.AnyFunSuite
 class Day3Test extends AnyFunSuite {
 
   val exampleInput = """xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"""
+  val exampleInput2 = """xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"""
 
   test("Part 1 examples") {
     assert(sumUncorruptedMuls(exampleInput) == 161)
@@ -13,5 +14,13 @@ class Day3Test extends AnyFunSuite {
 
   test("Part 1 input answer") {
     assert(sumUncorruptedMuls(input) == 182780583)
+  }
+
+  test("Part 2 examples") {
+    assert(sumUncorruptedMuls2(exampleInput2) == 48)
+  }
+
+  test("Part 2 input answer") {
+    assert(sumUncorruptedMuls2(input) == 90772405)
   }
 }
