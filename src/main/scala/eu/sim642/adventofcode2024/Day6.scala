610532a9dea2235268c04f4a886c0cb025dfc0ea
Simmo Saan <simmo.saan@gmail.com>
2024-12-06 09:01:35 +0200

    Solve 2024 day 6 part 1

---
+++ src/main/scala/eu/sim642/adventofcode2024/Day6.scala
@@ -0,0 +1,53 @@
+package eu.sim642.adventofcode2024
+
+import eu.sim642.adventofcodelib.Grid
+import eu.sim642.adventofcodelib.pos.Pos
+import eu.sim642.adventofcodelib.GridImplicits._
+import eu.sim642.adventofcode2018.Day13.DirectionPos
+
+object Day6 {
+
+  case class Guard(pos: Pos, direction: Pos)
+
+  case class Input(grid: Grid[Boolean], guard: Guard)
+
+  def stepGuard(grid: Grid[Boolean])(guard: Guard): Guard = {
+    val newPos = guard.pos + guard.direction
+    if (grid.containsPos(newPos) && grid(newPos))
+      guard.copy(direction = guard.direction.right)
+    else
+      guard.copy(pos = newPos)
+  }
+
+  def iterateGuard(input: Input): Iterator[Guard] =
+    Iterator.iterate(input.guard)(stepGuard(input.grid))
+
+  def countGuardPoss(input: Input): Int = {
+    iterateGuard(input)
+      .takeWhile(guard => input.grid.containsPos(guard.pos))
+      .map(_.pos)
+      .toSet
+      .size
+  }
+
+  def parseGrid(input: String): Grid[Boolean] = input.linesIterator.map(_.toVector).toVector.mapGrid({
+    case '#' => true
+    case '.' => false
+  })
+
+  def parseInput(input: String): Input = {
+    val rawGrid = input.linesIterator.map(_.toVector).toVector
+    val guard = Guard(rawGrid.posOf('^'), Pos(0, -1))
+    val grid = rawGrid.mapGrid({
+      case '#' => true
+      case _ => false
+    })
+    Input(grid, guard)
+  }
+
+  lazy val input: String = scala.io.Source.fromInputStream(getClass.getResourceAsStream("day6.txt")).mkString.trim
+
+  def main(args: Array[String]): Unit = {
+    println(countGuardPoss(parseInput(input)))
+  }
+}
---
+++ src/test/scala/eu/sim642/adventofcode2024/Day6Test.scala
@@ -0,0 +1,27 @@
+package eu.sim642.adventofcode2024
+
+import Day6._
+import org.scalatest.funsuite.AnyFunSuite
+
+class Day6Test extends AnyFunSuite {
+
+  val exampleInput =
+    """....#.....
+      |.........#
+      |..........
+      |..#.......
+      |.......#..
+      |..........
+      |.#..^.....
+      |........#.
+      |#.........
+      |......#...""".stripMargin
+
+  test("Part 1 examples") {
+    assert(countGuardPoss(parseInput(exampleInput)) == 41)
+  }
+
+  test("Part 1 input answer") {
+    assert(countGuardPoss(parseInput(input)) == 4752)
+  }
+}
