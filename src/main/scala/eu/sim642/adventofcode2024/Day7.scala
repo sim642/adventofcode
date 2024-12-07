bd44e548dc6e687ec305835373d3825d69e2c0ff
Simmo Saan <simmo.saan@gmail.com>
2024-12-07 08:37:30 +0200

    Solve 2024 day 7 part 2

--- src/main/scala/eu/sim642/adventofcode2024/Day7.scala
+++ src/main/scala/eu/sim642/adventofcode2024/Day7.scala
@@ -4,23 +4,56 @@ object Day7 {
 
   case class Equation(testValue: Long, numbers: Seq[Int])
 
-  def isSolvable(equation: Equation): Boolean = {
+  trait Part {
+    def isSolvable(equation: Equation): Boolean
 
-    def helper(testValue: Long, numbers: List[Int]): Boolean = numbers match {
-      case Nil => ???
-      case List(number) => number == testValue
-      case number :: numbers =>
-        testValue % number == 0 && helper(testValue / number, numbers) || helper(testValue - number, numbers)
+    def totalCalibrationResult(equations: Seq[Equation]): Long = {
+      equations
+        .filter(isSolvable)
+        .map(_.testValue)
+        .sum
     }
-
-    helper(equation.testValue, equation.numbers.view.reverse.toList)
   }
 
-  def totalCalibrationResult(equations: Seq[Equation]): Long = {
-    equations
-      .filter(isSolvable)
-      .map(_.testValue)
-      .sum
+  object Part1 extends Part {
+    override def isSolvable(equation: Equation): Boolean = {
+
+      def helper(testValue: Long, numbers: List[Int]): Boolean = numbers match {
+        case Nil => ???
+        case List(number) => number == testValue
+        case number :: numbers =>
+          testValue % number == 0 && helper(testValue / number, numbers) || helper(testValue - number, numbers)
+      }
+
+      helper(equation.testValue, equation.numbers.view.reverse.toList)
+    }
+  }
+
+  object Part2 extends Part {
+    override def isSolvable(equation: Equation): Boolean = {
+
+      def helper(testValue: Long, numbers: List[Int]): Boolean = numbers match {
+        case Nil => ???
+        case List(number) => number == testValue
+        case number :: newNumbers =>
+          testValue % number == 0 && helper(testValue / number, newNumbers) || helper(testValue - number, newNumbers) || concatHelper(testValue, numbers)
+      }
+
+      def concatHelper(testValue: Long, numbers: List[Int]): Boolean = numbers match {
+        case number :: newNumbers =>
+          val testValueStr = testValue.toString
+          val numberStr = number.toString
+          if (testValueStr.length > numberStr.length && testValueStr.endsWith(numberStr)) {
+            val newTestValue = testValueStr.dropRight(numberStr.length).toLong
+            helper(newTestValue, newNumbers)
+          }
+          else
+            false
+        case _ => false
+      }
+
+      helper(equation.testValue, equation.numbers.view.reverse.toList)
+    }
   }
 
   def parseEquation(s: String): Equation = s match {
@@ -33,6 +66,7 @@ object Day7 {
   lazy val input: String = scala.io.Source.fromInputStream(getClass.getResourceAsStream("day7.txt")).mkString.trim
 
   def main(args: Array[String]): Unit = {
-    println(totalCalibrationResult(parseInput(input)))
+    println(Part1.totalCalibrationResult(parseInput(input)))
+    println(Part2.totalCalibrationResult(parseInput(input)))
   }
 }
--- src/test/scala/eu/sim642/adventofcode2024/Day7Test.scala
+++ src/test/scala/eu/sim642/adventofcode2024/Day7Test.scala
@@ -17,10 +17,20 @@ class Day7Test extends AnyFunSuite {
       |292: 11 6 16 20""".stripMargin
 
   test("Part 1 examples") {
-    assert(totalCalibrationResult(parseInput(exampleInput)) == 3749)
+    assert(Part1.totalCalibrationResult(parseInput(exampleInput)) == 3749)
   }
 
   test("Part 1 input answer") {
-    assert(totalCalibrationResult(parseInput(input)) == 1620690235709L)
+    assert(Part1.totalCalibrationResult(parseInput(input)) == 1620690235709L)
+  }
+
+  test("Part 2 examples") {
+    assert(Part2.isSolvable(parseEquation("7290: 6 8 6 15")))
+
+    assert(Part2.totalCalibrationResult(parseInput(exampleInput)) == 11387)
+  }
+
+  test("Part 2 input answer") {
+    assert(Part2.totalCalibrationResult(parseInput(input)) == 145397611075341L)
   }
 }
