569fc66429e658507a73c312cd242ce37963dee1
Simmo Saan <simmo.saan@gmail.com>
2024-12-10 08:25:42 +0200

    Solve 2024 day 10 part 1

---
+++ src/main/scala/eu/sim642/adventofcode2024/Day10.scala
@@ -0,0 +1,44 @@
+package eu.sim642.adventofcode2024
+
+import eu.sim642.adventofcodelib.Grid
+import eu.sim642.adventofcodelib.GridImplicits.*
+import eu.sim642.adventofcodelib.graph.{BFS, GraphTraversal, UnitNeighbors}
+import eu.sim642.adventofcodelib.pos.Pos
+
+
+object Day10 {
+
+  def countTrails(grid: Grid[Char], startPos: Pos): Int = {
+    val graphTraversal = new GraphTraversal[Pos] with UnitNeighbors[Pos] {
+      override val startNode: Pos = startPos
+
+      override def unitNeighbors(pos: Pos): IterableOnce[Pos] = {
+        val height = grid(pos)
+        for {
+          offset <- Pos.axisOffsets
+          newPos = pos + offset
+          if grid.containsPos(newPos)
+          if grid(newPos) == height + 1
+        } yield newPos
+      }
+    }
+
+    BFS.traverse(graphTraversal).nodes.count(grid(_) == '9')
+  }
+
+  def sumTrailheadScores(grid: Grid[Char]): Int = {
+    (for {
+      (row, y) <- grid.view.zipWithIndex
+      (cell, x) <- row.view.zipWithIndex
+      if cell == '0'
+    } yield countTrails(grid, Pos(x, y))).sum
+  }
+
+  def parseGrid(input: String): Grid[Char] = input.linesIterator.map(_.toVector).toVector
+
+  lazy val input: String = scala.io.Source.fromInputStream(getClass.getResourceAsStream("day10.txt")).mkString.trim
+
+  def main(args: Array[String]): Unit = {
+    println(sumTrailheadScores(parseGrid(input)))
+  }
+}
---
+++ src/test/scala/eu/sim642/adventofcode2024/Day10Test.scala
@@ -0,0 +1,31 @@
+package eu.sim642.adventofcode2024
+
+import Day10._
+import org.scalatest.funsuite.AnyFunSuite
+
+class Day10Test extends AnyFunSuite {
+
+  val exampleInput =
+    """0123
+      |1234
+      |8765
+      |9876""".stripMargin
+
+  val exampleInput2 =
+    """89010123
+      |78121874
+      |87430965
+      |96549874
+      |45678903
+      |32019012
+      |01329801
+      |10456732""".stripMargin
+
+  test("Part 1 examples") {
+    assert(sumTrailheadScores(parseGrid(exampleInput2)) == 36)
+  }
+
+  test("Part 1 input answer") {
+    assert(sumTrailheadScores(parseGrid(input)) == 557)
+  }
+}
