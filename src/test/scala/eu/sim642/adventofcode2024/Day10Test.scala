5ee8580b0d5b69da60a394eeaada270d32cf1907
Simmo Saan <simmo.saan@gmail.com>
2024-12-10 08:52:20 +0200

    Add more tests to 2024 day 10

--- src/test/scala/eu/sim642/adventofcode2024/Day10Test.scala
+++ src/test/scala/eu/sim642/adventofcode2024/Day10Test.scala
@@ -21,7 +21,64 @@ class Day10Test extends AnyFunSuite {
       |01329801
       |10456732""".stripMargin
 
+  val exampleTrailhead =
+    """...0...
+      |...1...
+      |...2...
+      |6543456
+      |7.....7
+      |8.....8
+      |9.....9""".stripMargin
+
+  val exampleTrailhead2 =
+    """..90..9
+      |...1.98
+      |...2..7
+      |6543456
+      |765.987
+      |876....
+      |987....""".stripMargin
+
+  val exampleTrailheads =
+    """10..9..
+      |2...8..
+      |3...7..
+      |4567654
+      |...8..3
+      |...9..2
+      |.....01""".stripMargin
+
+  val exampleTrailhead3 =
+    """.....0.
+      |..4321.
+      |..5..2.
+      |..6543.
+      |..7..4.
+      |..8765.
+      |..9....""".stripMargin
+
+  val exampleTrailhead4 =
+    """..90..9
+      |...1.98
+      |...2..7
+      |6543456
+      |765.987
+      |876....
+      |987....""".stripMargin
+
+  val exampleTrailhead5 =
+    """012345
+      |123456
+      |234567
+      |345678
+      |4.6789
+      |56789.""".stripMargin
+
   test("Part 1 examples") {
+    assert(sumTrailheadScores(parseGrid(exampleInput)) == 1)
+    assert(sumTrailheadScores(parseGrid(exampleTrailhead)) == 2)
+    assert(sumTrailheadScores(parseGrid(exampleTrailhead2)) == 4)
+    assert(sumTrailheadScores(parseGrid(exampleTrailheads)) == 3)
     assert(sumTrailheadScores(parseGrid(exampleInput2)) == 36)
   }
 
@@ -30,6 +87,10 @@ class Day10Test extends AnyFunSuite {
   }
 
   test("Part 2 examples") {
+    assert(sumTrailheadRatings(parseGrid(exampleInput)) == 16) // not in text
+    assert(sumTrailheadRatings(parseGrid(exampleTrailhead3)) == 3)
+    assert(sumTrailheadRatings(parseGrid(exampleTrailhead4)) == 13)
+    assert(sumTrailheadRatings(parseGrid(exampleTrailhead5)) == 227)
     assert(sumTrailheadRatings(parseGrid(exampleInput2)) == 81)
   }
 
