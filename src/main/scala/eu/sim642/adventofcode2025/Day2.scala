83ef6e4332822371436ec26354a13974360882b1
Simmo Saan <simmo.saan@gmail.com>
2025-12-02 07:54:05 +0200

    Solve 2025 day 2 part 2

--- src/main/scala/eu/sim642/adventofcode2025/Day2.scala
+++ src/main/scala/eu/sim642/adventofcode2025/Day2.scala
@@ -6,19 +6,35 @@ object Day2 {
 
   type Range = Interval
 
-  val invalidIds: LazyList[Long] = LazyList.from(1).map(i =>
-    val len = i.toString.length // TODO: already have a (better) way to get number of digits?
-    i * (Math.powExact(10L, len) + 1)
+  def numDigits(i: Long): Int = {
+    i.toString.length // TODO: already have a (better) way to get number of digits?
+  }
+
+  def invalidIds(times: Int): LazyList[Long] = LazyList.from(1).map(i =>
+    val pow = Math.powExact(10L, numDigits(i))
+    val pattern = (1 until times).foldLeft(1L)((acc, _) => pow * acc + 1)
+    i * pattern
   )
 
-  def sumInvalidIds(ranges: Seq[Range]): Long = { // TODO: Long?
-    val max = ranges.map(_.max).max
-    invalidIds
-      .takeWhile(_ <= max)
-      .filter(id =>
-        ranges.exists(_.contains(id))
-      )
-      .sum
+  trait Part {
+    def maxTimes(max: Long): Int
+
+    def sumInvalidIds(ranges: Seq[Range]): Long = {
+      val max = ranges.map(_.max).max
+      (for {
+        times <- 2 to maxTimes(max) // TODO: bad shadowing
+        id <- invalidIds(times).takeWhile(_ <= max)
+        if ranges.exists(_.contains(id))
+      } yield id).toSet.sum // toSet because some IDs can be created multiple ways (2222 is 2-2-2-2 and 22-22), but shouldn't count multiple times
+    }
+  }
+
+  object Part1 extends Part {
+    override def maxTimes(max: Long): Int = 2
+  }
+
+  object Part2 extends Part {
+    override def maxTimes(max: Long): Int = numDigits(max)
   }
 
   def parseRange(s: String): Range = s match {
@@ -30,6 +46,7 @@ object Day2 {
   lazy val input: String = scala.io.Source.fromInputStream(getClass.getResourceAsStream("day2.txt")).mkString.trim
 
   def main(args: Array[String]): Unit = {
-    println(sumInvalidIds(parseRanges(input)))
+    println(Part1.sumInvalidIds(parseRanges(input)))
+    println(Part2.sumInvalidIds(parseRanges(input)))
   }
 }
--- src/test/scala/eu/sim642/adventofcode2025/Day2Test.scala
+++ src/test/scala/eu/sim642/adventofcode2025/Day2Test.scala
@@ -9,10 +9,18 @@ class Day2Test extends AnyFunSuite {
     """11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"""
 
   test("Part 1 examples") {
-    assert(sumInvalidIds(parseRanges(exampleInput)) == 1227775554)
+    assert(Part1.sumInvalidIds(parseRanges(exampleInput)) == 1227775554)
   }
 
   test("Part 1 input answer") {
-    assert(sumInvalidIds(parseRanges(input)) == 5398419778L)
+    assert(Part1.sumInvalidIds(parseRanges(input)) == 5398419778L)
+  }
+
+  test("Part 2 examples") {
+    assert(Part2.sumInvalidIds(parseRanges(exampleInput)) == 4174379265L)
+  }
+
+  test("Part 2 input answer") {
+    assert(Part2.sumInvalidIds(parseRanges(input)) == 15704845910L)
   }
 }
