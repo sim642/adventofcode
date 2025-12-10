46fe35da8394202a81471e9f8efc6ea368318354
Simmo Saan <simmo.saan@gmail.com>
2025-12-10 07:34:15 +0200

    Solve 2025 day 10 part 1

---
+++ src/main/scala/eu/sim642/adventofcode2025/Day10.scala
@@ -0,0 +1,43 @@
+package eu.sim642.adventofcode2025
+
+import eu.sim642.adventofcodelib.graph.{BFS, GraphSearch, TargetNode, UnitNeighbors}
+
+object Day10 {
+
+  type Lights = Vector[Boolean]
+  type Buttons = Seq[Seq[Int]]
+  type Joltages = Seq[Int]
+
+  case class Machine(lights: Lights, buttons: Buttons, joltages: Joltages)
+
+  def fewestPresses(machine: Machine): Int = {
+    val graphSearch = new GraphSearch[Lights] with UnitNeighbors[Lights] with TargetNode[Lights] {
+      override val startNode: Lights = machine.lights.map(_ => false)
+
+      override def unitNeighbors(lights: Lights): IterableOnce[Lights] =
+        machine.buttons.map(_.foldLeft(lights)((acc, i) => acc.updated(i, !acc(i))))
+
+      override val targetNode: Lights = machine.lights
+    }
+
+    BFS.search(graphSearch).target.get._2
+  }
+
+  def sumFewestPresses(machines: Seq[Machine]): Int = machines.map(fewestPresses).sum
+
+  def parseMachine(s: String): Machine = s match {
+    case s"[$lightsStr] $buttonsStr {$joltagesStr}" =>
+      val lights = lightsStr.map(_ == '#').toVector
+      val buttons = buttonsStr.split(" ").map(_.tail.init.split(",").map(_.toInt).toSeq).toSeq
+      val joltages = joltagesStr.split(",").map(_.toInt).toSeq
+      Machine(lights, buttons, joltages)
+  }
+
+  def parseMachines(input: String): Seq[Machine] = input.linesIterator.map(parseMachine).toSeq
+
+  lazy val input: String = scala.io.Source.fromInputStream(getClass.getResourceAsStream("day10.txt")).mkString.trim
+
+  def main(args: Array[String]): Unit = {
+    println(sumFewestPresses(parseMachines(input)))
+  }
+}
---
+++ src/test/scala/eu/sim642/adventofcode2025/Day10Test.scala
@@ -0,0 +1,20 @@
+package eu.sim642.adventofcode2025
+
+import Day10._
+import org.scalatest.funsuite.AnyFunSuite
+
+class Day10Test extends AnyFunSuite {
+
+  val exampleInput =
+    """[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+      |[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
+      |[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}""".stripMargin
+
+  test("Part 1 examples") {
+    assert(sumFewestPresses(parseMachines(exampleInput)) == 7)
+  }
+
+  test("Part 1 input answer") {
+    assert(sumFewestPresses(parseMachines(input)) == 449)
+  }
+}
